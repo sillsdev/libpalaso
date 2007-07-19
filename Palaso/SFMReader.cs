70c2c98506fa0321bfa86a9f1fa542c16731d0df
kellogg <devnull@localhost>
2007-07-19 06:32:03 +0000

    Created SFMReader
    git-svn-id: http://code.wesay.org/Palaso/trunk/Palaso@827 b25276a9-ce15-0410-9281-991c76e3d784
    
    committer: kellogg <kellogg@b25276a9-ce15-0410-9281-991c76e3d784>

--- Palaso/SFMReader.cs
+++ Palaso/SFMReader.cs
@@ -2,16 +2,30 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Text;
-using System.Text.RegularExpressions;
 
 namespace Palaso
 {
 	public class SFMReader
 	{
-		private TextReader buffer;
-		private string _text = "";
-		private string _extraTxt = "";
+		private readonly StreamReader buffer;
+
+		private enum State
+		{
+			Init,
+			Tag,
+			Text,
+			Finished
+		};
+
+		private State _parseState = State.Init;
+		private ParseMode _parseMode;
+
+		public enum ParseMode
+		{
+			Default,
+			Shoebox,
+			Usfm
+		}
 
 		/// <summary>
 		/// Construct a new SFMReader with filename
@@ -31,6 +45,12 @@ namespace Palaso
 			buffer = new StreamReader(stream);
 		}
 
+		public ParseMode Mode
+		{
+			get { return _parseMode;  }
+			set { _parseMode = value; }
+		}
+
 		/// <summary>
 		/// Read next tag and return the name only (exclude backslash
 		/// and space).
@@ -38,22 +58,73 @@ namespace Palaso
 		/// <returns>next tag name</returns>
 		public string ReadNextTag()
 		{
-			// build Regex pattern based on tagname
-			Regex reTag = new Regex(@".*?\\([^ ]+)[ ]([^\\]*)(.*)");
+			switch (_parseState)
+			{
+				case State.Init:
+					ReadInitialText();
+					break;
+				case State.Text:
+					ReadNextText();
+					break;
+			}
+			if(_parseState == State.Finished)
+			{
+				return null;
+			}
+			Debug.Assert(_parseState == State.Tag);
 
-			// Use extra text, if necessary
-			string line;
-			if(_extraTxt.Length>0)
-				line = _extraTxt;
+			int c = buffer.Read(); // advance input stream over the initial \
+			Debug.Assert(c == '\\' || c==-1);
+
+			string tag ;
+			bool hasReadNextChar = false;
+			if(Mode == ParseMode.Usfm)
+			{
+				tag = GetNextToken(delegate(char ch) { return Char.IsWhiteSpace(ch) || ch == '\\' || ch == '*'; });
+				if (buffer.Peek() == '*')
+				{
+					tag += (char) buffer.Read();
+					hasReadNextChar = true;
+				}
+			}
 			else
-				line = buffer.ReadLine();
+			{
+				tag = GetNextToken(delegate(char ch) { return Char.IsWhiteSpace(ch) || ch == '\\'; });
+			}
 
-			// Do matching
-			Match m = reTag.Match(line);
-			string tag = m.Groups[1].Value;
-			_text = m.Groups[2].Value;
-			_extraTxt = m.Groups[3].Value;
+			if (tag == null)
+			{
+				_parseState = State.Finished;
+				return null;
+			}
+			if (buffer.Peek() != '\\' && !hasReadNextChar)
+			{
+				c = buffer.Read(); // advance input stream over the terminating whitespace
+				Debug.Assert(c == -1 || char.IsWhiteSpace((char) c));
+			}
+			_parseState = State.Text;
 			return tag;
+	   }
+
+		private string GetNextToken(Predicate<char> isTokenTerminator)
+		{
+			string token = string.Empty;
+			for(;;)
+			{
+				int peekedChar = buffer.Peek();
+				if (peekedChar == -1)//end of stream
+				{
+					if (token.Length == 0)
+						token = null;
+					break;
+				}
+
+				if (isTokenTerminator((char) peekedChar))
+					break;
+
+				token += (char) buffer.Read();
+			}
+			return token;
 		}
 
 		/// <summary>
@@ -62,41 +133,64 @@ namespace Palaso
 		/// <returns>Next text</returns>
 		public string ReadNextText()
 		{
-			if(_extraTxt!="")
+			if (_parseState == State.Init)
 			{
-				return _text;
-			} else
-			{
-				Regex reTag = new Regex(@"([^\\]*)(.*)");
-
-				// It seems that if end of file, ReadLine returns null
-				string line = buffer.ReadLine();
-				if (line == null) return _text;
-
-				Match m = reTag.Match(line);
-				_text += (_text == "" ? "" : " ");
-				_text += m.Groups[1].Value;
-				_extraTxt = m.Groups[2].Value;
-
-				if(m.Groups[2].Value!="")
-				{   // Return available text
-					return _text;
-				}
-				else
-				{
-					ReadNextText();
-				}
+				ReadInitialText();
 			}
-			return _text;
+			if(_parseState == State.Tag)
+			{
+				ReadNextTag();
+			}
+			return ReadText();
 		}
 
-		public LinkedList<string> Tokenize(string txt)
-		{
-			LinkedList<string> rtn = new LinkedList<string>();
-			Regex reWords = new Regex(@"\s*(\S+)");
-			Match m = reWords.Match(txt);
+		//public LinkedList<string> Tokenize(string txt)
+		//{
+		//    LinkedList<string> rtn = new LinkedList<string>();
+		//    Regex reWords = new Regex(@"\s*(\S+)");
+		//    Match m = reWords.Match(txt);
+		//    if(m.Success)
+		//    {
+		//        rtn.AddLast(m.Groups[1].Value);
+		//    }
+		//    return rtn;
+		//}
 
-			return rtn;
+		public string ReadInitialText()
+		{
+			if(_parseState != State.Init)
+				throw new InvalidOperationException("ReadInitialText must be called before ReadNextText or ReadNextTag");
+
+			return ReadText() ?? "";
+		}
+
+		private string ReadText()
+		{
+			if (_parseState == State.Finished)
+				return null;
+			string text=string.Empty;
+			do
+			{
+				string token = GetNextToken(delegate(char c) { return c == '\\'; });
+				text += token;
+				if (Mode == ParseMode.Shoebox)
+				{
+					if (token == null)
+						break;
+					if (text.Length > 0 && text[text.Length - 1] != '\n' && buffer.Peek() != -1)
+					{
+						text += (char)buffer.Read();
+					}
+					else break;
+				}
+			} while (Mode == ParseMode.Shoebox);
+			if (text == null)
+			{
+				_parseState = State.Finished;
+				return "";
+			}
+			_parseState = State.Tag;
+			return text;
 		}
 	}
 }
