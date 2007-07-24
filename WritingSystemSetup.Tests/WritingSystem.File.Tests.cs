9e8fc7acc6f76c11c54b4e0362608708ce116060
John Hatton <hattonjohn@gmail.com>
2007-07-24 07:33:02 +0000

    git-svn-id: http://code.wesay.org/Palaso/trunk/Palaso@835 b25276a9-ce15-0410-9281-991c76e3d784
    committer: hatton <hatton@b25276a9-ce15-0410-9281-991c76e3d784>

--- WritingSystemSetup.Tests/WritingSystem.File.Tests.cs
+++ WritingSystemSetup.Tests/WritingSystem.File.Tests.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using NUnit.Framework;
+using Palaso;
 
 namespace WritingSystemSetup.Tests
 {
@@ -11,44 +13,99 @@ namespace WritingSystemSetup.Tests
 	[TestFixture]
 	public class WritingSystemFileTests
 	{
+		private string _testDir;
+		private WritingSystemRepository _repository;
+
 		[SetUp]
 		public void Setup()
 		{
-
+			_testDir = Palaso.Tests.TestUtilities.GetTempTestDirectory();
+			_repository = new WritingSystemRepository(_testDir);
 		}
 
 		[TearDown]
 		public void TearDown()
 		{
-
+			//Palaso.Tests.TestUtilities.DeleteFolderThatMayBeInUse(_testDir);
+			Directory.Delete(_testDir,true);
 		}
 
-		[Test, Ignore()]
+		[Test]
 		public void SavesWhenNotPreexisting()
 		{
-
+			WritingSystemDefinition ws = new WritingSystemDefinition();
+			ws.SaveToRepository(_repository);
+			AssertWritingSystemFileExists(ws);
 		}
 
-		[Test, Ignore()]
+		private void AssertWritingSystemFileExists(WritingSystemDefinition ws)
+		{
+		   AssertWritingSystemFileExists(ws,_repository);
+		}
+		private void AssertWritingSystemFileExists(WritingSystemDefinition ws, WritingSystemRepository repository)
+		{
+			string path = Path.Combine(repository.Path, ws.FileName);
+			Assert.IsTrue(File.Exists(path));
+		}
+		[Test]
+		public void FileNameWhenNothingKnown()
+		{
+			WritingSystemDefinition ws = new WritingSystemDefinition();
+			Assert.AreEqual("unknown.ldml", ws.FileName);
+		}
+
+		[Test]
+		public void FileNameWhenOnlyHaveIso()
+		{
+			WritingSystemDefinition ws = new WritingSystemDefinition();
+			ws.ISO = "en";
+			Assert.AreEqual("en.ldml", ws.FileName);
+		}
+		[Test]
+		public void FileNameWhenHaveIsoAndRegion()
+		{
+			WritingSystemDefinition ws = new WritingSystemDefinition();
+			ws.ISO = "en";
+			ws.Region = "us";
+			Assert.AreEqual("en-us.ldml", ws.FileName);
+		}
+
+		[Test]
 		public void SavesWhenPreexisting()
 		{
+			WritingSystemDefinition ws = new WritingSystemDefinition();
+			ws.ISO = "en";
+			ws.SaveToRepository(_repository);
+			ws.SaveToRepository(_repository);
 
+			WritingSystemDefinition ws2 = new WritingSystemDefinition();
+			ws.ISO = "en";
+			ws.SaveToRepository(_repository);
 		}
 
-		[Test, Ignore()]
+		[Test]
 		public void SavesWhenDirectoryNotFound()
 		{
-
+			WritingSystemRepository newGuy = new WritingSystemRepository(Path.Combine(_testDir, "newguy"));
+			WritingSystemDefinition ws = new WritingSystemDefinition();
+			ws.SaveToRepository(newGuy);
+			AssertWritingSystemFileExists(ws,newGuy);
 		}
-		[Test, Ignore()]
-		public void SavesWithExpectedFileName()
-		{
 
-		}
-		[Test, Ignore()]
+		[Test]
 		public void UpdatesFileNameWhenComponentsChange()
 		{
-
+			WritingSystemDefinition ws = new WritingSystemDefinition();
+			ws.ISO = "en";
+			ws.SaveToRepository(_repository);
+			string path = Path.Combine(_repository.Path, "en.ldml");
+			Assert.IsTrue(File.Exists(path));
+			ws.ISO = "blah";
+			ws.Region = "foo";
+			ws.SaveToRepository(_repository);
+			Assert.IsFalse(File.Exists(path));
+			 path = Path.Combine(_repository.Path, "blah-foo.ldml");
+			Assert.IsTrue(File.Exists(path));
 		}
 	}
 
