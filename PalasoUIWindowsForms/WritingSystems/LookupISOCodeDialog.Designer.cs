121b574a781cd8bcb07ca13098c67dbde2876aa4
Cambell Prince <cambell_prince@sil.org>
2009-05-04 13:46:03 +0700

    Changed anchor on LookupISOCodeDialog to allow vertical expansion

--- PalasoUIWindowsForms/WritingSystems/LookupISOCodeDialog.Designer.cs
+++ PalasoUIWindowsForms/WritingSystems/LookupISOCodeDialog.Designer.cs
@@ -28,32 +28,34 @@ namespace Palaso.UI.WindowsForms.WritingSystems
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.listView1 = new System.Windows.Forms.ListView();
-			this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
-			this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
-			this._okButton = new System.Windows.Forms.Button();
-			this._aboutLink = new System.Windows.Forms.LinkLabel();
-			this._aboutLink639_1 = new System.Windows.Forms.LinkLabel();
-			this.SuspendLayout();
+			this.listView1 = new System.Windows.Forms.ListView ();
+			this.columnHeader1 = new System.Windows.Forms.ColumnHeader ();
+			this.columnHeader2 = new System.Windows.Forms.ColumnHeader ();
+			this._okButton = new System.Windows.Forms.Button ();
+			this._aboutLink = new System.Windows.Forms.LinkLabel ();
+			this._aboutLink639_1 = new System.Windows.Forms.LinkLabel ();
+			this.SuspendLayout ();
 			//
 			// listView1
 			//
-			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+			this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles) (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+						| System.Windows.Forms.AnchorStyles.Left)));
+			this.listView1.Columns.AddRange (new System.Windows.Forms.ColumnHeader [] {
 			this.columnHeader1,
 			this.columnHeader2});
 			this.listView1.FullRowSelect = true;
 			this.listView1.HideSelection = false;
-			this.listView1.Location = new System.Drawing.Point(0, -1);
+			this.listView1.Location = new System.Drawing.Point (0, -1);
 			this.listView1.Name = "listView1";
-			this.listView1.Size = new System.Drawing.Size(292, 226);
+			this.listView1.Size = new System.Drawing.Size (292, 241);
 			this.listView1.TabIndex = 0;
 			this.listView1.UseCompatibleStateImageBehavior = false;
 			this.listView1.View = System.Windows.Forms.View.Details;
 			this.listView1.VirtualMode = true;
-			this.listView1.SearchForVirtualItem += new System.Windows.Forms.SearchForVirtualItemEventHandler(this.listView1_SearchForVirtualItem);
-			this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
-			this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
-			this.listView1.RetrieveVirtualItem += new System.Windows.Forms.RetrieveVirtualItemEventHandler(this.listView1_RetrieveVirtualItem);
+			this.listView1.SearchForVirtualItem += new System.Windows.Forms.SearchForVirtualItemEventHandler (this.listView1_SearchForVirtualItem);
+			this.listView1.SelectedIndexChanged += new System.EventHandler (this.listView1_SelectedIndexChanged);
+			this.listView1.DoubleClick += new System.EventHandler (this.listView1_DoubleClick);
+			this.listView1.RetrieveVirtualItem += new System.Windows.Forms.RetrieveVirtualItemEventHandler (this.listView1_RetrieveVirtualItem);
 			//
 			// columnHeader1
 			//
@@ -66,55 +68,58 @@ namespace Palaso.UI.WindowsForms.WritingSystems
 			//
 			// _okButton
 			//
-			this._okButton.Location = new System.Drawing.Point(205, 231);
+			this._okButton.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this._okButton.Location = new System.Drawing.Point (205, 274);
 			this._okButton.Name = "_okButton";
-			this._okButton.Size = new System.Drawing.Size(75, 23);
+			this._okButton.Size = new System.Drawing.Size (75, 23);
 			this._okButton.TabIndex = 1;
 			this._okButton.Text = "&OK";
 			this._okButton.UseVisualStyleBackColor = true;
-			this._okButton.Click += new System.EventHandler(this._okButton_Click);
+			this._okButton.Click += new System.EventHandler (this._okButton_Click);
 			//
 			// _aboutLink
 			//
+			this._aboutLink.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
 			this._aboutLink.AutoSize = true;
-			this._aboutLink.Location = new System.Drawing.Point(12, 252);
+			this._aboutLink.Location = new System.Drawing.Point (12, 287);
 			this._aboutLink.Name = "_aboutLink";
-			this._aboutLink.Size = new System.Drawing.Size(119, 13);
+			this._aboutLink.Size = new System.Drawing.Size (119, 13);
 			this._aboutLink.TabIndex = 2;
 			this._aboutLink.TabStop = true;
 			this._aboutLink.Text = "About ISO 639-3 Codes";
-			this._aboutLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this._aboutLink_LinkClicked);
+			this._aboutLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler (this._aboutLink_LinkClicked);
 			//
 			// _aboutLink639_1
 			//
+			this._aboutLink639_1.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
 			this._aboutLink639_1.AutoSize = true;
-			this._aboutLink639_1.Location = new System.Drawing.Point(12, 231);
+			this._aboutLink639_1.Location = new System.Drawing.Point (12, 259);
 			this._aboutLink639_1.Name = "_aboutLink639_1";
-			this._aboutLink639_1.Size = new System.Drawing.Size(119, 13);
+			this._aboutLink639_1.Size = new System.Drawing.Size (119, 13);
 			this._aboutLink639_1.TabIndex = 3;
 			this._aboutLink639_1.TabStop = true;
 			this._aboutLink639_1.Text = "About ISO 639-1 Codes";
-			this._aboutLink639_1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this._aboutLink639_1_LinkClicked);
+			this._aboutLink639_1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler (this._aboutLink639_1_LinkClicked);
 			//
 			// LookupISOCodeDialog
 			//
 			this.AcceptButton = this._okButton;
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF (6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(292, 274);
-			this.Controls.Add(this._aboutLink639_1);
-			this.Controls.Add(this._aboutLink);
-			this.Controls.Add(this._okButton);
-			this.Controls.Add(this.listView1);
+			this.ClientSize = new System.Drawing.Size (292, 309);
+			this.Controls.Add (this._aboutLink639_1);
+			this.Controls.Add (this._aboutLink);
+			this.Controls.Add (this._okButton);
+			this.Controls.Add (this.listView1);
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
 			this.Name = "LookupISOCodeDialog";
 			this.ShowIcon = false;
 			this.ShowInTaskbar = false;
 			this.Text = "Lookup ISO Code...";
-			this.Load += new System.EventHandler(this.LookupISOCodeDialog_Load);
-			this.ResumeLayout(false);
-			this.PerformLayout();
+			this.Load += new System.EventHandler (this.LookupISOCodeDialog_Load);
+			this.ResumeLayout (false);
+			this.PerformLayout ();
 
 		}
 
