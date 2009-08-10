e68cec6402c0e82aadc1ef6480ae6ab6149b2230
Cambell Prince <cambell_prince@sil.org>
2009-08-10 13:44:27 +0700

    moved Palaso.Data from WeSay

---
+++ Palaso.Tests/Data/RecordTokenTests.cs
@@ -0,0 +1,317 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Palaso.Data;
+using Palaso.Progress;
+
+namespace Palaso.Tests.Data
+{
+	[TestFixture]
+	public class RecordTokenTests
+	{
+		private MemoryDataMapper<PalasoTestItem> _dataMapper;
+
+		[SetUp]
+		public void Setup()
+		{
+			_dataMapper = new MemoryDataMapper<PalasoTestItem>();
+		}
+
+		[TearDown]
+		public void Teardown()
+		{
+			_dataMapper.Dispose();
+		}
+
+		[Test]
+		public void Construct()
+		{
+			Assert.IsNotNull(new RecordToken<PalasoTestItem>(_dataMapper, new TestRepositoryId(8)));
+		}
+
+		[Test]
+		[ExpectedException(typeof (ArgumentNullException))]
+		public void Construct_NullRepository_Throws()
+		{
+			Assert.IsNotNull(new RecordToken<PalasoTestItem>(null, new TestRepositoryId(8)));
+		}
+
+		[Test]
+		[ExpectedException(typeof (ArgumentNullException))]
+		public void Construct_NullRepositoryId_Throws()
+		{
+			Assert.IsNotNull(new RecordToken<PalasoTestItem>(_dataMapper, null));
+		}
+
+		[Test]
+		public void ConstructWithResults()
+		{
+			Assert.IsNotNull(new RecordToken<PalasoTestItem>(_dataMapper,
+													   new Dictionary<string, object>(),
+													   new TestRepositoryId(8)));
+		}
+
+		[Test]
+		[ExpectedException(typeof (ArgumentNullException))]
+		public void ConstructWithResults_NullRepository_Throws()
+		{
+			Assert.IsNotNull(new RecordToken<PalasoTestItem>(null,
+													   new Dictionary<string, object>(),
+													   new TestRepositoryId(8)));
+		}
+
+		[Test]
+		[ExpectedException(typeof (ArgumentNullException))]
+		public void ConstructWithResults_NullResults_Throws()
+		{
+			Assert.IsNotNull(new RecordToken<PalasoTestItem>(_dataMapper, null, new TestRepositoryId(8)));
+		}
+
+		[Test]
+		[ExpectedException(typeof (ArgumentNullException))]
+		public void ConstructWithResults_NullRepositoryId_Throws()
+		{
+			Assert.IsNotNull(new RecordToken<PalasoTestItem>(_dataMapper,
+													   new Dictionary<string, object>(),
+													   null));
+		}
+	}
+
+	public class RecordTokenTestsBase
+	{
+		private RecordToken<PalasoTestItem> _token;
+
+		public RecordToken<PalasoTestItem> Token
+		{
+			get
+			{
+				if (_token == null)
+				{
+					throw new InvalidOperationException(
+						"Token must be initialized before tests can be run");
+				}
+				return _token;
+			}
+			set { _token = value; }
+		}
+
+		[Test]
+		[ExpectedException(typeof (ArgumentNullException))]
+		public void GetIndexer_NullFieldName_Throws()
+		{
+			Token[null] = null;
+		}
+
+		[Test]
+		public void GetIndexer_AnyFieldName_Null()
+		{
+			Assert.IsNull(Token["anything"]);
+		}
+
+		[Test]
+		public void GetIndexer_EmptyName_Null()
+		{
+			Assert.IsNull(Token[""]);
+		}
+
+		[Test]
+		public void SetIndexer_AnthingFieldName_Null_KeepsNull()
+		{
+			Token["anything"] = null;
+			Assert.IsNull(Token["anything"]);
+		}
+
+		[Test]
+		public void SetIndexer_AnthingFieldName_Value_KeepsValue()
+		{
+			object o = new object();
+			Token["anything"] = o;
+			Assert.AreSame(o, Token["anything"]);
+		}
+
+		[Test]
+		public void SetIndexer_EmptyFieldName_Value_KeepsValue()
+		{
+			object o = new object();
+			Token[""] = o;
+			Assert.AreSame(o, Token[""]);
+		}
+
+		[Test]
+		public void Id_AsConstructed()
+		{
+			Assert.AreEqual(Token.Id, new TestRepositoryId(8));
+		}
+	}
+
+	[TestFixture]
+	public class RecordTokenConstructedWithoutResultsTests: RecordTokenTestsBase
+	{
+		private MemoryDataMapper<PalasoTestItem> dataMapper;
+
+		[SetUp]
+		public void Setup()
+		{
+			dataMapper = new MemoryDataMapper<PalasoTestItem>();
+			Token = new RecordToken<PalasoTestItem>(dataMapper, new TestRepositoryId(8));
+		}
+
+		[TearDown]
+		public void Teardown()
+		{
+			dataMapper.Dispose();
+		}
+	}
+
+	[TestFixture]
+	public class RecordTokenConstructedWithResultsTests: RecordTokenTestsBase
+	{
+		private MemoryDataMapper<PalasoTestItem> dataMapper;
+
+		[SetUp]
+		public void Setup()
+		{
+			dataMapper = new MemoryDataMapper<PalasoTestItem>();
+			Dictionary<string, object> results = new Dictionary<string, object>();
+			results["string"] = "result";
+			results["int"] = 12;
+			Token = new RecordToken<PalasoTestItem>(dataMapper, results, new TestRepositoryId(8));
+		}
+
+		[TearDown]
+		public void Teardown()
+		{
+			dataMapper.Dispose();
+		}
+
+		[Test]
+		public void GetIndexer_ExistingFieldName_ConstructedValue()
+		{
+			Assert.AreEqual("result", Token["string"]);
+			Assert.AreEqual(12, Token["int"]);
+		}
+
+		[Test]
+		public void SetIndexer_ExistingFieldName_OverwritesValue()
+		{
+			Token["string"] = "new result";
+			Assert.AreEqual("new result", Token["string"]);
+		}
+	}
+
+	[TestFixture]
+	public class RecordTokenUsesRepositoryTests
+	{
+		private class TestRepository: IDataMapper<PalasoTestItem>
+		{
+			private readonly PalasoTestItem _itemToReturn;
+
+			public TestRepository(PalasoTestItem itemToReturn)
+			{
+				_itemToReturn = itemToReturn;
+			}
+
+			public DateTime LastModified
+			{
+				get { throw new NotImplementedException(); }
+			}
+
+			public void Startup(ProgressState state)
+			{
+				throw new NotImplementedException();
+			}
+
+			public PalasoTestItem CreateItem()
+			{
+				throw new NotImplementedException();
+			}
+
+			public int CountAllItems()
+			{
+				throw new NotImplementedException();
+			}
+
+			public RepositoryId GetId(PalasoTestItem item)
+			{
+				throw new NotImplementedException();
+			}
+
+			public PalasoTestItem GetItem(RepositoryId id)
+			{
+				Assert.AreEqual(id, new TestRepositoryId(8));
+				return _itemToReturn;
+			}
+
+			public void DeleteItem(PalasoTestItem item)
+			{
+				throw new NotImplementedException();
+			}
+
+			public void DeleteItem(RepositoryId id)
+			{
+				throw new NotImplementedException();
+			}
+
+			public void DeleteAllItems()
+			{
+				throw new NotImplementedException();
+			}
+
+			public RepositoryId[] GetAllItems()
+			{
+				throw new NotImplementedException();
+			}
+
+			public void SaveItem(PalasoTestItem item)
+			{
+				throw new NotImplementedException();
+			}
+
+			public bool CanQuery
+			{
+				get { return false; }
+			}
+
+			public bool CanPersist
+			{
+				get { return false; }
+			}
+
+			public void SaveItems(IEnumerable<PalasoTestItem> items)
+			{
+				throw new NotImplementedException();
+			}
+
+			public ResultSet<PalasoTestItem> GetItemsMatching(IQuery<PalasoTestItem> query)
+			{
+				throw new NotImplementedException();
+			}
+
+			public void Dispose() {}
+		}
+
+		private TestRepository _repository;
+		private RecordToken<PalasoTestItem> _token;
+		private PalasoTestItem _testItem;
+
+		[SetUp]
+		public void Setup()
+		{
+			this._testItem = new PalasoTestItem();
+			_repository = new TestRepository(this._testItem);
+			this._token = new RecordToken<PalasoTestItem>(this._repository, new TestRepositoryId(8));
+		}
+
+		[TearDown]
+		public void Teardown()
+		{
+			_repository.Dispose();
+		}
+
+		[Test]
+		public void RealObject_DelegatesToRepository()
+		{
+			Assert.AreSame(_testItem, _token.RealObject);
+		}
+	}
+}
---
+++ Palaso.Tests/Data/ResultSetTests.cs
@@ -0,0 +1,187 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using Palaso.Data;
+
+namespace Palaso.Tests.Data
+{
+	[TestFixture]
+	public class ResultSetTests
+	{
+		private ResultSet<PalasoTestItem> _resultSet;
+		List<RecordToken<PalasoTestItem>> _results;
+		MemoryDataMapper<PalasoTestItem> _dataMapper;
+
+
+		[SetUp]
+		public void Setup()
+		{
+			_dataMapper = new MemoryDataMapper<PalasoTestItem>();
+			_results = new List<RecordToken<PalasoTestItem>>();
+
+			_results.Add(new RecordToken<PalasoTestItem>(_dataMapper, new TestRepositoryId(8)));
+			_results.Add(new RecordToken<PalasoTestItem>(_dataMapper, new TestRepositoryId(12)));
+			_results.Add(new RecordToken<PalasoTestItem>(_dataMapper, new TestRepositoryId(1)));
+			_results.Add(new RecordToken<PalasoTestItem>(_dataMapper, new TestRepositoryId(3)));
+
+			_resultSet = new ResultSet<PalasoTestItem>(_dataMapper, _results);
+		}
+
+		[TearDown]
+		public void Teardown()
+		{
+			_dataMapper.Dispose();
+		}
+
+		[Test]
+		public void FindFirstIndex_RepositoryIdEqualToOneInList_Index()
+		{
+			int index = _resultSet.FindFirstIndex(new TestRepositoryId(1));
+			Assert.AreEqual(2, index);
+		}
+
+		[Test]
+		public void Constructor_HasNoDuplicate_OrderRetained()
+		{
+			List<RecordToken<PalasoTestItem>> results = new List<RecordToken<PalasoTestItem>>();
+
+			results.Add(new RecordToken<PalasoTestItem>(_dataMapper, new TestRepositoryId(8)));
+			results.Add(new RecordToken<PalasoTestItem>(_dataMapper, new TestRepositoryId(2)));
+
+			ResultSet<PalasoTestItem> resultSet = new ResultSet<PalasoTestItem>(_dataMapper, results);
+			Assert.AreEqual(2, resultSet.Count);
+
+			int i = 0;
+			foreach (RecordToken<PalasoTestItem> token in resultSet)
+			{
+				Assert.AreEqual(results[i], token);
+				++i;
+			}
+		}
+
+		[Test]
+		public void Constructor_ResultsModifiedAfter_ResultSetNotModified()
+		{
+			ResultSet<PalasoTestItem> resultSet = new ResultSet<PalasoTestItem>(_dataMapper, _results);
+			_results.Add(new RecordToken<PalasoTestItem>(_dataMapper, _resultSet[2].Id));
+			Assert.AreNotEqual(_results.Count, resultSet.Count);
+
+		}
+
+	}
+
+	[TestFixture ]
+	public class ResultSetWithQueryTests
+	{
+		MemoryDataMapper<PalasoTestItem> dataMapper;
+		Dictionary<string, object> _queryResultsEmpty;
+		Dictionary<string, object> _queryResultsB;
+		Dictionary<string, object> _queryResultsA;
+
+		[SetUp]
+		public void Setup()
+		{
+			dataMapper = new MemoryDataMapper<PalasoTestItem>();
+			_queryResultsA = new Dictionary<string, object>();
+			_queryResultsA.Add("string", "A");
+			_queryResultsB = new Dictionary<string, object>();
+			_queryResultsB.Add("string", "B");
+			_queryResultsEmpty = new Dictionary<string, object>();
+			_queryResultsEmpty.Add("string", string.Empty);
+		}
+
+		[TearDown]
+		public void Teardown()
+		{
+			dataMapper.Dispose();
+		}
+
+		[Test]
+		public void Constructor_HasNoDuplicates_NoneRemoved()
+		{
+			List<RecordToken<PalasoTestItem>> results = new List<RecordToken<PalasoTestItem>>();
+
+			results.Add(new RecordToken<PalasoTestItem>(dataMapper, new TestRepositoryId(8)));
+			results.Add(new RecordToken<PalasoTestItem>(dataMapper, _queryResultsEmpty, new TestRepositoryId(8)));
+			results.Add(new RecordToken<PalasoTestItem>(dataMapper, _queryResultsA, new TestRepositoryId(8)));
+			results.Add(new RecordToken<PalasoTestItem>(dataMapper, _queryResultsB, new TestRepositoryId(8)));
+
+			ResultSet<PalasoTestItem> resultSet = new ResultSet<PalasoTestItem>(dataMapper, results);
+			Assert.AreEqual(4, resultSet.Count);
+		}
+
+		[Test]
+		public void Coalesce_NoItemsCanBeRemoved_NoneRemoved()
+		{
+			List<RecordToken<PalasoTestItem>> results = new List<RecordToken<PalasoTestItem>>();
+
+			results.Add(new RecordToken<PalasoTestItem>(dataMapper, _queryResultsA, new TestRepositoryId(8)));
+			results.Add(new RecordToken<PalasoTestItem>(dataMapper, _queryResultsB, new TestRepositoryId(12)));
+
+			ResultSet<PalasoTestItem> resultSet = new ResultSet<PalasoTestItem>(dataMapper, results);
+			resultSet.Coalesce("string", delegate(object o)
+											 {
+												 return string.IsNullOrEmpty((string)o);
+											 });
+			Assert.AreEqual(2, resultSet.Count);
+		}
+
+		[Test]
+		public void Coalesce_ItemCanBeRemovedButNoItemThatCannot_NoneRemoved()
+		{
+			List<RecordToken<PalasoTestItem>> results = new List<RecordToken<PalasoTestItem>>();
+
+			results.Add(new RecordToken<PalasoTestItem>(dataMapper, _queryResultsEmpty, new TestRepositoryId(8)));
+			results.Add(new RecordToken<PalasoTestItem>(dataMapper, new TestRepositoryId(12)));
+
+			ResultSet<PalasoTestItem> resultSet = new ResultSet<PalasoTestItem>(dataMapper, results);
+			resultSet.Coalesce("string", delegate(object o)
+											 {
+												 return string.IsNullOrEmpty((string)o);
+											 });
+
+			Assert.AreEqual(2, resultSet.Count);
+		}
+
+		[Test]
+		public void Coalesce_ItemThatCanBeRemovedItemThatCannot_ItemRemoved()
+		{
+			List<RecordToken<PalasoTestItem>> results = new List<RecordToken<PalasoTestItem>>();
+
+			results.Add(new RecordToken<PalasoTestItem>(dataMapper, _queryResultsEmpty, new TestRepositoryId(8)));
+			results.Add(new RecordToken<PalasoTestItem>(dataMapper, new TestRepositoryId(12)));
+			results.Add(new RecordToken<PalasoTestItem>(dataMapper, _queryResultsA, new TestRepositoryId(8)));
+			results.Add(new RecordToken<PalasoTestItem>(dataMapper, _queryResultsB, new TestRepositoryId(12)));
+
+			ResultSet<PalasoTestItem> resultSet = new ResultSet<PalasoTestItem>(dataMapper, results);
+			resultSet.Coalesce("string", delegate(object o)
+											 {
+												 return string.IsNullOrEmpty((string)o);
+											 });
+
+			Assert.AreEqual(2, resultSet.Count);
+
+		}
+
+		[Test]
+		public void Coalesce_ItemsCanBeRemoved_DoesNotChangeOrder()
+		{
+			List<RecordToken<PalasoTestItem>> results = new List<RecordToken<PalasoTestItem>>();
+
+			results.Add(new RecordToken<PalasoTestItem>(dataMapper, new TestRepositoryId(12)));
+			results.Add(new RecordToken<PalasoTestItem>(dataMapper, _queryResultsEmpty, new TestRepositoryId(8)));
+			results.Add(new RecordToken<PalasoTestItem>(dataMapper, _queryResultsB, new TestRepositoryId(12)));
+			results.Add(new RecordToken<PalasoTestItem>(dataMapper, _queryResultsA, new TestRepositoryId(8)));
+
+			ResultSet<PalasoTestItem> resultSet = new ResultSet<PalasoTestItem>(dataMapper, results);
+			resultSet.Coalesce("string", delegate(object o)
+											 {
+												 return string.IsNullOrEmpty((string)o);
+											 });
+
+			Assert.AreEqual("B", resultSet[0]["string"]);
+			Assert.AreEqual("A", resultSet[1]["string"]);
+
+		}
+
+	}
+}
