2201188e9f61eda7200c6ec66fbcf9f3e35ebd9d
John Hatton <hattonjohn@gmail.com>
2009-09-07 16:26:25 +1000

    Attempt at: WS-14959 (hatton) [linux] thumnails are in proper proportion

--- PalasoUIWindowsForms/ImageGallery/ImageUtilities.cs
+++ PalasoUIWindowsForms/ImageGallery/ImageUtilities.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Text;
+
 
 namespace Palaso.UI.WindowsForms.ImageGallery
 {
 	public static class ImageUtilities
 	{
-		public static Image GetThumbNail(string imagePath, int imgWidth, int imgHeight, Color borderColor)
+		public static Image GetThumbNail(string imagePath, int destinationWidth, int destinationHeight, Color borderColor)
 		{
 
 			Bitmap bmp;
@@ -19,36 +17,35 @@ namespace Palaso.UI.WindowsForms.ImageGallery
 			}
 			catch
 			{
-				bmp = new Bitmap(imgWidth, imgHeight); //If we cant load the image, create a blank one with ThumbSize
+				bmp = new Bitmap(destinationWidth, destinationHeight); //If we cant load the image, create a blank one with ThumbSize
 			}
 
+			//get the lesser of the desired and original size
+			destinationWidth = bmp.Width > destinationWidth ? destinationWidth : bmp.Width;
+			destinationHeight = bmp.Height > destinationHeight ? destinationHeight : bmp.Height;
 
-			imgWidth = bmp.Width > imgWidth ? imgWidth : bmp.Width;
-			imgHeight = bmp.Height > imgHeight ? imgHeight : bmp.Height;
-
-
-
-			int tnWidth = imgWidth, tnHeight = imgHeight;
+			int actualWidth = destinationWidth;
+			int actualHeight = destinationHeight;
 
 			if (bmp.Width > bmp.Height)
-				tnHeight = (int)(((float)bmp.Height / (float)bmp.Width) * tnWidth);
+				actualHeight = (int)(((float)bmp.Height / (float)bmp.Width) * actualWidth);
 			else if (bmp.Width < bmp.Height)
-				tnWidth = (int)(((float)bmp.Width / (float)bmp.Height) * tnHeight);
+				actualWidth = (int)(((float)bmp.Width / (float)bmp.Height) * actualHeight);
 
-			int iLeft = (imgWidth / 2) - (tnWidth / 2);
-			int iTop = (imgHeight / 2) - (tnHeight / 2);
+			int horizontalOffset = (destinationWidth / 2) - (actualWidth / 2);
+			int verticalOffset = (destinationHeight / 2) - (actualHeight / 2);
 
 #if MONO
-			Image x = bmp.GetThumbnailImage(imgWidth, imgHeight, callbackOnAbort, IntPtr.Zero);
+			Image x = bmp.GetThumbnailImage(destinationWidth, destinationHeight, callbackOnAbort, IntPtr.Zero);
 			return x;
 #else
 
-			Bitmap retBmp = new Bitmap(imgWidth, imgHeight, System.Drawing.Imaging.PixelFormat.Format64bppPArgb);
+			Bitmap retBmp = new Bitmap(destinationWidth, destinationHeight, System.Drawing.Imaging.PixelFormat.Format64bppPArgb);
 			Graphics grp = Graphics.FromImage(retBmp);
 			grp.PixelOffsetMode = PixelOffsetMode.None;
 			grp.InterpolationMode = InterpolationMode.HighQualityBicubic;
 
-			grp.DrawImage(bmp, iLeft, iTop, tnWidth, tnHeight);
+			grp.DrawImage(bmp, horizontalOffset, verticalOffset, actualWidth, actualHeight);
 
 			Pen pn = new Pen(borderColor, 1); //Color.Wheat
 
