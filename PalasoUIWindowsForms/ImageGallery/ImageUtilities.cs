2c70d573018f3c11866de997d0150d9248c66bdd
John Hatton <hattonjohn@gmail.com>
2009-09-10 14:42:17 +1000

    A second try at thumbnails on mono (1st try didn't scale them proportionally) (somehow rebase lost my first attempt)

--- PalasoUIWindowsForms/ImageGallery/ImageUtilities.cs
+++ PalasoUIWindowsForms/ImageGallery/ImageUtilities.cs
@@ -36,8 +36,25 @@ namespace Palaso.UI.WindowsForms.ImageGallery
 			int verticalOffset = (destinationHeight / 2) - (actualHeight / 2);
 
 #if MONO
-			Image x = bmp.GetThumbnailImage(destinationWidth, destinationHeight, callbackOnAbort, System.IntPtr.Zero);
-			return x;
+//    this worked but didn't incorporate the offsets, so when it went back to the caller, it got displayed
+//            out of proportion.
+//            Image x = bmp.GetThumbnailImage(destinationWidth, destinationHeight, callbackOnAbort, System.IntPtr.Zero);
+//            return x;
+
+
+			Bitmap retBmp = new Bitmap(destinationWidth, destinationHeight, System.Drawing.Imaging.PixelFormat.Format64bppPArgb);
+			Graphics grp = Graphics.FromImage(retBmp);
+			grp.PixelOffsetMode = PixelOffsetMode.None;
+		 //guessing that this is the problem?   grp.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+			grp.DrawImage(bmp, horizontalOffset, verticalOffset, actualWidth, actualHeight);
+
+//            Pen pn = new Pen(borderColor, 1); //Color.Wheat
+//
+//
+//            grp.DrawRectangle(pn, 0, 0, retBmp.Width - 1, retBmp.Height - 1);
+
+			return retBmp;
 #else
 
 			Bitmap retBmp = new Bitmap(destinationWidth, destinationHeight, System.Drawing.Imaging.PixelFormat.Format64bppPArgb);
