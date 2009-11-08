72d804cb05299a442bf48c8a151143ca0ed3d846
John Hatton <hattonjohn@gmail.com>
2009-11-08 11:47:09 +1000

    Added stringExtensions to help withn WS-15019 (dangerous unicode characters in xml).

--- Palaso/Extensions/StringExtensions.cs
+++ Palaso/Extensions/StringExtensions.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Xml;
 
 namespace Palaso.Extensions
 {
@@ -24,6 +25,38 @@ namespace Palaso.Extensions
 			return r;
 		}
 
+
+		private static XmlNode _xmlNodeUsedForEscaping;
+		public static string EscapeAnyUnicodeCharactersIllegalInXml(this string text)
+		{
+			//we actually want to preserve html markup, just escape the disallowed unicode characters
+			text = text.Replace("<", "_lt;");
+			text = text.Replace(">", "_gt;");
+			text = text.Replace("&", "_amp;");
+			text = text.Replace("\"", "_quot;");
+			text = text.Replace("'", "_apos;");
+
+			text = EscapeSoXmlSeesAsPureTextAndEscapeCharactersIllegalInXml(text);
+			//put it back, now
+			text = text.Replace("_lt;", "<");
+			text = text.Replace("_gt;", ">");
+			text = text.Replace("_amp;", "&");
+			text = text.Replace("_quot;", "\"");
+			text = text.Replace("_apos;", "'");
+			return text;
+		}
+		public static string EscapeSoXmlSeesAsPureTextAndEscapeCharactersIllegalInXml(this string text)
+		{
+			if (_xmlNodeUsedForEscaping == null)//notice, this is only done once per run
+			{
+				XmlDocument doc = new XmlDocument();
+				_xmlNodeUsedForEscaping = doc.CreateElement("text", "x", "");
+			}
+
+			_xmlNodeUsedForEscaping.InnerText = text;
+			text = _xmlNodeUsedForEscaping.InnerXml;
+			return text;
+		}
 
 	}
 }
