18e5bb5fe5516d4f5938d417fea8b091d0f25518
John Hatton <hattonjohn@gmail.com>
2010-05-04 13:34:18 +1000

    Add File checks to RequireThat, so you can now say, e.g., RequireThat.File.Exists()

--- Palaso/Code/RequireThat.cs
+++ Palaso/Code/RequireThat.cs
@@ -1,9 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Palaso.Code
 {
+	/// <summary>
+	/// Allows statements like:
+	/// 	RequireThat.Directory(dir).Exists();
+	///		RequireThat.Directory(dir).DoesNotExist();
+	///		RequireThat.File(path).DoesNotExist();
+	///		RequireThat.File(path).Directory.Exists();
+	/// </summary>
 	public static class RequireThat
 	{
 		public static DirectoryChecker Directory(string directory)
@@ -34,6 +42,44 @@ namespace Palaso.Code
 			public DirectoryChecker Parent()
 			{
 				return new DirectoryChecker(System.IO.Directory.GetParent(_directory).FullName);
+			}
+		}
+
+		public static FileChecker File(string path)
+		{
+			return new FileChecker(path);
+		}
+
+		public class FileChecker
+		{
+			private readonly string _path;
+
+			public FileChecker(string path)
+			{
+				_path = path;
+			}
+
+			public void Exists()
+			{
+				if (!System.IO.File.Exists(_path))
+					throw new ArgumentException("The file '" + _path + "' does not exist.");
+			}
+			public void DoesNotExist()
+			{
+				if (System.IO.File.Exists(_path))
+					throw new ArgumentException("The file '" + _path + "' must not already exist.");
+			}
+
+			/// <summary>
+			/// Allows us to say RequireThat.File("foo/blah.txt").Directory.Exists();
+			/// </summary>
+			/// <returns></returns>
+			public DirectoryChecker Directory
+			{
+				get
+				{
+					return new DirectoryChecker(Path.GetDirectoryName(_path));
+				}
 			}
 		}
 	}
