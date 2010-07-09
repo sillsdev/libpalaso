42d1e64bcca42f206abe19081a894d47d0f46cc1
John Hatton <hattonjohn@gmail.com>
2010-07-09 11:11:04 +1000

    BuildTask: FileUpdate now can update the current date inside the file.

--- Palaso.MSBuildTasks/FileUpdate.cs
+++ Palaso.MSBuildTasks/FileUpdate.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
@@ -16,11 +17,20 @@ namespace Palaso.BuildTasks
 		[Required]
 		public string ReplacementText { get; set; }
 
+		/// <summary>
+		/// The string pattern to replace with the current date (UTC, dd/MMM/yyyy)
+		/// </summary>
+		public string DatePlaceholder { get; set; }
 
 		public override bool Execute()
 		{
 			var content = System.IO.File.ReadAllText(File);
 			var newContents = System.Text.RegularExpressions.Regex.Replace(content, this.Regex, ReplacementText);
+
+			if(!string.IsNullOrEmpty(DatePlaceholder))
+			{
+				newContents = newContents.Replace(DatePlaceholder, DateTime.UtcNow.Date.ToString("dd/MMM/yyyy"));
+			}
 			if(!newContents.Contains(ReplacementText))
 			{
 				SafeLogError("Did not manage to replace '{0}' with '{1}'", this.Regex, ReplacementText);
