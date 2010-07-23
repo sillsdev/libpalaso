851fcbd2efdaba2cf2bac8b2ca13dd7337f28814
Tim Armstrong <tim_armstrong@sil.org>
2010-07-23 11:53:32 +0700

    Created SortedListAllowsDuplicates Type. This was necassary because all sorted lists that the .Net library provides don't allow duplicates. This makes writing queries in WeSay very tricky as you have to guaantee that every record token is unique even when you don't care that it is.

---
+++ Palaso.Tests/Data/SortedListAllowsDuplicatesTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Palaso.Data;
+
+namespace Palaso.Tests.Data
+{
+	[TestFixture]
+	public class SortedListAllowsDuplicatesTests
+	{
+
+		[Test]
+		public void Add_ListIsEmpty_ItemIsAddedToEnd()
+		{
+			SortedListAllowsDuplicates<string> sortedList= new SortedListAllowsDuplicates<string>(new StringLengthComparer());
+			sortedList.Add("bb");
+			Assert.AreEqual("bb", sortedList[0]);
+		}
+
+		[Test]
+		public void Add_ListHasOneElementAndNewGreaterElementIsAdded_ItemIsAddedAfter()
+		{
+			SortedListAllowsDuplicates<string> sortedList= new SortedListAllowsDuplicates<string>(new StringLengthComparer());
+			sortedList.Add("bb");
+			sortedList.Add("aaa");
+			Assert.AreEqual("bb", sortedList[0]);
+			Assert.AreEqual("aaa", sortedList[1]);
+		}
+
+		[Test]
+		public void Add_ListHasOneElementAndNewLesserElementIsAdded_ItemIsAddedBefore()
+		{
+			SortedListAllowsDuplicates<string> sortedList= new SortedListAllowsDuplicates<string>(new StringLengthComparer());
+			sortedList.Add("bb");
+			sortedList.Add("c");
+			Assert.AreEqual("c", sortedList[0]);
+			Assert.AreEqual("bb", sortedList[1]);
+		}
+
+		[Test]
+		public void Add_ListHasTwoElementsAndNewLesserGreaterElementIsAdded_ItemIsAddedInBetween()
+		{
+			SortedListAllowsDuplicates<string> sortedList= new SortedListAllowsDuplicates<string>(new StringLengthComparer());
+			sortedList.Add("c");
+			sortedList.Add("aaa");
+			sortedList.Add("bb");
+			Assert.AreEqual("c", sortedList[0]);
+			Assert.AreEqual("bb", sortedList[1]);
+			Assert.AreEqual("aaa", sortedList[2]);
+		}
+
+		[Test]
+		public void Add_ListHasThreeElementsAndNewDuplicateElementIsAdded_ItemIsAddedAdjacentToDuplicate()
+		{
+			SortedListAllowsDuplicates<string> sortedList= new SortedListAllowsDuplicates<string>(new StringLengthComparer());
+			sortedList.Add("c");
+			sortedList.Add("aaa");
+			sortedList.Add("bb");
+			sortedList.Add("bb");
+			Assert.AreEqual("c", sortedList[0]);
+			Assert.AreEqual("bb", sortedList[1]);
+			Assert.AreEqual("bb", sortedList[2]);
+			Assert.AreEqual("aaa", sortedList[3]);
+		}
+
+		private class StringLengthComparer:IComparer<string>
+		{
+			public int Compare(string x, string y)
+			{
+				if (x.Length < y.Length) return -1;
+				if (x.Length > y.Length) return 1;
+				else return 0;
+			}
+		}
+	}
+}
---
+++ Palaso/Data/SortedListAllowsDuplicates.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Palaso.Data
+{
+	public class SortedListAllowsDuplicates<T>:List<T>
+	{
+		private IComparer<T> _comparer;
+
+		public SortedListAllowsDuplicates(IComparer<T> comparer)
+		{
+			_comparer = comparer;
+		}
+
+		public SortedListAllowsDuplicates()
+		{
+			_comparer = Comparer<T>.Default;
+		}
+
+		public void Add(T i)
+		{
+			bool listIsEmpty = (Count == 0);
+			if (listIsEmpty)
+			{
+				base.Add(i);
+				return;
+			}
+			bool itemToInsertIsGreaterThanLastitemInList =
+				(_comparer.Compare(i, base[Count - 1]) > 0);
+			if (itemToInsertIsGreaterThanLastitemInList)
+			{
+				base.Add(i);
+				return;
+			}
+			int index = BinarySearch(i, _comparer);
+			bool identicalItemAlreadyExistsInList = (index >= 0);
+			if (identicalItemAlreadyExistsInList)
+			{
+				Insert(index + 1 , i);
+			}
+			else
+			{
+				int ind = ~index;
+				Insert(ind, i);
+			}
+		}
+	}
+}
