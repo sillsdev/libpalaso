406924cbaad3158efa5f1398dddb90f598db7277
Tim Armstrong <tim_armstrong@sil.org>
2010-07-28 14:06:37 +0700

    Added GlossQuery... no tests yet

---
+++ Palaso.DictionaryServices/Queries/GlossQuery.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Palaso.Data;
+using Palaso.DictionaryServices.Model;
+using Palaso.WritingSystems;
+
+namespace Palaso.DictionaryServices.Queries
+{
+	class GlossQuery : IQuery<LexEntry>
+	{
+		private IComparer _comparer;
+		private WritingSystemDefinition _writingSystemDefinition;
+
+		public GlossQuery(Comparer<Guid> guidComparer, WritingSystemDefinition wsDef)
+		{
+			_comparer = guidComparer;
+			_writingSystemDefinition = wsDef;
+		}
+
+		public GlossQuery(WritingSystemDefinition writingSystemDefinition)
+		{
+			_writingSystemDefinition = writingSystemDefinition;
+			_comparer = writingSystemDefinition.Collator;
+		}
+
+		public override IEnumerable<IDictionary<string, object>> GetResults(LexEntry entryToQuery)
+		{
+
+			var fieldsandValuesForRecordTokens = new List<IDictionary<string, object>>();
+
+			foreach (LexSense sense in entryToQuery.Senses)
+			{
+				var rawGloss = sense.Gloss[_writingSystemDefinition.Id];
+				var glosses = GetTrimmedElementsSeperatedBySemiColon(rawGloss);
+				foreach (string gloss in glosses)
+				{
+					IDictionary<string, object> tokenFieldsAndValues = new Dictionary<string, object>();
+					tokenFieldsAndValues.Add("Gloss", gloss);
+					fieldsandValuesForRecordTokens.Add(tokenFieldsAndValues);
+				}
+			}
+
+			return fieldsandValuesForRecordTokens;
+		}
+
+
+
+		private static List<string> GetTrimmedElementsSeperatedBySemiColon(string text)
+		{
+			var textElements = new List<string>();
+			foreach (string textElement in text.Split(new[] { ';' }))
+			{
+				string textElementTrimmed = textElement.Trim();
+				textElements.Add(textElementTrimmed);
+			}
+			return textElements;
+		}
+
+		public override IEnumerable<SortDefinition> SortDefinitions
+		{
+			get
+			{
+				var sortOrder = new SortDefinition[1];
+				sortOrder[0] = new SortDefinition("Gloss", _comparer);
+				return sortOrder;
+			}
+		}
+
+		public override string UniqueLabel
+		{
+			get { return "GlossQuery"; }
+		}
+	}
+}
