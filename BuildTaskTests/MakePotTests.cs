1df5376e723f4b47ace7a68b3f01813e3045810f
Cambell Prince <cambell_prince@sil.org>
2010-09-14 15:31:05 +0700

    MakePot now scans for Text = and StringCatalog.Get in addition to ~ strings

---
+++ BuildTaskTests/MakePotTests.cs
@@ -0,0 +1,203 @@
+﻿using System.Collections;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.Build.Framework;
+using Palaso.BuildTasks.MakePot;
+using NUnit.Framework;
+using Palaso.TestUtilities;
+
+namespace BuildTaskTests
+{
+	[TestFixture]
+	public class MakePotTests
+	{
+
+		private class MockTaskItem : ITaskItem
+		{
+
+			public MockTaskItem(string filePath)
+			{
+				ItemSpec = filePath;
+			}
+
+			public string GetMetadata(string metadataName)
+			{
+				return "";
+			}
+
+			public void SetMetadata(string metadataName, string metadataValue)
+			{
+			}
+
+			public void RemoveMetadata(string metadataName)
+			{
+			}
+
+			public void CopyMetadataTo(ITaskItem destinationItem)
+			{
+			}
+
+			public IDictionary CloneCustomMetadata()
+			{
+				return null;
+			}
+
+			public string ItemSpec { get; set; }
+
+			public ICollection MetadataNames
+			{
+				get { return null; }
+			}
+
+			public int MetadataCount
+			{
+				get { return 0; }
+			}
+		}
+
+		private class EnvironmentForTest : TemporaryFolder
+		{
+			public EnvironmentForTest() :
+				base("Palaso.BuildTaskTests.MakePotTests")
+			{
+			}
+
+			public static ITaskItem[] CreateTaskItemsForFilePath(string filePath)
+			{
+				var items = new ITaskItem[1];
+				items[0] = new MockTaskItem(filePath);
+				return items;
+			}
+
+		}
+
+		[Test]
+		public void MatchesInCSharpString_StringWithTilde_HasMatch()
+		{
+			string contents = @"
+somevar.MyLocalizableFunction('~MyLocalizableString');
+".Replace("'", "\"");
+
+			var pot = new MakePot();
+			MatchCollection matches = pot.MatchesInCSharpString(contents);
+			Assert.AreEqual(1, matches.Count);
+			foreach (Match match in matches)
+			{
+				Assert.AreEqual(3, match.Groups.Count);
+				Assert.AreEqual("MyLocalizableString", match.Groups["key"].Value);
+			}
+		}
+
+		[Test]
+		public void MatchesInCSharpString_StringWithTildeAndNotes_HasMatchAndNotes()
+		{
+			string contents = @"
+somevar.MyLocalizableFunction('~MyLocalizableString', 'MyTranslationNotes');
+".Replace("'", "\"");
+
+			var pot = new MakePot();
+			MatchCollection matches = pot.MatchesInCSharpString(contents);
+			Assert.AreEqual(1, matches.Count);
+			foreach (Match match in matches)
+			{
+				Assert.AreEqual(3, match.Groups.Count);
+				Assert.AreEqual("MyLocalizableString", match.Groups["key"].Value);
+				Assert.AreEqual("MyTranslationNotes", match.Groups["note"].Value);
+
+			}
+		}
+
+		[Test]
+		public void MatchesInCSharpString_UsingStringCatalogNoTilde_HasMatchAndNotes()
+		{
+			string contents = @"
+somevar.MyLocalizableFunction(StringCatalog.Get('MyLocalizableString', 'MyTranslationNotes'));
+".Replace("'", "\"");
+
+			var pot = new MakePot();
+			MatchCollection matches = pot.MatchesInCSharpString(contents);
+			Assert.AreEqual(1, matches.Count);
+			foreach (Match match in matches)
+			{
+				Assert.AreEqual(3, match.Groups.Count);
+				Assert.AreEqual("MyLocalizableString", match.Groups["key"].Value);
+				Assert.AreEqual("MyTranslationNotes", match.Groups["note"].Value);
+
+			}
+		}
+
+		[Test]
+		public void MatchesInCSharpString_UsingTextEqual_HasMatchAndNotes()
+		{
+			string contents = @"
+somevar.Text = 'MyLocalizableString';
+".Replace("'", "\"");
+
+			var pot = new MakePot();
+			MatchCollection matches = pot.MatchesInCSharpString(contents);
+			Assert.AreEqual(1, matches.Count);
+			foreach (Match match in matches)
+			{
+				Assert.AreEqual(3, match.Groups.Count);
+				Assert.AreEqual("MyLocalizableString", match.Groups["key"].Value);
+
+			}
+		}
+
+		[Test]
+		public void ProcessSrcFile_AllMatches_OutputsGoodPo()
+		{
+			string contents = @"
+somevar.Text = 'FirstLocalizableString';
+
+somevar.MyLocalizableFunction(StringCatalog.Get('SecondLocalizableString', 'SecondNotes'));
+
+somevar.MyLocalizableFunction('~ThirdLocalizableString', 'ThirdNotes');
+
+".Replace("'", "\"");
+
+			string expected =
+@"Project-Id-Version: ''
+Report-Msgid-Bugs-To: ''
+POT-Creation-Date: '.*'
+PO-Revision-Date: '.*'
+Last-Translator: ''
+Language-Team: ''
+MIME-Version: '1.0'
+Content-Type: 'text/plain; charset=UTF-8'
+Content-Transfer-Encoding: '8bit'
+
+#; C:\Users\C\AppData\Local\Temp\Palaso.BuildTaskTests.MakePotTests\csharp.cs
+msgid 'FirstLocalizableString'
+msgstr ''
+
+#; C:\Users\C\AppData\Local\Temp\Palaso.BuildTaskTests.MakePotTests\csharp.cs
+#. SecondNotes
+msgid 'SecondLocalizableString'
+msgstr ''
+
+#; C:\Users\C\AppData\Local\Temp\Palaso.BuildTaskTests.MakePotTests\csharp.cs
+#. ThirdNotes
+msgid 'ThirdLocalizableString'
+msgstr ''".Replace('\'', '"').Replace("\\", "\\\\");
+
+			using (var e = new EnvironmentForTest())
+			{
+				string csharpFilePath = Path.Combine(e.Path, "csharp.cs");
+				File.WriteAllText(csharpFilePath, contents);
+
+				var pot = new MakePot();
+				pot.OutputFile = Path.Combine(e.Path, "output.pot");
+				pot.CSharpFiles = EnvironmentForTest.CreateTaskItemsForFilePath(csharpFilePath);
+				pot.Execute();
+
+				string actual = File.ReadAllText(pot.OutputFile);
+				Assert.That(actual, Is.StringMatching(expected));
+			}
+
+
+		}
+
+
+	}
+}
--- Palaso.MSBuildTasks/MakePot/MakePot.cs
+++ Palaso.MSBuildTasks/MakePot/MakePot.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 using System.Xml;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
@@ -25,6 +26,16 @@ namespace Palaso.BuildTasks.MakePot
 
 		public string XpathToStrings { get; set; }
 
+		private Regex _pattern;
+
+		public MakePot()
+		{
+			//            _pattern = new Regex(@"""~([^""]*)""\s*(,\s*""([^""]*)"")?", RegexOptions.Compiled);
+//            _pattern = new Regex(@"""~(?<key>[^""]*)""(\s*,\s*""(?<note>[^""]*)"")?", RegexOptions.Compiled | RegexOptions.ExplicitCapture);
+			_pattern = new Regex(@"(Text\s*=\s*""|StringCatalog\.Get\(""|""~)(?<key>[^""]*)""(\s*,\s*""(?<note>[^""]*)"")?", RegexOptions.Compiled | RegexOptions.ExplicitCapture);
+			//_pattern = new Regex(@"(StringCatalog\.Get\(""|""~)(?([^""]*))""\s*(,\s*""([^""]*)"")?", RegexOptions.Compiled | RegexOptions.ExplicitCapture);
+		}
+
 		public override bool Execute()
 		{
 			using (StreamWriter writer = File.CreateText(OutputFile))
@@ -44,43 +55,46 @@ namespace Palaso.BuildTasks.MakePot
 					}
 				}
 
-				WritePotHeader(writer);
+				WritePotFile(writer);
 
-				foreach (KeyValuePair<string, List<string>> pair in _entries)
-				{
-					WriteEntry(pair.Key, pair.Value, writer);
-				}
-
-				this.Log.LogMessage(MessageImportance.High, "MakePot wrote " + _entries.Count + " strings to " + OutputFile);
+				LogMessage(MessageImportance.High, "MakePot wrote " + _entries.Count + " strings to " + OutputFile);
 			}
 			return true;
 		}
 
-		private void WritePotHeader(StreamWriter writer)
+		internal void WritePotFile(TextWriter writer)
 		{
-			writer.WriteLine("msgid \"\"");
-			writer.WriteLine("msgstr \"\"");
-			writer.WriteLine("\"Project-Id-Version: {0}\"", ProjectId);
-			writer.WriteLine("\"Report-Msgid-Bugs-To: {0}\"", MsdIdBugsTo);
+			WritePotHeader(writer);
 
-			writer.WriteLine("\"POT-Creation-Date: {0}\"", DateTime.UtcNow.ToString("s"));
-			writer.WriteLine("\"PO-Revision-Date: {0}\"", DateTime.UtcNow.ToString("s"));
-			writer.WriteLine("\"Last-Translator: \"");
-			writer.WriteLine("\"Language-Team: \"");
-			writer.WriteLine("\"MIME-Version: 1.0\"");
-			writer.WriteLine("\"Content-Type: text/plain; charset=UTF-8\"");
-			writer.WriteLine("\"Content-Transfer-Encoding: 8bit\"");
+			foreach (KeyValuePair<string, List<string>> pair in _entries)
+			{
+				WriteEntry(pair.Key, pair.Value, writer);
+			}
+		}
+
+		private void WritePotHeader(TextWriter writer)
+		{
+			writer.WriteLine("Project-Id-Version: \"{0}\"", ProjectId);
+			writer.WriteLine("Report-Msgid-Bugs-To: \"{0}\"", MsdIdBugsTo);
+
+			writer.WriteLine("POT-Creation-Date: \"{0}\"", DateTime.UtcNow.ToString("s"));
+			writer.WriteLine("PO-Revision-Date: \"{0}\"", DateTime.UtcNow.ToString("s"));
+			writer.WriteLine("Last-Translator: \"\"");
+			writer.WriteLine("Language-Team: \"\"");
+			writer.WriteLine("MIME-Version: \"1.0\"");
+			writer.WriteLine("Content-Type: \"text/plain; charset=UTF-8\"");
+			writer.WriteLine("Content-Transfer-Encoding: \"8bit\"");
 		}
 
 		private void ProcessXmlFile(ITaskItem  fileSpec)
 		{
 			if (string.IsNullOrEmpty(XpathToStrings))
 			{
-				this.Log.LogError("You must define XPathToStrings if you include anything in XPathFiles");
+				LogError("You must define XPathToStrings if you include anything in XPathFiles");
 				return;
 			}
-			this.Log.LogMessage("Processing {0}", fileSpec.ItemSpec);
-			XmlDocument doc = new XmlDocument();
+			LogMessage("Processing {0}", fileSpec.ItemSpec);
+			var doc = new XmlDocument();
 			doc.Load(fileSpec.ItemSpec);
 			foreach (XmlNode node in doc.SelectNodes(XpathToStrings))
 			{
@@ -88,50 +102,82 @@ namespace Palaso.BuildTasks.MakePot
 			}
 		}
 
+		private void LogMessage(string message, params object[] args)
+		{
+			LogMessage(MessageImportance.Normal, message, args);
+		}
+
+		private void LogMessage(MessageImportance importance, string message, params object[] args)
+		{
+			try
+			{
+				Log.LogMessage(importance, message, args);
+			}
+			catch (InvalidOperationException)
+			{
+				// Swallow exceptions for testing
+			}
+		}
+
+		private void LogError(string message, params object[] args)
+		{
+			try
+			{
+				Log.LogError(message, args);
+			}
+			catch (InvalidOperationException)
+			{
+				// Swallow exceptions for testing
+			}
+		}
+
 		private void AddStringInstance(string stringToTranslate, string commentsForTranslator)
 		{
 			if (!_entries.ContainsKey(stringToTranslate)) //first time we've encountered this string?
 			{
-				this.Log.LogMessage(MessageImportance.Low, "Found '{0}'", stringToTranslate);
+				LogMessage(MessageImportance.Low, "Found '{0}'", stringToTranslate);
 				_entries.Add(stringToTranslate, new List<string>());
 			}
 			_entries[stringToTranslate].Add(commentsForTranslator);//add this reference
 		}
 
-		private void ProcessSrcFile(string filePath)
+		internal void ProcessSrcFile(string filePath)
 		{
-			this.Log.LogMessage("Processing {0}", filePath);
+			LogMessage("Processing {0}", filePath);
 			string contents = File.ReadAllText(filePath);
-			System.Text.RegularExpressions.Regex pattern =
-				new System.Text.RegularExpressions.Regex(@"""~([^""]*)""\s*(,\s*""(.*)"")?", System.Text.RegularExpressions.RegexOptions.Compiled);
 
-			foreach (System.Text.RegularExpressions.Match match in pattern.Matches(contents))
+			foreach (Match match in MatchesInCSharpString(contents))
 			{
-				string str = match.Groups[1].Value;
+				string str = match.Groups["key"].Value;
 				if (!_entries.ContainsKey(str)) //first time we've encountered this string?
 				{
-					this.Log.LogMessage(MessageImportance.Low, "Found '{0}'", str);
+					this.LogMessage(MessageImportance.Low, "Found '{0}'", str);
 					_entries.Add(str, new List<string>());
 				}
 				string comments = "#; " + filePath;
 
 				//catch the second parameter from calls like this:
-				//            StringCatalog.Get("~Note", "The label for the field showing a note.");
+				//            StringCataGet("~Note", "The label for the field showing a note.");
 
-				if (match.Groups.Count >= 3 && match.Groups[3].Length > 0)
+				if (!String.IsNullOrEmpty(match.Groups["note"].Value))
 				{
-					string comment = match.Groups[3].Value;
-					this.Log.LogMessage(MessageImportance.Low, "  with comment '{0}'", comment);
+					string comment = match.Groups["note"].Value;
+					this.LogMessage(MessageImportance.Low, "  with comment '{0}'", comment);
 					comments += System.Environment.NewLine + "#. " + comment;
 				}
 				_entries[str].Add(comments);//add this reference
 			}
 		}
 
-		private static void WriteEntry(string key, List<string> comments, StreamWriter writer)
+		internal MatchCollection MatchesInCSharpString(string contents)
+		{
+			return _pattern.Matches(contents);
+		}
+
+		private static void WriteEntry(string key, IEnumerable<string> comments, TextWriter writer)
 		{
 			writer.WriteLine("");
-			foreach (string s in comments)
+			foreach (var s in comments)
 			{
 				writer.WriteLine(s);
 			}
