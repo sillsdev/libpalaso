017a06019b2533c27f3d03f157a958fad93f6914
Tim Armstrong <tim_armstrong@sil.org>
2011-01-10 10:10:33 +0700

    Added a writing system validator and corresponding tests.

---
+++ Palaso.Tests/WritingSystems/WritingSystemDefinitionValidatorTests.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Palaso.WritingSystems;
+
+namespace Palaso.Tests.WritingSystems
+{
+	[TestFixture]
+	public class WritingSystemDefinitionValidatorTests
+	{
+		[Test]
+		public void IsValidWritingSystem_IsoIsNotEmpty_ReturnsTrue()
+		{
+			WritingSystemDefinition ws = new WritingSystemDefinition();
+			ws.ISO = "de";
+			Assert.IsTrue(WritingSystemDefinitionValidator.IsValidWritingSystem(ws));
+		}
+
+		[Test]
+		public void IsValidWritingSystem_IsoIsEmpty_ReturnsFalse()
+		{
+			WritingSystemDefinition ws = new WritingSystemDefinition();
+			ws.ISO = String.Empty;
+			Assert.IsFalse(WritingSystemDefinitionValidator.IsValidWritingSystem(ws));
+		}
+
+		[Test]
+		public void IsValidWritingSystem_VariantIndicatesThatWsIsAudioAndScriptIsNotSetCorrectly_ReturnsTrue()
+		{
+			WritingSystemDefinition ws = new WritingSystemDefinition();
+			ws.ISO = "de";
+			ws.Script = "Zxxx";
+			ws.Variant = "x-audio";
+			Assert.IsTrue(WritingSystemDefinitionValidator.IsValidWritingSystem(ws));
+		}
+
+		[Test]
+		public void IsValidWritingSystem_VariantContainsUnderscoreInsteadOfHyphen_WsIsTreatedAsAudioWsAndReturnsFalseDueToBadScriptSubTag()
+		{
+			WritingSystemDefinition ws = new WritingSystemDefinition();
+			ws.ISO = "de";
+			ws.Script = "bogus";
+			ws.Variant = "x_audio";
+			Assert.IsTrue(WritingSystemDefinitionValidator.IsValidWritingSystem(ws));
+		}
+
+		[Test]
+		public void IsValidWritingSystem_VariantContainsCapitalXDashAUDIOAndScriptIsZxxx_WsIsTreatedAsAudioWsAndReturnsFalseDueToBadScriptSubTag()
+		{
+			WritingSystemDefinition ws = new WritingSystemDefinition();
+			ws.ISO = "de";
+			ws.Script = "bogus";
+			ws.Variant = "X-AUDIO";
+			Assert.IsFalse(WritingSystemDefinitionValidator.IsValidWritingSystem(ws));
+		}
+
+		[Test]
+		public void IsValidWritingSystem_VariantIndicatesThatWsIsAudioAndScriptIsCapitalZXXX_ReturnsTrue()
+		{
+			WritingSystemDefinition ws = new WritingSystemDefinition();
+			ws.ISO = "de";
+			ws.Script = "ZXXX";
+			ws.Variant = "x-audio";
+			Assert.IsTrue(WritingSystemDefinitionValidator.IsValidWritingSystem(ws));
+		}
+
+		[Test]
+		public void IsValidWritingSystem_VariantIndicatesThatWsIsAudioAndScriptIsNotZxxx_ReturnsFalse()
+		{
+			WritingSystemDefinition ws = new WritingSystemDefinition();
+			ws.ISO = "de";
+			ws.Script = "ltn";
+			ws.Variant = "x-audio";
+			Assert.IsFalse(WritingSystemDefinitionValidator.IsValidWritingSystem(ws));
+		}
+
+		[Test]
+		public void IsValidWritingSystem_VariantIndicatesThatWsIsAudioButContainsotherThanJustTheNecassaryXDashAudioTagAndScriptIsNotZxxx_WsIsTreatedAsAudioWsAndReturnsFalseDueToBadScriptSubTag()
+		{
+			WritingSystemDefinition ws = new WritingSystemDefinition();
+			ws.ISO = "de";
+			ws.Script = "ltn";
+			ws.Variant = "x-private-x-audio";
+			Assert.IsFalse(WritingSystemDefinitionValidator.IsValidWritingSystem(ws));
+		}
+
+		[Test]
+		public void IsValidWritingSystem_LanguageSubtagContainsXDashAudio_ReturnsFalse()
+		{
+			WritingSystemDefinition ws = new WritingSystemDefinition();
+			ws.ISO = "de-x-audio";
+			Assert.IsFalse(WritingSystemDefinitionValidator.IsValidWritingSystem(ws));
+		}
+
+		[Test]
+		public void IsValidWritingSystem_LanguageContainsZxxx_ReturnsFalse()
+		{
+			WritingSystemDefinition ws = new WritingSystemDefinition();
+			ws.ISO = "de-Zxxx";
+			Assert.IsFalse(WritingSystemDefinitionValidator.IsValidWritingSystem(ws));
+		}
+
+		[Test]
+		public void IsValidWritingSystem_LanguageSubtagContainsCapitalXDashAudio_ReturnsFalse()
+		{
+			WritingSystemDefinition ws = new WritingSystemDefinition();
+			ws.ISO = "de-X-AuDiO";
+			Assert.IsFalse(WritingSystemDefinitionValidator.IsValidWritingSystem(ws));
+		}
+
+		[Test]
+		public void IsValidWritingSystem_LanguageSubtagContainsCapitalZxxx_ReturnsFalse()
+		{
+			WritingSystemDefinition ws = new WritingSystemDefinition();
+			ws.ISO = "de-ZXXX";
+			Assert.IsFalse(WritingSystemDefinitionValidator.IsValidWritingSystem(ws));
+		}
+
+		[Test]
+		public void IsValidWritingSystem_ScriptSubtagContainsOtherThanLegalScripts_ReturnsFalse()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Palaso/WritingSystems/WritingSystemDefinitionValidator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Palaso.WritingSystems
+{
+	public class WritingSystemDefinitionValidator
+	{
+		static private bool ScriptTagIsNonconformantToAudioWritingSystem(WritingSystemDefinition ws)
+		{
+			if (StringsAreEqualAccordingToLdmlSpec("Zxxx", ws.Script)) return false;
+			return true;
+		}
+
+		static private bool VariantTagIndicatesAudioWritingSystem(WritingSystemDefinition ws)
+		{
+			if(StringContainsSubstringAccordingToLdmlSpec(ws.Variant, "x-audio")) return true;
+			return false;
+		}
+
+		private static bool StringsAreEqualAccordingToLdmlSpec(string shouldValue, string isValue)
+		{
+			return String.Equals(shouldValue, isValue,StringComparison.InvariantCultureIgnoreCase); //This comparer should be extended to be "-"/"_" insensitive as well.
+		}
+
+		static private bool LanguageSubTagIsEmpty(WritingSystemDefinition ws)
+		{
+			if(String.IsNullOrEmpty(ws.ISO)) return true;
+			return false;
+		}
+
+		static public bool IsValidWritingSystem(WritingSystemDefinition ws)
+		{
+			if (LanguageSubTagIsEmpty(ws)) return false;
+			if (LanguageSubTagContainsXDashAudio(ws)) return false;
+			if (LanguageSubTagContainsZxxx(ws)) return false;
+			if (VariantTagIndicatesAudioWritingSystem(ws))
+			{
+				if (ScriptTagIsNonconformantToAudioWritingSystem(ws))   return false;
+			}
+			return true;
+		}
+
+		private static bool LanguageSubTagContainsZxxx(WritingSystemDefinition ws)
+		{
+			return StringContainsSubstringAccordingToLdmlSpec(ws.ISO, "Zxxx");
+		}
+
+		private static bool LanguageSubTagContainsXDashAudio(WritingSystemDefinition ws)
+		{
+			return StringContainsSubstringAccordingToLdmlSpec(ws.ISO,"x-audio");
+		}
+
+		private static bool StringContainsSubstringAccordingToLdmlSpec(string stringToSearch, string stringToFind)
+		{
+			int ind = stringToSearch.IndexOf(stringToFind, StringComparison.InvariantCultureIgnoreCase); //This comparer should be extended to be "-"/"_" insensitive as well.
+			return ind == -1 ? false : true;
+		}
+	}
+}
