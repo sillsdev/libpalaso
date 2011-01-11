16d76cbd43256c411ce956135a111a4cb7554913
Tim Armstrong <tim_armstrong@sil.org>
2011-01-11 10:25:38 +0700

    Added an extension method to String to give us a Contains method that takes a StringComparison

---
+++ Palaso/WritingSystems/StringContainsExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Palaso.WritingSystems
+{
+	public static class StringExtension
+	{
+		public static bool Contains(this String stringToSearch, String stringToFind, StringComparison comparison)
+		{
+			int ind = stringToSearch.IndexOf(stringToFind, comparison); //This comparer should be extended to be "-"/"_" insensitive as well.
+			return ind == -1 ? false : true;
+		}
+	}
+}
