5a66c23cbf355c2ee7d741a6c4a7186530610fc6
Tim Armstrong <tim_armstrong@sil.org>
2011-01-11 10:24:35 +0700

    Added LdmlStyleStringComparer... probably won't be as useful as I'd hoped :-(

---
+++ Palaso.Tests/WritingSystems/LdmlStyleStringComparerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Palaso.Tests.WritingSystems
+{
+	[TestFixture]
+	public class LdmlStyleStringComparerTests
+	{
+		[Test]
+		public void Compare_DashAndHyphen_ReturnsZero()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Test]
+		public void Compare_HyphenAndDash_ReturnsZero()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Test]
+		public void Equals_DashAndHyphen_ReturnsTrue()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Test]
+		public void Equals_HyphenAndDash_ReturnsTrue()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Test]
+		public void GetHashCode_HyphenAndDash_ReturnSameValue()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Palaso/WritingSystems/LdmlStyleStringComparer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Palaso.WritingSystems
+{
+	public class LdmlStyleStringComparer:StringComparer
+	{
+		public override int Compare(string x, string y)
+		{
+			x = NormalizeString(x);
+			y = NormalizeString(y);
+			return String.Compare(x, y, StringComparison.OrdinalIgnoreCase);
+		}
+
+		public override bool Equals(string x, string y)
+		{
+			x = NormalizeString(x);
+			y = NormalizeString(y);
+			return String.Equals(x, y, StringComparison.OrdinalIgnoreCase);
+		}
+
+		public override int GetHashCode(string obj)
+		{
+			obj = NormalizeString(obj);
+			return obj.GetHashCode();
+		}
+
+		private string NormalizeString(string stringToNormalize)
+		{
+			string normalizedString;
+			normalizedString = ReplaceUnderScoreWithDash(stringToNormalize);
+			return normalizedString;
+		}
+
+		private string ReplaceUnderScoreWithDash(string stringToNormalize)
+		{
+			return stringToNormalize.Replace('_', '-');
+		}
+	}
+}
