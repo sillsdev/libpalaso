30bcb5da4ecf4e1b12ad52fbc7f7c996fea42c92
Cambell Prince <cambell_prince@sil.org>
2011-01-12 15:42:14 +0700

    PalasoBuildTasks add Archive task for tar on linux

---
+++ BuildTaskTests/ArchiveTests.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using NUnit.Framework;
+using Palaso.BuildTasks.Archive;
+
+namespace BuildTaskTests
+{
+	[TestFixture]
+	public class ArchiveTests
+	{
+		private class EnvironmentForTest : IDisposable
+		{
+			public ITaskItem[] TwoItemsWithBasePath(string a, string b)
+			{
+				var result = new[]
+				{
+					new MockTaskItem(BasePath + a),
+					new MockTaskItem(BasePath + b)
+				};
+				return result;
+			}
+
+			public string BasePath { get { return "/trim/path/"; }}
+
+			public void Dispose()
+			{
+			}
+		}
+
+		[Test]
+		public void ExecutableName_ForTar_Tar()
+		{
+			var task = new Archive();
+			task.Command = "Tar";
+			Assert.AreEqual("tar", task.ExecutableName());
+		}
+
+		[Test]
+		public void ExecutableName_ForUnknown_EmptyString()
+		{
+			var task = new Archive();
+			task.Command = "Unknown";
+			Assert.AreEqual(String.Empty, task.ExecutableName());
+		}
+
+		[Test]
+		public void Arguments_ForTar_CorrectAndIncludeFileName()
+		{
+			var task = new Archive();
+			task.Command = "Tar";
+			task.FileName = "MyOutputFile.tar.gz";
+			Assert.AreEqual("-cvzf MyOutputFile.tar.gz", task.Arguments());
+		}
+
+		[Test]
+		public void Arguments_ForUnknown_EmptyString()
+		{
+			var task = new Archive();
+			task.Command = "Unknown";
+			task.FileName = "MyOutputFile.tar.gz";
+			Assert.AreEqual(String.Empty, task.Arguments());
+		}
+
+		[Test]
+		public void TrimBaseFromFilePath_WithBase_ExcludesBasePath()
+		{
+			var task = new Archive();
+			task.BasePath = "/trim/this/path/";
+			var result = task.TrimBaseFromFilePath("/trim/this/path/myproject/here");
+			Assert.AreEqual("myproject/here", result);
+		}
+
+		[Test]
+		public void TrimBaseFromFilePath_WithBaseNoTrailingSlash_ExcludesBasePath()
+		{
+			var task = new Archive();
+			task.BasePath = "/trim/this/path";
+			var result = task.TrimBaseFromFilePath("/trim/this/path/myproject/here");
+			Assert.AreEqual("myproject/here", result);
+		}
+
+		[Test]
+		public void FlattenFilePaths_TwoItems_TrimsAndStringCorrect()
+		{
+			using (var e = new EnvironmentForTest())
+			{
+				var task = new Archive();
+				task.BasePath = e.BasePath;
+				task.InputFilePaths = e.TwoItemsWithBasePath("a.cs", "b.cs");
+				var result = task.FlattenFilePaths(task.InputFilePaths, ' ', false);
+				Assert.AreEqual("a.cs b.cs", result);
+			}
+		}
+
+		[Test]
+		public void FlattenFilePaths_TwoItemsOneWithSpace_TrimsAndQuotesStringCorrect()
+		{
+			using (var e = new EnvironmentForTest())
+			{
+				var task = new Archive();
+				task.BasePath = e.BasePath;
+				task.InputFilePaths = e.TwoItemsWithBasePath("a space.cs", "b.cs");
+				var result = task.FlattenFilePaths(task.InputFilePaths, ' ', false);
+				Assert.AreEqual("\"a space.cs\" b.cs", result);
+			}
+		}
+
+		[Test]
+		public void FlattenFilePaths_TwoItemsForceQuote_TrimsAndQuotesStringCorrect()
+		{
+			using (var e = new EnvironmentForTest())
+			{
+				var task = new Archive();
+				task.BasePath = e.BasePath;
+				task.InputFilePaths = e.TwoItemsWithBasePath("a.cs", "b.cs");
+				var result = task.FlattenFilePaths(task.InputFilePaths, ' ', true);
+				Assert.AreEqual("\"a.cs\" \"b.cs\"", result);
+			}
+		}
+	}
+}
--- build/TestBuild.sh
+++ build/TestBuild.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xbuild /target:Build /property:Configuration=DebugMono /property:RootDir=..  /property:BUILD_NUMBER="0.0.0.abcd" build.mono.proj
+xbuild "/target:Clean;Compile" /property:Configuration=DebugMono /property:RootDir=..  /property:BUILD_NUMBER="0.0.0.abcd" build.mono.proj
