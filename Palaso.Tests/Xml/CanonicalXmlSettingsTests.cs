959077096775540dd2dc3e78b8fe292af299b835
Cambell Prince <cambell_prince@sil.org>
2011-01-27 14:25:06 +0700

    Added Palaso.Xml.CanonicalXmlSettings

---
+++ Palaso.Tests/Xml/CanonicalXmlSettingsTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using NUnit.Framework;
+using Palaso.Xml;
+
+namespace Palaso.Tests.Xml
+{
+	[TestFixture]
+	public class CanonicalXmlSettingsTests
+	{
+		[Test]
+		public void WriteReadWithBuilder_WithCanonicalXmlWriterSettings_MatchesExpected()
+		{
+			string xmlInput = @"<a attrib1='value1' attrib2='value2'><b>Content</b></a>".Replace('\'', '"');
+			const string expected = "<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<a\r\n\tattrib1=\"value1\"\r\n\tattrib2=\"value2\">\r\n\t<b>Content</b>\r\n</a>";
+			// Note that a StringBuilder forces the stream to be 16 bit, so the output is force to utf-16.
+			var builder = new StringBuilder();
+			using (var reader = XmlReader.Create(new StringReader(xmlInput)))
+			{
+				using (var writer = XmlWriter.Create(builder, CanonicalXmlSettings.CreateXmlWriterSettings()))
+				{
+					writer.WriteNode(reader, false);
+				}
+			}
+			Assert.AreEqual(expected, builder.ToString());
+		}
+
+		[Test]
+		public void WriteReadWithFile_WithCanonicalXmlWriterSettings_MatchesExpected()
+		{
+			string xmlInput = @"<a attrib1='value1' attrib2='value2'><b>Content</b></a>".Replace('\'', '"');
+			const string expected = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n<a\r\n\tattrib1=\"value1\"\r\n\tattrib2=\"value2\">\r\n\t<b>Content</b>\r\n</a>";
+			using (var tempFile = TestUtilities.TempFile.CreateAndGetPathButDontMakeTheFile())
+			{
+				using (var reader = XmlReader.Create(new StringReader(xmlInput)))
+				{
+					using (var writer = XmlWriter.Create(tempFile.Path, CanonicalXmlSettings.CreateXmlWriterSettings()))
+					{
+						writer.WriteNode(reader, false);
+					}
+				}
+				string result = File.ReadAllText(tempFile.Path);
+				Console.WriteLine(result);
+				Assert.AreEqual(expected, result);
+			}
+		}
+	}
+}
---
+++ Palaso/Xml/CanonicalXmlSettings.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace Palaso.Xml
+{
+	///<summary>
+	/// Canonical xml settings suitable for use in Chorus applications.
+	///</summary>
+	public class CanonicalXmlSettings
+	{
+		///<summary>
+		/// Return an XmlWriterSettings suitable for use in Chorus applications.
+		///</summary>
+		/// <remarks>
+		/// This formats with new line on attributes, indents with tab, and encoded in UTF8 with no BOM.
+		/// </remarks>
+		/// <param name="conformanceLevel">Document|Fragment</param>
+		///<returns>XmlWriterSettings</returns>
+		public static XmlWriterSettings CreateXmlWriterSettings(ConformanceLevel conformanceLevel)
+		{
+			var settings = new XmlWriterSettings
+							   {
+								   NewLineOnAttributes = true,              // New line for each attribute, saves space on a typical Chorus changeset.
+								   Indent = true,                           // Indent entities
+								   IndentChars = "\t",                      // Tabs for the indent
+								   CheckCharacters = false,
+								   Encoding = new UTF8Encoding(false),      // UTF8 without a BOM.
+								   CloseOutput = true,                      // Close the underlying stream on Close.  This is not the default.
+								   ConformanceLevel = conformanceLevel,
+								   NewLineChars = "\r\n",                   // Use /r/n for our end of lines
+								   NewLineHandling = NewLineHandling.None,  // Assume that the input is as written /r/n
+								   OmitXmlDeclaration = false               // The default, an xml declaration is written
+							   };
+			return settings;
+		}
+
+		///<summary>
+		///
+		///</summary>
+		///<returns></returns>
+		public static XmlWriterSettings CreateXmlWriterSettings()
+		{
+			return CreateXmlWriterSettings(ConformanceLevel.Document);
+		}
+	}
+}
