2db69154c3e4d63a8f29c73729e717c516a58508
Tim Armstrong <tim_armstrong@sil.org>
2011-02-02 10:34:07 +0700

    Added basic classes and tests for WritingSystemLdmlMigrator And WritingSystemStoreMigrator

---
+++ Palaso.Tests/WritingSystems/Migration/WritingSystemDefinitionLdmlMigratorTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Palaso.WritingSystems;
+using Palaso.WritingSystems.Migration;
+
+namespace Palaso.Tests.WritingSystems.Migration
+{
+	[TestFixture]
+	public class WritingSystemDefinitionLdmlMigratorTests
+	{
+		string _pathToLdml = Path.GetTempFileName();
+
+		[Test]
+		public void MigrateIfNecassary_LanguageSubtagContainsXDashAudio_ResultingWsIsVoice()
+		{
+			var migrator = new WritingSystemDefinitionLdmlMigrator(1, _pathToLdml);
+			migrator.MigrateIfNecassary();
+			var adaptor = new LdmlAdaptor();
+			WritingSystemDefinition migratedWs = GetMigratedWs(adaptor);
+			Assert.IsTrue(migratedWs.IsVoice);
+		}
+
+		private WritingSystemDefinition GetMigratedWs(LdmlAdaptor adaptor)
+		{
+			var migratedWs = new WritingSystemDefinition();
+			adaptor.Read(_pathToLdml, migratedWs);
+			return migratedWs;
+		}
+
+		[Test]
+		public void MigrateIfNecassary_LanguageSubtagContainsIpa_IpaStatusIsSetToIpa()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Test]
+		public void MigrateIfNecassary_LanguageSubtagContainsIpa_IpaIsRemovedFromLanguageSubtag()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Test]
+		public void MigrateIfNecassary_LdmlIsVersion0_IsLatestVersion()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Test]
+		public void MigrateIfNecassary_LdmlIsVersion0_NeedsMigratingIsTrue()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Palaso/WritingSystems/Migration/WritingSystemDefinitionLdmlMigrator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Palaso.Migration;
+
+namespace Palaso.WritingSystems.Migration
+{
+	public class WritingSystemDefinitionLdmlMigrator
+	{
+		private Migrator _migrator;
+
+		public WritingSystemDefinitionLdmlMigrator(string pathToFileToMigrate)
+		{
+			_migrator = new Migrator(WritingSystemDefinition.LatestVersion, pathToFileToMigrate);
+		}
+
+		public WritingSystemDefinitionLdmlMigrator(int versionToMigrateTo, string pathToFileToMigrate)
+		{
+			_migrator = new Migrator(versionToMigrateTo, pathToFileToMigrate);
+		}
+
+		public void MigrateIfNecassary()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
