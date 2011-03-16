98406b4c984ef445bb2edb12f97bd143d97461d9
Tim Armstrong <tim_armstrong@sil.org>
2011-03-16 18:59:38 +0700

    Friendly wrapper class for Single Ldml File Migration

--- Palaso/WritingSystems/Migration/WritingSystemDefinitionLdmlMigrator.cs
+++ Palaso/WritingSystems/Migration/WritingSystemDefinitionLdmlMigrator.cs
@@ -1,46 +1,22 @@
-﻿using Palaso.Migration;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Palaso.Migration;
 using Palaso.WritingSystems.Migration.WritingSystemsLdmlV0To1Migration;
 
 namespace Palaso.WritingSystems.Migration
 {
-	public class WritingSystemDefinitionLdmlMigrator
+	public class WritingSystemDefinitionLdmlMigrator:Migrator
 	{
-		private readonly Migrator _migrator;
-		private readonly int _versionToMigrateTo;
-		private readonly string _pathOfFileToMigrate;
 
 		public WritingSystemDefinitionLdmlMigrator(string pathToFileToMigrate):this(WritingSystemDefinition.LatestWritingSystemDefinitionVersion, pathToFileToMigrate)
 		{
 		}
 
-		public WritingSystemDefinitionLdmlMigrator(int versionToMigrateTo, string pathToFileToMigrate)
+		public WritingSystemDefinitionLdmlMigrator(int versionToMigrateTo, string pathToFileToMigrate):base(versionToMigrateTo, pathToFileToMigrate)
 		{
-			_migrator = new Migrator(versionToMigrateTo, pathToFileToMigrate);
-			_versionToMigrateTo = versionToMigrateTo;
-			_pathOfFileToMigrate = pathToFileToMigrate;
-			_migrator.AddVersionStrategy(new WritingSystemLdmlVersionGetter());
-			_migrator.AddMigrationStrategy(new Version0Migrator());
-		}
-
-		public void MigrateIfNecassary()
-		{
-			if(FileNeedsMigrating)
-			{
-				_migrator.Migrate();
-			}
-		}
-
-		public bool FileNeedsMigrating
-		{
-			get
-			{
-				var versionGetter = new WritingSystemLdmlVersionGetter();
-				if(_versionToMigrateTo == versionGetter.GetFileVersion(_pathOfFileToMigrate))
-				{
-					return false;
-				}
-				return true;
-			}
+			AddVersionStrategy(new WritingSystemLdmlVersionGetter());
+			AddMigrationStrategy(new Version0MigrationStrategy());
 		}
 	}
 }
