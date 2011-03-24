b66b96a872cb98bc21d3b536f3a7947c9b050dbe
John Hatton <hattonjohn@gmail.com>
2011-03-24 11:47:27 +1000

    Tweaking Usage Reporting.

--- Palaso/Reporting/ReportingSettings.cs
+++ Palaso/Reporting/ReportingSettings.cs
@@ -13,24 +13,26 @@ namespace Palaso.Reporting
 		public ReportingSettings()
 		{
 			UserIdentifier = string.Empty;
-			LastLaunchDate = default(DateTime);
+			PreviousLaunchDate = default(DateTime);
 			OkToPingBasicUsageData = true;
 			HaveShowRegistrationDialog = false;
+			PreviousVersion = string.Empty;
 		}
 
-		public int Launches
-		{
-			get;
-			set;
-		}
+		public int Launches { get; set;}
 
 		public string UserIdentifier { get; set; }
 
-		public DateTime LastLaunchDate { get; set; }
+		public DateTime PreviousLaunchDate { get; set; }
 
 		public bool OkToPingBasicUsageData { get; set; }
 
 		public bool HaveShowRegistrationDialog { get; set; }
+
+		/// <summary>
+		/// help notice that someone upgraded
+		/// </summary>
+		public string PreviousVersion { get; set; }
 
 
 	}
