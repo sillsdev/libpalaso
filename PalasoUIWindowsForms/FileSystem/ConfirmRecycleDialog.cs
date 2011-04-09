c5ae2786363d712e12cca674170b687ff34cc270
John Hatton <hattonjohn@gmail.com>
2011-04-09 11:35:41 +1000

    Add actual recycling to ConfirmRecycleDialog

--- PalasoUIWindowsForms/FileSystem/ConfirmRecycleDialog.cs
+++ PalasoUIWindowsForms/FileSystem/ConfirmRecycleDialog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace Palaso.UI.WindowsForms.FileSystem
@@ -42,5 +43,88 @@ namespace Palaso.UI.WindowsForms.FileSystem
 			_messageLabel.Text = string.Format(_messageLabel.Text, LabelForThingBeingDeleted);
 		}
 
+
+		public static bool JustConfirm(string labelForThingBeingDeleted)
+		{
+			using (var dlg = new ConfirmRecycleDialog(labelForThingBeingDeleted))
+			{
+				return DialogResult.OK == dlg.ShowDialog();
+			}
+		}
+
+		 public static bool ConfirmThenRecycle(string labelForThingBeingDeleted, string pathToRecycle)
+		 {
+			 using (var dlg = new ConfirmRecycleDialog(labelForThingBeingDeleted))
+			 {
+				 if (DialogResult.OK != dlg.ShowDialog())
+					 return false;
+			 }
+			 return Recycle(pathToRecycle);
+		 }
+
+		/// <summary>
+		/// Actually do the move of a file/directory to the recycleBin
+		/// </summary>
+		/// <param name="path"></param>
+		/// <returns>true if it succeed.</returns>
+		public static bool Recycle(string path)
+		{
+			try
+			{
+
+			   #if MONO
+					// TODO: Find a way in Mono to send something to the recycle bin.
+									Directory.Delete(item.FolderPath);
+									return true;
+				#else
+
+				//alternative using visual basic dll:  FileSystem.DeleteDirectory(item.FolderPath,UIOption.OnlyErrorDialogs), RecycleOption.SendToRecycleBin);
+
+				//moves it to the recyle bin
+				var shf = new SHFILEOPSTRUCT();
+				shf.wFunc = FO_DELETE;
+				shf.fFlags = FOF_ALLOWUNDO | FOF_NOCONFIRMATION;
+				string pathWith2Nulls = path + "\0\0";
+				shf.pFrom = pathWith2Nulls;
+
+				SHFileOperation(ref shf);
+				return !shf.fAnyOperationsAborted;
+				#endif
+
+			}
+			catch (Exception exception)
+			{
+				Palaso.Reporting.ErrorReport.NotifyUserOfProblem(exception, "Could not delete that book.");
+				return false;
+			}
+		}
+
+#if !MONO
+		[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto, Pack = 1)]
+		public struct SHFILEOPSTRUCT
+		{
+			public IntPtr hwnd;
+			[MarshalAs(UnmanagedType.U4)]
+			public int wFunc;
+			public string pFrom;
+			public string pTo;
+			public short fFlags;
+			[MarshalAs(UnmanagedType.Bool)]
+			public bool fAnyOperationsAborted;
+			public IntPtr hNameMappings;
+			public string lpszProgressTitle;
+		}
+
+		[DllImport("shell32.dll", CharSet = CharSet.Auto)]
+		public static extern int SHFileOperation(ref SHFILEOPSTRUCT FileOp);
+
+		public const int FO_DELETE = 3;
+		public const int FOF_ALLOWUNDO = 0x40;
+		public const int FOF_NOCONFIRMATION = 0x10; // Don't prompt the user
+		public const int FOF_SIMPLEPROGRESS = 0x0100;
+#endif
 	}
+
+
+
 }
