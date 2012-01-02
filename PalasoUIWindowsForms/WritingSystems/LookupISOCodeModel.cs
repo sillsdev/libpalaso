a34c7a5d3afc10b3716aacf9c6b2874bf3178787
John Hatton <hattonjohn@gmail.com>
2012-01-02 14:57:21 -0700

    Add LookupIsoCodeModel.GetExactLanguageMatch()

--- PalasoUIWindowsForms/WritingSystems/LookupISOCodeModel.cs
+++ PalasoUIWindowsForms/WritingSystems/LookupISOCodeModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Palaso.WritingSystems;
 
 namespace Palaso.UI.WindowsForms.WritingSystems
@@ -13,6 +14,19 @@ namespace Palaso.UI.WindowsForms.WritingSystems
 		public LookupIsoCodeModel()
 		{
 			_languageCodes = StandardTags.ValidIso639LanguageCodes;
+		}
+
+		/// <summary>
+		/// Maybe this doesn't belong here... using it in Bloom which is more concerned with language than writing system
+		/// </summary>
+		/// <param name="iso639Code"></param>
+		/// <returns></returns>
+		public Iso639LanguageCode GetExactLanguageMatch(string iso639Code)
+		{
+			iso639Code = iso639Code.ToLowerInvariant();
+			return _languageCodes.FirstOrDefault(
+				code => code.Code.ToLowerInvariant() == iso639Code
+				);
 		}
 
 		public IEnumerable<Iso639LanguageCode> GetMatchingWritingSystems(string typedText)
