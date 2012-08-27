d1c9003c58e02b18692355e0e18bcc49042a1a38
Tim Armstrong <tim_armstrong@sil.org>
2012-08-27 16:38:07 +0700

    Beautified the Conflation dialog

--- PalasoUIWindowsForms/WritingSystems/ConflateWritingSystemsDialog.Designer.cs
+++ PalasoUIWindowsForms/WritingSystems/ConflateWritingSystemsDialog.Designer.cs
@@ -28,7 +28,6 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConflateWritingSystemsDialog));
 			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
 			this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
 			this._wsLabel = new System.Windows.Forms.Label();
@@ -57,7 +56,7 @@
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.tableLayoutPanel1.Size = new System.Drawing.Size(326, 133);
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(327, 107);
 			this.tableLayoutPanel1.TabIndex = 0;
 			//
 			// tableLayoutPanel2
@@ -72,11 +71,11 @@
 			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
 			this.tableLayoutPanel2.Controls.Add(this._wsLabel, 0, 0);
 			this.tableLayoutPanel2.Controls.Add(this._wsSelectionComboBox, 1, 0);
-			this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 68);
+			this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 42);
 			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
 			this.tableLayoutPanel2.RowCount = 1;
 			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-			this.tableLayoutPanel2.Size = new System.Drawing.Size(320, 27);
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(321, 27);
 			this.tableLayoutPanel2.TabIndex = 0;
 			//
 			// _wsLabel
@@ -86,16 +85,16 @@
 			this._wsLabel.AutoSize = true;
 			this._wsLabel.Location = new System.Drawing.Point(3, 0);
 			this._wsLabel.Name = "_wsLabel";
-			this._wsLabel.Size = new System.Drawing.Size(78, 27);
+			this._wsLabel.Size = new System.Drawing.Size(79, 27);
 			this._wsLabel.TabIndex = 0;
-			this._wsLabel.Text = "Conflate {0} to:";
+			this._wsLabel.Text = "Merge {0} with:";
 			this._wsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			//
 			// _wsSelectionComboBox
 			//
 			this._wsSelectionComboBox.Dock = System.Windows.Forms.DockStyle.Fill;
 			this._wsSelectionComboBox.FormattingEnabled = true;
-			this._wsSelectionComboBox.Location = new System.Drawing.Point(87, 3);
+			this._wsSelectionComboBox.Location = new System.Drawing.Point(88, 3);
 			this._wsSelectionComboBox.Name = "_wsSelectionComboBox";
 			this._wsSelectionComboBox.Size = new System.Drawing.Size(230, 21);
 			this._wsSelectionComboBox.TabIndex = 2;
@@ -105,21 +104,23 @@
 			this.tableLayoutPanel3.AutoSize = true;
 			this.tableLayoutPanel3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
 			this.tableLayoutPanel3.ColumnCount = 2;
-			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
 			this.tableLayoutPanel3.Controls.Add(this._cancelButton, 1, 0);
 			this.tableLayoutPanel3.Controls.Add(this._okButton, 0, 0);
 			this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 101);
+			this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 75);
 			this.tableLayoutPanel3.Name = "tableLayoutPanel3";
 			this.tableLayoutPanel3.RowCount = 1;
-			this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-			this.tableLayoutPanel3.Size = new System.Drawing.Size(320, 29);
+			this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel3.Size = new System.Drawing.Size(321, 29);
 			this.tableLayoutPanel3.TabIndex = 1;
 			//
 			// _cancelButton
 			//
-			this._cancelButton.Location = new System.Drawing.Point(163, 3);
+			this._cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this._cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this._cancelButton.Location = new System.Drawing.Point(243, 3);
 			this._cancelButton.Name = "_cancelButton";
 			this._cancelButton.Size = new System.Drawing.Size(75, 23);
 			this._cancelButton.TabIndex = 0;
@@ -129,11 +130,12 @@
 			// _okButton
 			//
 			this._okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this._okButton.Location = new System.Drawing.Point(82, 3);
+			this._okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this._okButton.Location = new System.Drawing.Point(162, 3);
 			this._okButton.Name = "_okButton";
 			this._okButton.Size = new System.Drawing.Size(75, 23);
 			this._okButton.TabIndex = 1;
-			this._okButton.Text = "OK";
+			this._okButton.Text = "&Merge";
 			this._okButton.UseVisualStyleBackColor = true;
 			//
 			// _infoTextLabel
@@ -142,20 +144,24 @@
 			this._infoTextLabel.Location = new System.Drawing.Point(3, 0);
 			this._infoTextLabel.MaximumSize = new System.Drawing.Size(320, 0);
 			this._infoTextLabel.Name = "_infoTextLabel";
-			this._infoTextLabel.Size = new System.Drawing.Size(320, 65);
+			this._infoTextLabel.Size = new System.Drawing.Size(316, 39);
 			this._infoTextLabel.TabIndex = 2;
-			this._infoTextLabel.Text = resources.GetString("_infoTextLabel.Text");
+			this._infoTextLabel.Text = "The input system {0} is in use in your data and cannot be safely deleted.  Howeve" +
+	"r, if you want to merge the input system {0} with another input sytem, select th" +
+	"at writing system from the list below.";
 			//
 			// ConflateWritingSystemsDialog
 			//
+			this.AcceptButton = this._okButton;
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.AutoSize = true;
 			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-			this.ClientSize = new System.Drawing.Size(358, 160);
+			this.CancelButton = this._cancelButton;
+			this.ClientSize = new System.Drawing.Size(347, 131);
 			this.Controls.Add(this.tableLayoutPanel1);
 			this.Name = "ConflateWritingSystemsDialog";
-			this.Text = "Conflate Writing Systems";
+			this.Text = "Delete Input Systems";
 			this.tableLayoutPanel1.ResumeLayout(false);
 			this.tableLayoutPanel1.PerformLayout();
 			this.tableLayoutPanel2.ResumeLayout(false);
