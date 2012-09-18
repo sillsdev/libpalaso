5ee737e9e4dfa98312ee8cb196acb633770b23f8
Tim Armstrong <tim_armstrong@sil.org>
2012-09-18 14:29:22 +0700

    Updated Annotatable and Annotation classes to be deep clonable

---
+++ Palaso.Tests/Annotations/AnnotatableTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Palaso.Annotations;
+using Palaso.Tests.Code;
+
+namespace Palaso.Tests.Annotations
+{
+	[TestFixture]
+	public class AnnotatableIClonableGenericTests : IClonableGenericTests<Annotatable>
+	{
+		public override Annotatable CreateNewClonable()
+		{
+			return new Annotatable();
+		}
+
+		public override string ExceptionList
+		{
+			get { return ""; }
+		}
+
+		public override Dictionary<Type, object> DefaultValuesForTypes
+		{
+			get
+			{
+				return new Dictionary<Type, object>
+							 {
+								 {typeof(Annotation), new Annotation()}
+							 };
+			}
+		}
+	}
+
+	[TestFixture]
+	public class AnnotationIClonableGenericTests : IClonableGenericTests<Annotation>
+	{
+		public override Annotation CreateNewClonable()
+		{
+			return new Annotation();
+		}
+
+		public override string ExceptionList
+		{
+			get { return ""; }
+		}
+
+		public override Dictionary<Type, object> DefaultValuesForTypes
+		{
+			get
+			{
+				return new Dictionary<Type, object>
+							 {
+								 {typeof(int), 42}
+							 };
+			}
+		}
+	}
+}
--- Palaso/Annotations/Annotation.cs
+++ Palaso/Annotations/Annotation.cs
@@ -1,12 +1,21 @@
+using System;
 using System.Xml.Serialization;
+using Palaso.Code;
+using Palaso.Text;
 
 
 namespace Palaso.Annotations
 {
-	public class Annotatable : IAnnotatable
+	public class Annotatable : IAnnotatable, IClonableGeneric<Annotatable>
 	{
 		private Annotation _annotation;
 
+		public Annotatable(){}
+
+		public Annotatable(Annotatable annotatable)
+		{
+			_annotation = new Annotation(annotatable._annotation);
+		}
 
 		[XmlAttribute("starred")]
 		public bool IsStarred
@@ -32,12 +41,17 @@ namespace Palaso.Annotations
 				}
 			}
 		}
+
+		public Annotatable Clone()
+		{
+			return new Annotatable(this);
+		}
 	}
 
 	/// <summary>
 	/// An annotation is a like a "flag" on a field. You can say, e.g., "I'm not sure about this"
 	/// </summary>
-	public class Annotation
+	public class Annotation: IClonableGeneric<Annotation>
 	{
 		/// <summary>
 		/// 0 means "off".  1 means "starred". In the future, other positive values could correspond to other icon.
@@ -47,6 +61,16 @@ namespace Palaso.Annotations
 		public Annotation()
 		{
 			IsOn = false;
+		}
+
+		public Annotation(Annotation annotation)
+		{
+			_status = annotation._status;
+		}
+
+		public Annotation Clone()
+		{
+			return new Annotation(this);
 		}
 
 		public bool IsOn
@@ -59,6 +83,22 @@ namespace Palaso.Annotations
 			{
 				_status = value?1:0;
 			}
+		}
+
+		public override bool Equals(Object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			if (obj.GetType() != typeof(Annotation)) return false;
+			return Equals((Annotation)obj);
+		}
+
+		public bool Equals(Annotation other)
+		{
+			if (ReferenceEquals(null, other)) return false;
+			if (ReferenceEquals(this, other)) return true;
+			bool statusAreEqual = Equals(other._status, _status);
+			return statusAreEqual;
 		}
 	}
 }
