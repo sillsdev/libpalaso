ae711c3e3ed5000dbc3351ddfb3d9c303db17e7c
Tom Bogle <tom_bogle@sil.org>
2013-08-07 17:11:04 -0400

    Added SIL.Archiving to the solution used by TeamCity & to batch files for making local copies to other dev projects.

--- CopyTo SayMore.bat
+++ CopyTo SayMore.bat
@@ -8,6 +8,10 @@ copy /Y output\debug\Palaso.Media.xml c:\dev\SayMore\lib\dotnet
 copy /Y output\debug\Palaso.Media.pdb c:\dev\SayMore\lib\dotnet
 copy /Y output\debug\PalasoUIWindowsForms.xml c:\dev\SayMore\lib\dotnet
 copy /Y output\debug\PalasoUIWindowsForms.pdb c:\dev\SayMore\lib\dotnet
+copy /Y output\debug\SIL.Archiving.dll c:\dev\SayMore\lib\dotnet
+copy /Y output\debug\SIL.Archiving.xml c:\dev\SayMore\lib\dotnet
+copy /Y output\debug\SIL.Archiving.pdb c:\dev\SayMore\lib\dotnet
+
 copy /Y output\debug\Palaso.BuildTasks.dll c:\dev\SayMore\build
 
 copy /Y c:\dev\SayMore\lib\dotnet\Palaso*.* c:\dev\SayMore\output\debug
