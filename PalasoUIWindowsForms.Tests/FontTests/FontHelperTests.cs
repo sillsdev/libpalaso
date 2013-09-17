255fc29f75e8dfc63f7bf51808b7fff92fbb84ec
Phillip Hopper <phillip_hopper@sil.org>
2013-09-17 11:42:10 -0400

    Fix mono bug in the FontHelper class

--- PalasoUIWindowsForms.Tests/FontTests/FontHelperTests.cs
+++ PalasoUIWindowsForms.Tests/FontTests/FontHelperTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Linq;
 using NUnit.Framework;
 using Palaso.UI.WindowsForms;
 
@@ -22,14 +23,27 @@ namespace PalasoUIWindowsForms.Tests.FontTests
 		}
 
 		[Test]
-		public void MakeFont_FontAndStyle_ValidFont()
+		public void MakeFont_FontName_ValidFont()
 		{
 			Font sourceFont = SystemFonts.DefaultFont;
-
-			Font returnFont = FontHelper.MakeFont(sourceFont, FontStyle.Bold);
-
+			Font returnFont = FontHelper.MakeFont(sourceFont.FontFamily.Name);
 			Assert.AreEqual(sourceFont.FontFamily.Name, returnFont.FontFamily.Name);
-			Assert.AreEqual(FontStyle.Bold, returnFont.Style & FontStyle.Bold);
+		}
+
+		[Test]
+		public void MakeFont_FontNameAndStyle_ValidFont()
+		{
+			// use Times New Roman
+			foreach (var family in FontFamily.Families.Where(family => family.Name == "Times New Roman"))
+			{
+				Font sourceFont = new Font(family, 10f, FontStyle.Regular);
+				Font returnFont = FontHelper.MakeFont(sourceFont, FontStyle.Bold);
+
+				Assert.AreEqual(sourceFont.FontFamily.Name, returnFont.FontFamily.Name);
+				Assert.AreEqual(FontStyle.Bold, returnFont.Style & FontStyle.Bold);
+
+				break;
+			}
 		}
 	}
 }
