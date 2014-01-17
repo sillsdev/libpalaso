a695a52c1daed0504c6c8d1c7108a8a7e1884450
Eberhard Beilharz <eb1@sil.org>
2014-01-17 09:20:43 +0100

    Fix TestBuild script
    Until our other way of managing dependencies gets integrated we
    still need to download the dependencies at least once. Added
    the necessary command to TestBuild.sh.
    
    Change-Id: I6d15a18aa0583f5248530d09472f699d10c45aec

--- build/TestBuild.sh
+++ build/TestBuild.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+cd $(dirname "$0")
+xbuild /t:BeforeBuild /p:Configuration=DebugMono build/Download.targets
 xbuild "/target:Clean;Compile" /property:Configuration=DebugMono /property:RootDir=..  /property:BUILD_NUMBER="0.0.0.abcd" build.mono.proj
