7daf67825f8e2f116d0cba5c38a57087a8adae61
Eberhard Beilharz <eb1@sil.org>
2014-01-27 08:29:50 +0100

    Fix build on Linux
    Change-Id: I5a28ad2d3ae6a310b83213fe45be2cba7b440e2d

--- build/TestBuild.sh
+++ build/TestBuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $(dirname "$0")
-xbuild /t:BeforeBuild /p:Configuration=DebugMono build/Download.targets
+xbuild /t:BeforeBuild /p:Configuration=DebugMono Download.targets
 xbuild "/target:Clean;Compile" /property:Configuration=DebugMono /property:RootDir=..  /property:BUILD_NUMBER="0.0.0.abcd" build.mono.proj
