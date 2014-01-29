13aa5bb5c99d6f51d5c7f443d02f7d251e12ae06
Eberhard Beilharz <eb1@sil.org>
2014-01-29 18:31:23 +0100

    Add GetInvalidOSIndependentFileNameChars method
    On Unix there are more characters valid in file names, but we
    want the result to be identical on both platforms, so we want
    to use the larger invalid Windows list for both platforms.
    This changes moves the method from Bloom to Palaso and renames
    it to Path.GetInvalidOSIndependentFileNameChars().
    
    Change-Id: Ia66459a8f3ee7e18aa9f353ad40a834d90f40943

---
+++ Palaso/IO/Path.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) 2014 SIL International
+// This software is licensed under the MIT License (http://opensource.org/licenses/MIT)
+using System;
+
+namespace Palaso.IO
+{
+	public static class Path
+	{
+
+		// On Unix there are more characters valid in file names, but we
+		// want the result to be identical on both platforms, so we want
+		// to use the larger invalid Windows list for both platforms
+		public static char[] GetInvalidOSIndependentFileNameChars()
+		{
+			return new char[]
+			{
+				'\0',
+				'\u0001',
+				'\u0002',
+				'\u0003',
+				'\u0004',
+				'\u0005',
+				'\u0006',
+				'\a',
+				'\b',
+				'\t',
+				'\n',
+				'\v',
+				'\f',
+				'\r',
+				'\u000e',
+				'\u000f',
+				'\u0010',
+				'\u0011',
+				'\u0012',
+				'\u0013',
+				'\u0014',
+				'\u0015',
+				'\u0016',
+				'\u0017',
+				'\u0018',
+				'\u0019',
+				'\u001a',
+				'\u001b',
+				'\u001c',
+				'\u001d',
+				'\u001e',
+				'\u001f',
+				'"',
+				'<',
+				'>',
+				'|',
+				':',
+				'*',
+				'?',
+				'\\',
+				'/'
+			};
+		}
+	}
+}
+
