307d3ac5da6f77c899d82b233b80c58a679291b0
Hasso Pape <hasso_pape@sil.org>
2014-01-31 14:06:18 -0600

    Build Script Updates

--- GetAndBuildThis.bat
+++ GetAndBuildThis.bat
@@ -8,5 +8,5 @@ IF "%2"=="" (
 	call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 )
 
-hg pull -u --rebase
+git pull --rebase
 msbuild "Palaso VS2010.sln" /verbosity:quiet /maxcpucount /p:Configuration=%BUILD_CONFIG%
