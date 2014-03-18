45e495fc7681e772d0a8437d4076f61c604e7825
Chris Hubbard <chris_hubbard@sil.org>
2014-03-18 17:28:11 -0400

    Allow SourcePackage to be created w/ TestBuild.sh
    Call build/TestBuild.sh Release SourcePackage for linux packaging

--- build/TestBuild.sh
+++ build/TestBuild.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd "$(dirname "$0")"
-
+cd "$(dirname "$0")/.."
+root=$PWD
+cd build
 #If a parameter is defined, then it will be used as the Configuration (defaulting to DebugMono)
-xbuild "/target:Clean;Compile" /property:Configuration="${1-Debug}Mono" /property:RootDir=..  /property:BUILD_NUMBER="0.0.0.abcd" build.mono.proj
+xbuild "/target:${2-Clean;Compile}" /property:Configuration="${1-Debug}Mono" /property:RootDir=$root  /property:BUILD_NUMBER="0.0.0.abcd" build.mono.proj
