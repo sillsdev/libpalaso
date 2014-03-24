0c414d55b8f1425d105ec859e4b3cfc36257b8a8
Tom Bogle <tom_bogle@sil.org>
2014-03-24 17:21:41 -0400

    Made Confirm Recycle dialog localizable.

--- PalasoUIWindowsForms/FileSystem/ConfirmRecycleDialog.cs
+++ PalasoUIWindowsForms/FileSystem/ConfirmRecycleDialog.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using L10NSharp;
 
 namespace Palaso.UI.WindowsForms.FileSystem
 {
@@ -16,10 +17,17 @@ namespace Palaso.UI.WindowsForms.FileSystem
 			_messageLabel.Font = SystemFonts.MessageBoxFont;
 		}
 
-		public ConfirmRecycleDialog(string labelForThingBeingDeleted) : this()
+		public ConfirmRecycleDialog(string labelForThingBeingDeleted, bool multipleItems = false, string localizationManagerId = null)
+			: this()
 		{
+			if (!string.IsNullOrEmpty(localizationManagerId))
+				_L10NSharpExtender.LocalizationManagerId = localizationManagerId;
+
 			LabelForThingBeingDeleted = labelForThingBeingDeleted.Trim();
-			_messageLabel.Text = string.Format(_messageLabel.Text, LabelForThingBeingDeleted);
+			string msgFmt = multipleItems ? LocalizationManager.GetString("DialogBoxes.ConfirmRecycleDialog.MessageForMultipleItems",
+				"{0} will be moved to the Recycle Bin.", 
+				"Param 0 is a description of the things being deleted (e.g., \"The selected files\"") : _messageLabel.Text;
+			_messageLabel.Text = string.Format(msgFmt, LabelForThingBeingDeleted);
 
 			// Sometimes, setting the text in the previous line will force the table layout control
 			// to resize itself accordingly, which will fire its SizeChanged event. However,
@@ -56,17 +64,17 @@ namespace Palaso.UI.WindowsForms.FileSystem
 			Close();
 		}
 
-		public static bool JustConfirm(string labelForThingBeingDeleted)
+		public static bool JustConfirm(string labelForThingBeingDeleted, bool multipleItems = false, string localizationManagerId = null)
 		{
-			using (var dlg = new ConfirmRecycleDialog(labelForThingBeingDeleted))
+			using (var dlg = new ConfirmRecycleDialog(labelForThingBeingDeleted, multipleItems, localizationManagerId))
 			{
 				return DialogResult.OK == dlg.ShowDialog();
 			}
 		}
 
-		public static bool ConfirmThenRecycle(string labelForThingBeingDeleted, string pathToRecycle)
+		public static bool ConfirmThenRecycle(string labelForThingBeingDeleted, string pathToRecycle, bool multipleItems = false, string localizationManagerId = null)
 		{
-			using (var dlg = new ConfirmRecycleDialog(labelForThingBeingDeleted))
+			using (var dlg = new ConfirmRecycleDialog(labelForThingBeingDeleted, multipleItems, localizationManagerId))
 			{
 				if (DialogResult.OK != dlg.ShowDialog())
 					return false;
