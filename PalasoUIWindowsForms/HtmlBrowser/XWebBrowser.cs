b70c6ef650f745fc88c4a0974e3676e80871ca96
jason_naylor <jason_naylor@sil.org>
2014-05-27 14:28:48 -0500

    Add properties to the IWebBrowser interface
    * Add three properties to the XWebBrowser interface to
      enable features used on the browsers in Chorus.
    * Add WinFormsBrowserAdapter handling of those properties.
    * Add todo comments where these features are as yet unimplemented
      in the GeckoFxWebBrowserAdapter
    * Fix DocumentText content loading for Geckofx14

--- PalasoUIWindowsForms/HtmlBrowser/XWebBrowser.cs
+++ PalasoUIWindowsForms/HtmlBrowser/XWebBrowser.cs
@@ -91,6 +91,18 @@ namespace Palaso.UI.WindowsForms.HtmlBrowser
 
 		#region IWebBrowser Members
 
+		public bool AllowNavigation
+		{
+			get { return m_WebBrowserAdapter.AllowNavigation; }
+			set { m_WebBrowserAdapter.AllowNavigation = value; }
+		}
+
+		public bool AllowWebBrowserDrop
+		{
+			get { return m_WebBrowserAdapter.AllowWebBrowserDrop; }
+			set { m_WebBrowserAdapter.AllowWebBrowserDrop = value; }
+		}
+
 		public bool CanGoBack
 		{
 			get { return m_WebBrowserAdapter.CanGoBack; }
@@ -186,6 +198,8 @@ namespace Palaso.UI.WindowsForms.HtmlBrowser
 		{
 			get { return m_WebBrowserAdapter.NativeBrowser; }
 		}
+
+		public bool WebBrowserShortcutsEnabled { get; set; }
 
 		#endregion
 
