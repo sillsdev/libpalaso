c2cd9bffbc18ce7773a56f87a69a9df52ee359f8
Stephen McConnel <stephen_mcconnel@sil.org>
2014-10-09 11:40:16 -0500

    Make USB drive access more robust
    This includes a couple of minor fixes to mitigate random crashes seen
    in testing WeSay Send/Receive via USB drives.

--- Palaso/UsbDrive/Linux/UsbDriveInfoUDisks.cs
+++ Palaso/UsbDrive/Linux/UsbDriveInfoUDisks.cs
@@ -27,9 +27,15 @@ namespace Palaso.UsbDrive.Linux
 			{
 				if (_device.IsMounted)
 				{
-					string mountPath = _device.MountPaths[0];
-					//When a device is present but not mounted. This method will throw an ArgumentException.
-					//In particular this can be the case just after inserting a UsbDevice
+					string mountPath = String.Empty;
+					//When a device is present but not mounted, this method will throw an ArgumentException.
+					//In particular this can be the case just after inserting a UsbDevice.
+					//The loop here tries to mitigate such an occurrence.
+					string[] paths = _device.MountPaths;
+					while (_device.IsMounted && paths == null || paths.Length == 0)
+						paths = _device.MountPaths;
+					if (paths != null && paths.Length > 0)
+						mountPath = paths[0];
 					return new DirectoryInfo(mountPath);
 				}
 				return new DirectoryInfo(String.Empty);
