9b04eabfe4d152616002ddfd96de7b75408fd4f2
John Hatton <hattonjohn@gmail.com>
2014-11-18 19:23:28 -0700

    Make ReplaceFileWithUserInteractionIfNeeded OK with, e.g. "\\localhost\..."

--- Palaso/IO/FileUtils.cs
+++ Palaso/IO/FileUtils.cs
@@ -217,9 +217,12 @@ namespace Palaso.IO
 			{
 				try
 				{
-					if (!PathUtilities.PathsAreOnSameVolume(sourcePath, destinationPath)
+					if (UnsafeForFileReplaceMethod(sourcePath) || UnsafeForFileReplaceMethod(destinationPath)
 						||
-						(!string.IsNullOrEmpty(backupPath) && !PathUtilities.PathsAreOnSameVolume(sourcePath,backupPath)))
+						(!string.IsNullOrEmpty(backupPath) && !PathUtilities.PathsAreOnSameVolume(sourcePath,backupPath))
+						||
+						!PathUtilities.PathsAreOnSameVolume(sourcePath, destinationPath)
+						)
 					{
 						//can't use File.Replace or File.Move across volumes (sigh)
 						if (!string.IsNullOrEmpty(backupPath) && File.Exists(destinationPath))
@@ -264,6 +267,19 @@ namespace Palaso.IO
 			while (!succeeded);
 		}
 
+		// NB: I don't actually know for sure that we can't do the replace on these paths; this dev doesn't
+		// have a network to test on. What I do know is that the code checking to see if they are on the
+		// same drive fails for UNC paths, so this at least gets us past that problem
+		private static bool UnsafeForFileReplaceMethod(string path)
+		{
+#if !MONO
+			return path.StartsWith("//") || path.StartsWith("\\\\");
+#else
+			return false; // we will soon be requesting some testing with networks on Linux; 
+			//as a result of that, we might need to do something here, too. Or maybe not.
+#endif
+		}
+
 		private static void ReportFailedReplacement(string destinationPath, Exception error)
 		{
 			var result = ErrorReport.NotifyUserOfProblem(new ShowAlwaysPolicy(), "Give Up", ErrorResult.No,
@@ -280,6 +296,14 @@ namespace Palaso.IO
 		   [MarshalAs(UnmanagedType.LPTStr)]string lpszLongPath,
 		   [MarshalAs(UnmanagedType.LPTStr)]StringBuilder lpszShortPath,
 		   uint cchBuffer);
+
+		[DllImport("Shlwapi.dll")]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		static extern bool PathIsNetworkPath(String pszPath);
+
+		[DllImport("Shlwapi.dll")]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		static extern bool PathIsUNC(String pszPath);
 #endif
 
 		/// <summary>
