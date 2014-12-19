faa861b120f434d773899641badaa9560ec16fb1
darcywong00 <darcy_wong@sil.org>
2014-12-19 11:58:40 +0700

    Create API to SLDR service
     GetLdmlFile retrieves LDML file from the service
    New SldrTests are ignored until the service can fix the SIL namespace declaration in the LDML files

---
+++ SIL.WritingSystems.Tests/SldrTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using NUnit.Framework;
+
+namespace SIL.WritingSystems.Tests
+{
+	[TestFixture]
+	public class SldrTests
+	{
+		[SetUp]
+		public void SetUp()
+		{
+
+		}
+
+		[Test, Ignore("Run by hand")]
+		public void Get_EmptyFileName_Throws()
+		{
+			string filename = string.Empty;
+			const string bcp47Tag = "en";
+
+			if (File.Exists(filename))
+				File.Delete(filename);
+
+			Assert.Throws<ArgumentException>(
+				() => Sldr.GetLdmlFile(filename, bcp47Tag)
+			);
+		}
+
+		[Test, Ignore("Run by hand")]
+		public void Get_BadFileName_Throws()
+		{
+			const string filename = "c:\badpath";
+			const string bcp47Tag = "en";
+
+			if (File.Exists(filename))
+				File.Delete(filename);
+
+			Assert.Throws<System.Net.WebException>(
+				() => Sldr.GetLdmlFile(filename, bcp47Tag)
+			);
+		}
+
+		[Test, Ignore("Run by hand")]
+		public void Get_BadBcp47Tag_Throws()
+		{
+			const string filename = "c:\\temp\\en.ldml";
+			const string bcp47Tag = "!@#";
+
+			if (File.Exists(filename))
+				File.Delete(filename);
+
+			Assert.Throws<System.Net.WebException>(
+				() => Sldr.GetLdmlFile(filename, bcp47Tag)
+			);
+		}
+
+		// This test should be run on TC.  But ignore for now until LDML service correctly writes the SIL namespace
+		[Test, Ignore("Run by hand")]
+		public void Get_Validate()
+		{
+			const string filename = "c:\\temp\\en_GB.ldml";
+			const string bcp47Tag = "en-GB";
+
+			const string expectedLanguage = "en";
+			const string expectedScript = "Latn";
+			const string expectedTerritory = "GB";
+
+			if (File.Exists(filename))
+				File.Delete(filename);
+
+			Sldr.GetLdmlFile(filename, bcp47Tag);
+
+			// Parse the LDML file
+			XElement element = XElement.Load(filename);
+			XElement identity = element.Descendants("identity").First();
+			Assert.AreEqual((string)identity.Element("language").Attribute("type"), expectedLanguage);
+			Assert.AreEqual((string)identity.Element("script").Attribute("type"), expectedScript);
+			Assert.AreEqual((string)identity.Element("territory").Attribute("type"), expectedTerritory);
+
+			// Verify user ID is generated
+			XElement silIdentityElem = identity.Descendants(Sldr.Sil + "identity").First();
+			Assert.AreEqual(((string) silIdentityElem.Attribute("uid")).Length, 8);
+		}
+	}
+}
---
+++ SIL.WritingSystems/Sldr.cs
@@ -0,0 +1,74 @@
+﻿using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using System;
+using System.Net;
+using System.Web;
+
+namespace SIL.WritingSystems
+{
+	public static class Sldr
+	{
+		public static XNamespace Sil = "urn://www.sil.org/ldml/0.1";
+		private const string SldrRepository = "https://ldml.api.sil.org/";
+
+		// If the user wants to request a new UID, you use "uid=unknown" and that will create a new random identifier
+		public const string UnknownUserId = "unknown";
+
+		/// <summary>
+		/// API request to return an LDML file and save it
+		/// </summary>
+		/// <param name="filename">Full filename to save the requested LDML file</param>
+		/// <param name="bcp47Tag">Current BCP47 tag which is a concatenation of the Language, Script, Region and Variant properties</param>
+		/// <param name="flatten">Currently not supported.  Indicates whether or not you want to include all the data 
+		/// inherited from a more general file.  SLDR currently defaults to true (1)</param>
+		public static void GetLdmlFile(string filename, string bcp47Tag, Boolean flatten = true)
+		{
+			if (String.IsNullOrEmpty(filename))
+			{
+				throw new ArgumentException("filename");
+			}
+			if (String.IsNullOrEmpty(bcp47Tag))
+			{
+				throw new ArgumentException("bcp47Tag");
+			}
+
+			// Random 8-character identifier.  This marks various bits of information as alternatives that have been suggested by that specific user.
+			string userId = UnknownUserId;
+
+			// If the LDML file already exists, attempt to extract userId
+			if (File.Exists(filename))
+			{
+				XElement element = XElement.Load(filename);
+				XElement identityElem = element.Descendants(Sil + "identity").First(); 
+				if (identityElem != null)
+				{
+					userId = (string) identityElem.Attribute("uid") ?? UnknownUserId;
+				}
+			}
+
+			// Concatenate url string
+			string url = string.Format("{0}{1}?uid={2}&flatten={3}", SldrRepository, HttpUtility.UrlEncode(bcp47Tag) , HttpUtility.UrlEncode(userId), Convert.ToInt32(flatten));
+			string tempFilename = filename + ".tmp";
+			try
+			{
+				// Download the LDML file to a temp file in case the transfer gets interrupted
+				WebClient webClient = new WebClient();
+				webClient.DownloadFile(url, tempFilename);
+
+				// Move the completed temp file to filename
+				if (File.Exists(filename))
+					File.Delete(filename);
+				File.Move(tempFilename, filename);
+			}
+			catch (Exception)
+			{
+				// Cleanup temp file
+				if (File.Exists(tempFilename))
+					File.Delete(tempFilename);
+				throw;
+			}
+		}
+
+	}
+}
