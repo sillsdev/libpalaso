233f8094301f2eab98fce2b0e042020e51ed84ed
Damien Daspit <damien_daspit@sil.org>
2015-02-19 14:07:28 +0700

    Change TestBuild.sh to use DebugMonoStrongName configuration by default

--- build/TestBuild.sh
+++ build/TestBuild.sh
@@ -2,5 +2,5 @@
 cd "$(dirname "$0")/.."
 root=$PWD
 cd build
-#If a parameter is defined, then it will be used as the Configuration (defaulting to DebugMono)
-xbuild "/target:${2-Clean;Compile}" /property:Configuration="${1-DebugMono}" /property:RootDir=$root  /property:BUILD_NUMBER="0.0.0.abcd" build.mono.proj
+#If a parameter is defined, then it will be used as the Configuration (defaulting to DebugMonoStrongName)
+xbuild "/target:${2-Clean;Compile}" /property:Configuration="${1-DebugMonoStrongName}" /property:RootDir=$root  /property:BUILD_NUMBER="0.0.0.abcd" build.mono.proj
