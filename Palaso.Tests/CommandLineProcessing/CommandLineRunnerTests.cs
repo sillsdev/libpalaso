abb70a39bb734881cf6b75b8cdc696203fd8eb70
Eberhard Beilharz <eb1@sil.org>
2015-03-06 11:43:23 +0100

    Some refactoring of CommandLineRunnerTests
    Change-Id: I439fbd777d0a48c246b7834ce2e38e3caf6680fd

--- Palaso.Tests/CommandLineProcessing/CommandLineRunnerTests.cs
+++ Palaso.Tests/CommandLineProcessing/CommandLineRunnerTests.cs
@@ -4,20 +4,19 @@ using Palaso.CommandLineProcessing;
 using Palaso.Progress;
 
 
-namespace Palaso.Tests.Progress
+namespace Palaso.Tests.CommandLineProcessing
 {
 	[TestFixture]
 	public class CommandLineRunnerTests
 	{
+		private const string App = "PalasoUIWindowsForms.TestApp.exe";
 
-		[Test, Category("KnownMonoIssue")]
-		[Platform(Exclude="Linux", Reason = "Test has problems on Mono")]
+		[Test]
 		public void CommandWith10Line_NoCallbackOption_Get10LinesSynchronously()
 		{
-			var app = "PalasoUIWindowsForms.TestApp.exe";// FileLocator.GetFileDistributedWithApplication("PalasoUIWindowsForms.TestApp.exe");
 			var progress = new StringBuilderProgress();
-			int linesReceivedAsynchronously = 0;
-			var result = CommandLineRunner.Run(app, "CommandLineRunnerTest", null, string.Empty, 100, progress, null);
+			var result = CommandLineRunner.Run(App, "CommandLineRunnerTest", null, string.Empty, 100,
+				progress, null);
 			Assert.IsTrue(result.StandardOutput.Contains("0"));
 			Assert.IsTrue(result.StandardOutput.Contains("9"));
 		}
@@ -26,10 +25,12 @@ namespace Palaso.Tests.Progress
 		[Platform(Exclude="Linux", Reason = "Test has problems on Mono")]
 		public void CommandWith10Line_CallbackOption_Get10LinesAsynchronously()
 		{
-			var app = "PalasoUIWindowsForms.TestApp.exe";// FileLocator.GetFileDistributedWithApplication("PalasoUIWindowsForms.TestApp.exe");
 			var progress = new StringBuilderProgress();
 			int linesReceivedAsynchronously = 0;
-			CommandLineRunner.Run(app, "CommandLineRunnerTest", null, string.Empty, 100, progress, s => { ++linesReceivedAsynchronously; });
+			CommandLineRunner.Run(App, "CommandLineRunnerTest", null, string.Empty, 100,
+				progress, s => ++linesReceivedAsynchronously);
+			// The test fails on Linux because progress gets called 10x for StdOutput plus
+			// 1x for StdError (probably on the closing of the stream), so linesReceivedAsync is 11.
 			Assert.AreEqual(10, linesReceivedAsynchronously);
 		}
 	}
