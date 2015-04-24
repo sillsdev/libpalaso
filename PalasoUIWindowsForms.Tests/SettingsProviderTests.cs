83a6fa0d7648b4316cbcfd068587bcaf3b07534b
JohnThomson <john_thomson@sil.org>
2015-04-24 16:01:22 -0500

    Support customization of where Registration settings are stored
    Also switches Registration settings to be persisted using our
    cross-platform class. The first release using this will not upgrade
    old registration settings. The only current client is Bloom, which
    is OK with this.
    Also adds a better algorithm (modify time of config file) for finding
    previous configurations to upgrade from.

---
+++ PalasoUIWindowsForms.Tests/SettingsProviderTests.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using NUnit.Framework;
+using Palaso.IO;
+using Palaso.Settings;
+using Palaso.TestUtilities;
+using Palaso.UI.WindowsForms.Registration;
+
+namespace PalasoUIWindowsForms.Tests
+{
+	[TestFixture]
+	public class SettingsProviderTests
+	{
+		[Test]
+		public void SettingsFolderWithNoConfig_SortsAfterOneWithConfig()
+		{
+			// We do two iterations like this because we can't count on deleting the file to really make it gone at once.
+			for (var which = 0; which < 1; which++)
+			{
+				using (var tempFolder = new TemporaryFolder())
+				{
+					var firstDirPath = Path.Combine(tempFolder.Path, "first");
+					var secondDirPath = Path.Combine(tempFolder.Path, "second");
+					var firstConfigFile = Path.Combine(firstDirPath, CrossPlatformSettingsProvider.UserConfigFileName);
+					var secondConfigFile = Path.Combine(firstDirPath, CrossPlatformSettingsProvider.UserConfigFileName);
+					Directory.CreateDirectory(firstDirPath);
+					Directory.CreateDirectory(secondDirPath);
+					if (which == 0)
+						File.WriteAllText(firstConfigFile, @"nonsense");
+					else
+						File.WriteAllText(secondConfigFile, @"nonsense");
+
+					var list = new List<string>();
+					list.Add(secondDirPath);
+					list.Add(firstDirPath);
+					list.Sort(CrossPlatformSettingsProvider.VersionDirectoryComparison);
+					if (which == 0)
+						Assert.That(list[0], Is.EqualTo(firstDirPath), "first directory has config so should have come first");
+					else
+						Assert.That(list[0], Is.EqualTo(secondDirPath), "second directory has config so should have come first");
+				}
+			}
+		}
+
+		[Test]
+		public void SettingsFolderWithNewerConfig_SortsBeforeOneWithOlderConfig()
+		{
+
+			using (var tempFolder = new TemporaryFolder())
+			{
+				var firstDirPath = Path.Combine(tempFolder.Path, "first");
+				var secondDirPath = Path.Combine(tempFolder.Path, "second");
+				var firstConfigFile = Path.Combine(firstDirPath, CrossPlatformSettingsProvider.UserConfigFileName);
+				var secondConfigFile = Path.Combine(secondDirPath, CrossPlatformSettingsProvider.UserConfigFileName);
+				Directory.CreateDirectory(firstDirPath);
+				Directory.CreateDirectory(secondDirPath);
+				File.WriteAllText(firstConfigFile, @"nonsense");
+				File.WriteAllText(secondConfigFile, @"nonsense"); // second is newer
+
+				var result = CrossPlatformSettingsProvider.VersionDirectoryComparison(firstDirPath, secondDirPath);
+				Assert.That(result, Is.GreaterThan(0));
+
+				File.WriteAllText(firstConfigFile, @"nonsense"); // now first is newer
+				result = CrossPlatformSettingsProvider.VersionDirectoryComparison(firstDirPath, secondDirPath);
+				Assert.That(result, Is.LessThan(0));
+
+				// A final check to make sure it is really working the way we want
+				var list = new List<string>();
+				list.Add(secondDirPath);
+				list.Add(firstDirPath);
+				list.Sort(CrossPlatformSettingsProvider.VersionDirectoryComparison);
+				Assert.That(list[0], Is.EqualTo(firstDirPath));
+			}
+		}
+
+		/// <summary>
+		/// This test was primarily created to test that we can override the default location for the RegistrationSettingsProvider.
+		/// However it's also quite a useful test of the whole business of reading settings.
+		/// </summary>
+		[Test]
+		public void CanOverrideDefaultLocation()
+		{
+			RegistrationSettingsProvider.SetProductName("BloomUnitTest");
+			var settingsProvider = new RegistrationSettingsProvider();
+			settingsProvider.Initialize(null, null); // Seems to be what .NET does, despite warnings
+			var dirPath = settingsProvider.UserConfigLocation;
+			Assert.That(dirPath, Is.StringContaining("BloomUnitTest"));
+			Directory.CreateDirectory(dirPath);
+			var filePath = Path.Combine(dirPath, CrossPlatformSettingsProvider.UserConfigFileName);
+			using (var tempFile = new TempFile(filePath, true))
+			{
+				File.WriteAllText(filePath,
+					@"<?xml version='1.0' encoding='utf-8'?>
+<configuration>
+    <userSettings>
+        <Palaso.UI.WindowsForms.Registration.Registration>
+            <setting name='Email' serializeAs='String'>
+                <value>someone@somewhere.org</value>
+            </setting>
+        </Palaso.UI.WindowsForms.Registration.Registration>
+    </userSettings>
+</configuration>");
+
+				var regSettings = Registration.Default;
+				var email = regSettings.Email;
+				Assert.That(email, Is.EqualTo("someone@somewhere.org"));
+			}
+		}
+	}
+}
---
+++ PalasoUIWindowsForms/Registration/RegistrationSettingsProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Palaso.Settings;
+
+namespace Palaso.UI.WindowsForms.Registration
+{
+	/// <summary>
+	/// This class is used to store the settings for the Registration object.
+	/// In addition to the benefits of using the CrossPlatformSettingsProvider, it allows control
+	/// of the string used as the application name. This allows applications like Bloom, which
+	/// support parallel installation of different 'channel' versions of the product, to have them
+	/// all share a single location for these settings. This in turn saves the user from having
+	/// to register separately for each channel, and prevents us from counting the same user
+	/// multiple times in different channels.
+	/// </summary>
+	public class RegistrationSettingsProvider : CrossPlatformSettingsProvider
+	{
+		public static void SetProductName(string product)
+		{
+			ProductName = product;
+		}
+	}
+}
