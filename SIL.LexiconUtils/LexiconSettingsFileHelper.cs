74f047009daef0b2c62d2f686d100624540f81d1
Damien Daspit <damien_daspit@sil.org>
2015-05-06 16:17:01 +0700

    Add LexiconSettingsFileHelper utility class

---
+++ SIL.LexiconUtils/LexiconSettingsFileHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace SIL.LexiconUtils
+{
+	/// <summary>
+	/// Provides utility functions for generating the lexicon settings filenames.
+	/// </summary>
+	public static class LexiconSettingsFileHelper
+	{
+		public const string LexiconProjectSettingsExtension = ".lpsx";
+		public const string LexiconUserSettingsExtension = ".lusx";
+		public const string SharedSettingsFolder = "SharedSettings";
+		public const string LexiconProjectSettingsFilename = "LexiconProjectSettings" + LexiconProjectSettingsExtension;
+
+		public static string GetSharedSettingsPath(string basePath)
+		{
+			return Path.Combine(basePath, SharedSettingsFolder);
+		}
+
+		public static string GetLexiconProjectSettingsPath(string basePath)
+		{
+			return Path.Combine(GetSharedSettingsPath(basePath), LexiconProjectSettingsFilename);
+		}
+
+		public static string GetLexiconUserSettingsPath(string basePath)
+		{
+			return Path.Combine(GetSharedSettingsPath(basePath), Environment.UserName + LexiconUserSettingsExtension);
+		}
+	}
+}
