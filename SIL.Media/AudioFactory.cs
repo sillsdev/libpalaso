cc52e3987ad9ce355bb2619e337e47057d7b8738
Damien Daspit <damien_daspit@sil.org>
2015-06-03 11:46:40 +0700

    Fix AudioFactory on Linux

--- SIL.Media/AudioFactory.cs
+++ SIL.Media/AudioFactory.cs
@@ -1,4 +1,7 @@
 using System;
+#if MONO
+using SIL.Media.AlsaAudio;
+#endif
 
 namespace SIL.Media
 {
@@ -7,8 +10,8 @@ namespace SIL.Media
 		public static ISimpleAudioSession CreateAudioSession(string filePath)
 		{
 #if MONO
-				//return new AudioGStreamerSession(filePath);
-				return new AudioAlsaSession(filePath);
+			//return new AudioGStreamerSession(filePath);
+			return new AudioAlsaSession(filePath);
 #else
 			return new AudioIrrKlangSession(filePath);
 #endif
