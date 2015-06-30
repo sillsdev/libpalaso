a09426b2e7f8dcf3c94c0e1bc91215d1492f5ab9
darcywong00 <darcy_wong@sil.org>
2015-06-30 08:13:07 +0700

    Update copy scripts for Chorus and WeSay
    Updated for new SIL assembly names
    Also copying StrongName assemblies

--- CopyTo Dev WeSay.bat
+++
@@ -1,9 +0,0 @@
-set weSayDir=..\wesayDev
-copy /Y output\debug\*.dll %weSayDir%\lib\debug
-copy /Y output\debug\*.pdb %weSayDir%\lib\debug
-copy /Y output\debug\Palaso.BuildTasks.dll %weSayDir%\bld
-
-copy /Y %weSayDir%\lib\debug\palaso*.* %weSayDir%\output\debug
-
-
-pause
