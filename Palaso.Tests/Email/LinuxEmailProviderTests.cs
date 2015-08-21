9e171f34f35b6634e4c1b4c1f0637711672eeab0
MarkS <marksvc@gmail.com>
2015-08-21 15:36:09 -0600

    Use TestCases

--- Palaso.Tests/Email/LinuxEmailProviderTests.cs
+++ Palaso.Tests/Email/LinuxEmailProviderTests.cs
@@ -7,27 +7,21 @@ namespace Palaso.Tests.Email
 	[TestFixture]
 	public class LinuxEmailProviderTests
 	{
-		[Test]
-		public void EscapeString()
+		// Does not change most things.
+		[TestCase(@"hello1234 hello", Result =@"hello1234 hello")]
+		[TestCase("hello\"hello", Result ="hello\"hello")]
+		[TestCase(@"hello`~!@#$%^&*)(][}{/?=+-_|", Result =@"hello`~!@#$%^&*)(][}{/?=+-_|")]
+		// Escapes quote and backslash characters.
+		[TestCase(@"\", Result =@"\\\\\\\\")]
+		[TestCase(@"hello\hello", Result =@"hello\\\\\\\\hello")]
+		[TestCase(@"hello\t\n\a\r\0end", Result =@"hello\\\\\\\\t\\\\\\\\n\\\\\\\\a\\\\\\\\r\\\\\\\\0end")]
+		[TestCase(@"hello'hello", Result =@"hello\'hello")]
+		[TestCase(@"hello'hello'", Result =@"hello\'hello\'")]
+		[TestCase("hello'\"hello", Result ="hello\\'\"hello")]
+		[TestCase(@"C:\'Data\0end", Result =@"C:\\\\\\\\\'Data\\\\\\\\0end")]
+		public string EscapeString(string input)
 		{
-			string input;
-
-			// Does not change most things.
-			input = @"hello1234 hello";
-			Assert.That(LinuxEmailProvider.EscapeString(input), Is.EqualTo(input));
-			input = "hello\"hello";
-			Assert.That(LinuxEmailProvider.EscapeString(input), Is.EqualTo(input));
-			input = @"hello`~!@#$%^&*)(][}{/?=+-_|";
-			Assert.That(LinuxEmailProvider.EscapeString(input), Is.EqualTo(input));
-
-			// Escapes quote and backslash characters.
-			Assert.That(LinuxEmailProvider.EscapeString(@"\"), Is.EqualTo(@"\\\\\\\\"));
-			Assert.That(LinuxEmailProvider.EscapeString(@"hello\hello"), Is.EqualTo(@"hello\\\\\\\\hello"));
-			Assert.That(LinuxEmailProvider.EscapeString(@"hello\t\n\a\r\0end"), Is.EqualTo(@"hello\\\\\\\\t\\\\\\\\n\\\\\\\\a\\\\\\\\r\\\\\\\\0end"));
-			Assert.That(LinuxEmailProvider.EscapeString(@"hello'hello"), Is.EqualTo(@"hello\'hello"));
-			Assert.That(LinuxEmailProvider.EscapeString(@"hello'hello'"), Is.EqualTo(@"hello\'hello\'"));
-			Assert.That(LinuxEmailProvider.EscapeString("hello'\"hello"), Is.EqualTo("hello\\'\"hello"));
-			Assert.That(LinuxEmailProvider.EscapeString(@"C:\'Data\0end"), Is.EqualTo(@"C:\\\\\\\\\'Data\\\\\\\\0end"));
+			return LinuxEmailProvider.EscapeString(input);
 		}
 	}
 }
