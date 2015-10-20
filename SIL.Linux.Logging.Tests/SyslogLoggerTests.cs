fe821df94d44a3cdb5247ac85aa17263174cd9de
Robin Munn <robin_munn@sil.org>
2015-10-20 14:53:39 +0700

    Add SyslogLogger, exception handler, and tests
    SyslogLogger will only run on Linux (and Linux-like systems such as OS X,
    as long as they have a libc library). It uses the syslog() API defined in
    libc to log messages to the local syslog program.
    
    SyslogExceptionHandler likewise will only run on Linux, since it relies on
    SyslogLogger to log exceptions to syslog.
    
    Both classes are contained in the SIL.Linux.Logging DLL, which will only
    build on Linux (i.e., in the Mono build configurations).

---
+++ SIL.Linux.Logging.Tests/SyslogLoggerTests.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace SIL.Linux.Logging.Tests
+{
+	/// <summary>
+	/// CAUTION: These tests assume that the machine you're running them on logs its messages
+	/// to /var/log/syslog. If that's not the case, all the tests will fail!
+	/// </summary>
+	[TestFixture]
+	[Platform(Include="Linux")]
+	[Explicit("by hand only")] // Don't want to log to syslog during automated tests
+    public class SyslogLoggerTests
+	{
+		public string FakeAppName = "FakeAppNameForTestingPurposes";
+
+		/// <summary>
+		/// If you need to watch a different file rather than /var/log/syslog, change this field.
+		/// </summary>
+		public string SyslogFileToWatch = "/var/log/syslog";
+
+		[Test]
+		public void SyslogLogger_WritesOneLine()
+		{
+			var watcher = new SyslogWatcher(FakeAppName, SyslogFileToWatch);
+			var logger = new SyslogLogger(FakeAppName);
+			watcher.StartWatching();
+			logger.Error("This string should be in the results");
+			IEnumerable<string> data = watcher.WaitForData();
+			watcher.StopWatching();
+			Assert.That(data, Has.Some.StringContaining("This string should be in the results"));
+		}
+
+		[Test]
+		public void SyslogLogger_LogMany_WritesManyLines()
+		{
+			var watcher = new SyslogWatcher(FakeAppName, SyslogFileToWatch);
+			var logger = new SyslogLogger(FakeAppName);
+			watcher.StartWatching();
+			string[] messages = 
+			{
+				"First log message",
+				"Second log message",
+				"Third log message",
+				"Fourth log message",
+				"Fifth log message"
+			};
+			logger.LogMany(SyslogPriority.Error, messages);
+			string[] data = watcher.WaitForData(5).ToArray();
+			watcher.StopWatching();
+			for (int i = 0; i < messages.Length; i++)
+				Assert.That(data[i], Is.StringContaining(messages[i]));
+		}
+
+		[Test]
+		public void SyslogLogger_TwoLoggers_WillNotInterefereWithEachOther()
+		{
+			var watcher = new SyslogWatcher(FakeAppName, SyslogFileToWatch);
+			var logger1 = new SyslogLogger(FakeAppName + "1");
+			var logger2 = new SyslogLogger(FakeAppName + "2");
+			watcher.StartWatching();
+			string[] messages = 
+			{
+				"First log message",
+				"Second log message",
+				"Third log message",
+				"Fourth log message",
+				"Fifth log message"
+			};
+			foreach (string s in messages)
+			{
+				logger1.Error(s);
+				logger2.Error(s);
+			}
+			string[] data = watcher.WaitForData(10).ToArray();
+			watcher.StopWatching();
+			for (int i = 0; i < messages.Length; i++)
+			{
+				// Expect messages perfectly interleaved, logger1 followed by logger2 all five times
+				string expectedAppName = FakeAppName + (i % 2 == 0 ? "1" : "2");
+				Assert.That(data[i], Is.StringContaining(expectedAppName));
+				Assert.That(data[i], Is.StringContaining(messages[i / 2]));
+			}
+		}
+
+		[Test]
+		public void SyslogLogger_TwoLogManyCalls_WillRunSequentially()
+		{
+			var watcher = new SyslogWatcher(FakeAppName, SyslogFileToWatch);
+			var logger1 = new SyslogLogger(FakeAppName + "1");
+			var logger2 = new SyslogLogger(FakeAppName + "2");
+			watcher.StartWatching();
+			string[] messages = 
+			{
+				"First log message",
+				"Second log message",
+				"Third log message",
+				"Fourth log message",
+				"Fifth log message"
+			};
+			logger1.LogMany(SyslogPriority.Error, messages);
+			logger2.LogMany(SyslogPriority.Error, messages);
+			string[] data = watcher.WaitForData(10).ToArray();
+			watcher.StopWatching();
+			for (int i = 0; i < messages.Length; i++)
+			{
+				// Expect messages in sequence, logger1's five lines followed by logger2's five lines
+				string expectedAppName = FakeAppName + (i / 5 == 0 ? "1" : "2");
+				Assert.That(data[i], Is.StringContaining(expectedAppName));
+				Assert.That(data[i], Is.StringContaining(messages[i % 5]));
+			}
+		}
+	}
+}
---
+++ SIL.Linux.Logging.Tests/SyslogWatcher.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace SIL.Linux.Logging.Tests
+{
+	public class SyslogWatcher
+	{
+		public string StringToWatchFor { get; set; }
+		private string WatchedFile { get; set; }
+		private FileStream WatchingStream { get; set; }
+		private StreamReader WatchingReader { get; set; }
+		private AutoResetEvent DataReady { get; set; }
+		private Int64 _startPos = Int64.MinValue;
+		private List<string> _collectedStrings = new List<string>();
+		private Timer _pollTimer;
+		private double _pollInterval;
+		private int _timeout;
+
+		public SyslogWatcher(string stringToWatchFor = null, string logfile = "/var/log/syslog", double pollInterval = 50.0, int timeout = 10000)
+		{
+			StringToWatchFor = stringToWatchFor ?? String.Empty;
+			WatchedFile = logfile;
+			_pollInterval = pollInterval;
+			_timeout = timeout;
+		}
+
+		public void StartWatching()
+		{
+			// Once we start using .NET 4.5, this can be rewritten much more simply using async/await
+			var info = new FileInfo(WatchedFile);
+			_startPos = info.Length;
+			SafelyCloseStreamAndReader();
+			WatchingStream = new FileStream(WatchedFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+			WatchingStream.Seek(_startPos, SeekOrigin.Begin);
+			WatchingReader = new StreamReader(WatchingStream, Encoding.UTF8);
+			DataReady = new AutoResetEvent(false);
+			StartTimer();
+		}
+
+		public void StopWatching()
+		{
+			StopTimer();
+			SafelyCloseStreamAndReader();
+		}
+
+		public IEnumerable<string> WaitForData(int numLinesExpected = 1)
+		{
+			string[] result = new string[numLinesExpected];
+			int linesFound = 0;
+			while (true) { 
+				if (DataReady.WaitOne(_timeout))
+				{
+					StopTimer();
+					_collectedStrings.CopyTo(0, result, linesFound, (numLinesExpected - linesFound));
+					linesFound += _collectedStrings.Count;
+					_collectedStrings.Clear();
+					if (linesFound >= numLinesExpected)
+						return result;
+					StartTimer();
+				}
+				else
+				{
+					// If timeout reached without finding any new data, return what we have so far
+					return result;
+				}
+			}
+		}
+
+		private void StartTimer()
+		{
+			_collectedStrings.Clear();
+			_pollTimer = new Timer(_pollInterval);
+			_pollTimer.Elapsed += CheckForData;
+			_pollTimer.Start();
+		}
+
+		private void StopTimer()
+		{
+			if (_pollTimer != null)
+			{
+				_pollTimer.Stop();
+				_pollTimer.Elapsed -= CheckForData;
+			}
+		}
+
+		private void CheckForData(object sender, ElapsedEventArgs elapsedEventArgs)
+		{
+			while (true)
+			{
+				string s = WatchingReader.ReadLine();
+				if (s == null)
+					break;
+				if (String.IsNullOrEmpty(StringToWatchFor) || s.Contains(StringToWatchFor))
+				{
+					_collectedStrings.Add(s);
+				} 
+			}
+			if (_collectedStrings.Count > 0)
+				DataReady.Set();
+		}
+
+		private void SafelyCloseStreamAndReader()
+		{
+			if (WatchingReader != null)
+			{
+				WatchingReader.Close();
+				WatchingReader = StreamReader.Null;
+			}
+			if (WatchingStream != null)
+			{
+				WatchingStream.Close();
+				WatchingStream = null;
+			}
+		}
+	}
+}
