b2287385b1526dfc8fe21f00c508b948f0c3a685
Damien Daspit <damien_daspit@sil.org>
2015-11-30 11:03:49 +0700

    Add OfflineSldr class to enable offline mode for the SLDR in test setup methods

---
+++ SIL.TestUtilities/OfflineSldr.cs
@@ -0,0 +1,32 @@
+﻿using SIL.ObjectModel;
+using SIL.WritingSystems;
+
+namespace SIL.TestUtilities
+{
+	/// <summary>
+	/// This is a disposable class that enables offline mode for the SLDR. It is used for unit tests, specifically
+	/// in fixture or test setup methods. The OfflineSldrAttribute is the easier method for enabling offline mode,
+	/// but it is run after the fixture and test setup methods. This class can be used in fixture and test setup
+	/// methods so that offline mode can be enabled for the SLDR before other code is executed in these methods.
+	/// </summary>
+	public class OfflineSldr : DisposableBase
+	{
+		private readonly TemporaryFolder _sldrCacheFolder;
+
+		public OfflineSldr()
+		{
+			_sldrCacheFolder = new TemporaryFolder("SldrCacheTest");
+			Sldr.SldrCachePath = _sldrCacheFolder.Path;
+			Sldr.OfflineMode = true;
+			Sldr.ResetLanguageTags();
+		}
+
+		protected override void DisposeManagedResources()
+		{
+			Sldr.OfflineMode = false;
+			Sldr.ResetLanguageTags();
+			_sldrCacheFolder.Dispose();
+			Sldr.SldrCachePath = Sldr.DefaultSldrCachePath;
+		}
+	}
+}
