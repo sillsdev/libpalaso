e85806f49f02a615edbd030952f5b1f3c93bfba2
MarkS <marksvc@gmail.com>
2016-01-04 12:59:11 -0500

    Close NDesk bus when exit

--- Palaso/PalasoSetup.cs
+++ Palaso/PalasoSetup.cs
@@ -5,15 +5,16 @@ namespace Palaso
 	/// Provide global setup and shutdown for the Palaso assembly.
 	/// </summary>
 	/// <remarks>
-	/// This should be used something like the following in a program's Main() method:
+	/// This would be used something like the following in a program's Main() method:
 	/// using (new PalasoSetup())
 	/// {
 	/// 	Application.Run(new MainWindow(args));
 	/// }
 	/// </remarks>
 	/// <remarks>
-	/// At the moment, this is needed only if the program implicitly or explicitly
-	/// uses Palaso.UsbDrive on Linux/Mono.
+	/// This class was originally created to help close NDesk.DBus on
+	/// Linux, which it is no longer used for, but might be useful for
+	/// another purpose in the future.
 	/// </remarks>
 	public class PalasoSetup : IDisposable
 	{
@@ -34,14 +35,6 @@ namespace Palaso
 				return;
 			if (disposing)
 			{
-#if __MonoCS__
-				// Using Palaso.UsbDrive on Linux/Mono results in NDesk spinning up a thread that
-				// continues until NDesk Bus is closed.  Failure to close the thread results in a
-				// program hang when closing.  Closing the system bus allows the thread to close,
-				// and thus the program to close.  Closing the system bus can happen safely only
-				// at the end of the program.
-				NDesk.DBus.Bus.System.Close();
-#endif
 			}
 			disposed = true;
 		}
