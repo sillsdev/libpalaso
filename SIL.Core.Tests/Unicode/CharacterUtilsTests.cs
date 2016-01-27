7b5464a0699f8e1d40f93c2339968037169dc9c1
Tom Bogle <tom_bogle@sil.org>
2016-01-27 13:19:43 -0500

    Added a new static class "CharacterUtils" to eliminate the need for multiple projects to maintain (sometimes conflicting/incomplete) lists of sentence-ending punctuation characters and to ensure the fastest implementation for checking to see whether a character is one of them.

---
+++ SIL.Core.Tests/Unicode/CharacterUtilsTests.cs
@@ -0,0 +1,309 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using SIL.Unicode;
+
+namespace SIL.Tests.Unicode
+{
+	[TestFixture]
+	class CharacterUtilsTests
+	{
+		// For completeness, this list should be kept in synch with the list used in the following test
+		// and with the list and switch statement in CharacterUtils itself.
+
+		// Note... while one might think that char.GetUnicodeCategory could tell you if a character was a sentence separator, this is not the case. 
+		// This is because, for example, '.' can be used for various things (abbreviation, decimal point, as well as sentence terminator).
+		// This should be a complete list of code points with the \p{Sentence_Break=STerm} or \p{Sentence_Break=ATerm} properties that also
+		// have the \p{Terminal_Punctuation} property. This list is up-to-date as of Unicode v6.1.
+		// ENHANCE: Ideally this should be dynamic.
+
+		[TestCase('.')] // ROMAN FULL STOP
+		[TestCase('?')] // ROMAN QUESTION MARK
+		[TestCase('!')] // ROMAN EXCLAMATION MARK
+		[TestCase('\u00A7')] // SECTION SIGN
+		[TestCase('\u055C')] // ARMENIAN EXCLAMATION MARK
+		[TestCase('\u055E')] // ARMENIAN QUESTION MARK
+		[TestCase('\u0589')] // ARMENIAN FULL STOP
+		[TestCase('\u061F')] // ARABIC QUESTION MARK
+		[TestCase('\u06D4')] // ARABIC FULL STOP
+		[TestCase('\u0700')] // SYRIAC END OF PARAGRAPH
+		[TestCase('\u0701')] // SYRIAC SUPRALINEAR FULL STOP
+		[TestCase('\u0702')] // SYRIAC SUBLINEAR FULL STOP
+		[TestCase('\u07F9')] // NKO EXCLAMATION MARK
+		[TestCase('\u0964')] // DEVANAGARI DANDA
+		[TestCase('\u0965')] // DEVANAGARI DOUBLE DANDA
+		[TestCase('\u104A')] // MYANMAR SIGN LITTLE SECTION
+		[TestCase('\u104B')] // MYANMAR SIGN SECTION
+		[TestCase('\u1362')] // ETHIOPIC FULL STOP
+		[TestCase('\u1367')] // ETHIOPIC QUESTION MARK
+		[TestCase('\u1368')] // ETHIOPIC PARAGRAPH SEPARATOR
+		[TestCase('\u166E')] // CANADIAN SYLLABICS FULL STOP
+		[TestCase('\u1803')] // MONGOLIAN FULL STOP
+		[TestCase('\u1809')] // MONGOLIAN MANCHU FULL STOP
+		[TestCase('\u1944')] // LIMBU EXCLAMATION MARK
+		[TestCase('\u1945')] // LIMBU QUESTION MARK
+		[TestCase('\u1AA8')] // TAI THAM SIGN KAAN
+		[TestCase('\u1AA9')] // TAI THAM SIGN KAANKUU
+		[TestCase('\u1AAA')] // TAI THAM SIGN SATKAAN
+		[TestCase('\u1AAB')] // TAI THAM SIGN SATKAANKUU
+		[TestCase('\u1B5A')] // BALINESE PANTI
+		[TestCase('\u1B5B')] // BALINESE PAMADA
+		[TestCase('\u1B5E')] // BALINESE CARIK SIKI
+		[TestCase('\u1B5F')] // BALINESE CARIK PAREREN
+		[TestCase('\u1C3B')] // LEPCHA PUNCTUATION TA-ROL
+		[TestCase('\u1C3C')] // LEPCHA PUNCTUATION NYET THYOOM TA-ROL
+		[TestCase('\u1C7E')] // OL CHIKI PUNCTUATION MUCAAD
+		[TestCase('\u1C7F')] // OL CHIKI PUNCTUATION DOUBLE MUCAAD
+		[TestCase('\u203C')] // DOUBLE EXCLAMATION MARK
+		[TestCase('\u203D')] // INTERROBANG
+		[TestCase('\u2047')] // DOUBLE QUESTION MARK
+		[TestCase('\u2048')] // QUESTION EXCLAMATION MARK
+		[TestCase('\u2049')] // EXCLAMATION QUESTION MARK
+		[TestCase('\u2CF9')] // COPTIC OLD NUBIAN FULL STOP
+		[TestCase('\u2CFA')] // COPTIC OLD NUBIAN DIRECT QUESTION MARK
+		[TestCase('\u2CFB')] // COPTIC OLD NUBIAN INDIRECT QUESTION MARK
+		[TestCase('\u2CFE')] // COPTIC FULL STOP
+		[TestCase('\u2E2E')] // REVERSED QUESTION MARK
+		[TestCase('\u2E3C')] // STENOGRAPHIC FULL STOP
+		[TestCase('\u3002')] // IDEOGRAPHIC FULL STOP
+		[TestCase('\uA4FF')] // LISU PUNCTUATION FULL STOP
+		[TestCase('\uA60E')] // VAI FULL STOP
+		[TestCase('\uA60F')] // VAI QUESTION MARK
+		[TestCase('\uA6F3')] // BAMUM FULL STOP
+		[TestCase('\uA6F7')] // BAMUM QUESTION MARK
+		[TestCase('\uA876')] // PHAGS-PA MARK SHAD
+		[TestCase('\uA877')] // PHAGS-PA MARK DOUBLE SHAD
+		[TestCase('\uA8CE')] // SAURASHTRA DANDA
+		[TestCase('\uA8CF')] // SAURASHTRA DOUBLE DANDA
+		[TestCase('\uA92F')] // KAYAH LI SIGN SHYA
+		[TestCase('\uA9C8')] // JAVANESE PADA LINGSA
+		[TestCase('\uA9C9')] // JAVANESE PADA LUNGSI
+		[TestCase('\uAA5D')] // CHAM PUNCTUATION DANDA
+		[TestCase('\uAA5E')] // CHAM PUNCTUATION DOUBLE DANDA
+		[TestCase('\uAA5F')] // CHAM PUNCTUATION TRIPLE DANDA
+		[TestCase('\uAAF0')] // MEETEI MAYEK CHEIKHAN
+		[TestCase('\uAAF1')] // MEETEI MAYEK AHANG KHUDAM
+		[TestCase('\uABEB')] // MEETEI MAYEK CHEIKHEI
+		[TestCase('\uFE52')] // SMALL FULL STOP
+		[TestCase('\uFE56')] // SMALL QUESTION MARK
+		[TestCase('\uFE57')] // SMALL EXCLAMATION MARK
+		[TestCase('\uFF01')] // FULLWIDTH EXCLAMATION MARK
+		[TestCase('\uFF0E')] // FULLWIDTH FULL STOP
+		[TestCase('\uFF1F')] // FULLWIDTH QUESTION MARK
+		[TestCase('\uFF61')] // HALFWIDTH IDEOGRAPHIC FULL STOP
+		public void SentenceFinalPunctuation_ContainsExpectedCharacters(char c)
+		{
+			Assert.IsTrue(CharacterUtils.SentenceFinalPunctuation.Contains(c));
+		}
+
+		// For completeness, this list should be kept in synch with the list used in the above test
+		// and with the list and switch statement in CharacterUtils itself.
+		[TestCase('.')] // ROMAN FULL STOP
+		[TestCase('?')] // ROMAN QUESTION MARK
+		[TestCase('!')] // ROMAN EXCLAMATION MARK
+		[TestCase('\u00A7')] // SECTION SIGN
+		[TestCase('\u055C')] // ARMENIAN EXCLAMATION MARK
+		[TestCase('\u055E')] // ARMENIAN QUESTION MARK
+		[TestCase('\u0589')] // ARMENIAN FULL STOP
+		[TestCase('\u061F')] // ARABIC QUESTION MARK
+		[TestCase('\u06D4')] // ARABIC FULL STOP
+		[TestCase('\u0700')] // SYRIAC END OF PARAGRAPH
+		[TestCase('\u0701')] // SYRIAC SUPRALINEAR FULL STOP
+		[TestCase('\u0702')] // SYRIAC SUBLINEAR FULL STOP
+		[TestCase('\u07F9')] // NKO EXCLAMATION MARK
+		[TestCase('\u0964')] // DEVANAGARI DANDA
+		[TestCase('\u0965')] // DEVANAGARI DOUBLE DANDA
+		[TestCase('\u104A')] // MYANMAR SIGN LITTLE SECTION
+		[TestCase('\u104B')] // MYANMAR SIGN SECTION
+		[TestCase('\u1362')] // ETHIOPIC FULL STOP
+		[TestCase('\u1367')] // ETHIOPIC QUESTION MARK
+		[TestCase('\u1368')] // ETHIOPIC PARAGRAPH SEPARATOR
+		[TestCase('\u166E')] // CANADIAN SYLLABICS FULL STOP
+		[TestCase('\u1803')] // MONGOLIAN FULL STOP
+		[TestCase('\u1809')] // MONGOLIAN MANCHU FULL STOP
+		[TestCase('\u1944')] // LIMBU EXCLAMATION MARK
+		[TestCase('\u1945')] // LIMBU QUESTION MARK
+		[TestCase('\u1AA8')] // TAI THAM SIGN KAAN
+		[TestCase('\u1AA9')] // TAI THAM SIGN KAANKUU
+		[TestCase('\u1AAA')] // TAI THAM SIGN SATKAAN
+		[TestCase('\u1AAB')] // TAI THAM SIGN SATKAANKUU
+		[TestCase('\u1B5A')] // BALINESE PANTI
+		[TestCase('\u1B5B')] // BALINESE PAMADA
+		[TestCase('\u1B5E')] // BALINESE CARIK SIKI
+		[TestCase('\u1B5F')] // BALINESE CARIK PAREREN
+		[TestCase('\u1C3B')] // LEPCHA PUNCTUATION TA-ROL
+		[TestCase('\u1C3C')] // LEPCHA PUNCTUATION NYET THYOOM TA-ROL
+		[TestCase('\u1C7E')] // OL CHIKI PUNCTUATION MUCAAD
+		[TestCase('\u1C7F')] // OL CHIKI PUNCTUATION DOUBLE MUCAAD
+		[TestCase('\u203C')] // DOUBLE EXCLAMATION MARK
+		[TestCase('\u203D')] // INTERROBANG
+		[TestCase('\u2047')] // DOUBLE QUESTION MARK
+		[TestCase('\u2048')] // QUESTION EXCLAMATION MARK
+		[TestCase('\u2049')] // EXCLAMATION QUESTION MARK
+		[TestCase('\u2CF9')] // COPTIC OLD NUBIAN FULL STOP
+		[TestCase('\u2CFA')] // COPTIC OLD NUBIAN DIRECT QUESTION MARK
+		[TestCase('\u2CFB')] // COPTIC OLD NUBIAN INDIRECT QUESTION MARK
+		[TestCase('\u2CFE')] // COPTIC FULL STOP
+		[TestCase('\u2E2E')] // REVERSED QUESTION MARK
+		[TestCase('\u2E3C')] // STENOGRAPHIC FULL STOP
+		[TestCase('\u3002')] // IDEOGRAPHIC FULL STOP
+		[TestCase('\uA4FF')] // LISU PUNCTUATION FULL STOP
+		[TestCase('\uA60E')] // VAI FULL STOP
+		[TestCase('\uA60F')] // VAI QUESTION MARK
+		[TestCase('\uA6F3')] // BAMUM FULL STOP
+		[TestCase('\uA6F7')] // BAMUM QUESTION MARK
+		[TestCase('\uA876')] // PHAGS-PA MARK SHAD
+		[TestCase('\uA877')] // PHAGS-PA MARK DOUBLE SHAD
+		[TestCase('\uA8CE')] // SAURASHTRA DANDA
+		[TestCase('\uA8CF')] // SAURASHTRA DOUBLE DANDA
+		[TestCase('\uA92F')] // KAYAH LI SIGN SHYA
+		[TestCase('\uA9C8')] // JAVANESE PADA LINGSA
+		[TestCase('\uA9C9')] // JAVANESE PADA LUNGSI
+		[TestCase('\uAA5D')] // CHAM PUNCTUATION DANDA
+		[TestCase('\uAA5E')] // CHAM PUNCTUATION DOUBLE DANDA
+		[TestCase('\uAA5F')] // CHAM PUNCTUATION TRIPLE DANDA
+		[TestCase('\uAAF0')] // MEETEI MAYEK CHEIKHAN
+		[TestCase('\uAAF1')] // MEETEI MAYEK AHANG KHUDAM
+		[TestCase('\uABEB')] // MEETEI MAYEK CHEIKHEI
+		[TestCase('\uFE52')] // SMALL FULL STOP
+		[TestCase('\uFE56')] // SMALL QUESTION MARK
+		[TestCase('\uFE57')] // SMALL EXCLAMATION MARK
+		[TestCase('\uFF01')] // FULLWIDTH EXCLAMATION MARK
+		[TestCase('\uFF0E')] // FULLWIDTH FULL STOP
+		[TestCase('\uFF1F')] // FULLWIDTH QUESTION MARK
+		[TestCase('\uFF61')] // HALFWIDTH IDEOGRAPHIC FULL STOP
+		public void IsSentenceFinalPunctuation_CharacterIsSentenceFinalPunctuation_ReturnsTrue(char c)
+		{
+			Assert.IsTrue(CharacterUtils.IsSentenceFinalPunctuation(c));
+		}
+
+		[Test]
+		public void IsSentenceFinalPunctuation_AllSentenceFinalPunctuation_ReturnsTrue()
+		{
+			foreach (var c in CharacterUtils.SentenceFinalPunctuation)
+				Assert.IsTrue(CharacterUtils.IsSentenceFinalPunctuation(c));
+		}
+
+		[TestCase(',')]
+		[TestCase('1')]
+		[TestCase('#')]
+		[TestCase('$')]
+		[TestCase('(')]
+		[TestCase('"')]
+		[TestCase(')')]
+		[TestCase('/')]
+		[TestCase('|')]
+		[TestCase(':')]
+		[TestCase(';')]
+		[TestCase('\uFEFF')]
+		[TestCase('\uFFFF')]
+		public void IsSentenceFinalPunctuation_CharacterIsNotSentenceFinalPunctuation_ReturnsFalse(char c)
+		{
+			Assert.IsFalse(CharacterUtils.IsSentenceFinalPunctuation(c));
+		}
+
+		[TestCase(',')]
+		[TestCase('1')]
+		[TestCase('#')]
+		[TestCase('$')]
+		[TestCase('(')]
+		[TestCase('"')]
+		[TestCase(')')]
+		[TestCase('/')]
+		[TestCase('|')]
+		[TestCase(':')]
+		[TestCase(';')]
+		[TestCase('\uFEFF')]
+		[TestCase('\uFFFF')]
+		public void SentenceFinalPunctuation_DoesNotContainUnexpectedCharacters(char c)
+		{
+			Assert.IsFalse(CharacterUtils.SentenceFinalPunctuation.Contains(c));
+		}
+
+		[Test]
+		public void SentenceFinalPunctuation_LocalArrayModified_SentenceFinalPunctuationNotChanged()
+		{
+			var array = CharacterUtils.SentenceFinalPunctuation;
+			var originalFirstElement = array[0];
+			array[0] = '$';
+			Assert.AreEqual(originalFirstElement, CharacterUtils.SentenceFinalPunctuation[0]);
+		}
+
+		//[Test]
+		//public void SpeedTest()
+		//{
+		//	var sw = new Stopwatch();
+		//	sw.Start();
+		//	for (int i = 0; i < 10000; i++)
+		//	{
+		//		CharacterUtils.IsSentenceFinalPunctuation('.'); // ROMAN FULL STOP
+		//		CharacterUtils.IsSentenceFinalPunctuation('?'); // ROMAN QUESTION MARK
+		//		CharacterUtils.IsSentenceFinalPunctuation('!'); // ROMAN EXCLAMATION MARK
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u0589'); // ARMENIAN FULL STOP
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u061F'); // ARABIC QUESTION MARK
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u06D4'); // ARABIC FULL STOP
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u0700'); // SYRIAC END OF PARAGRAPH
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u0701'); // SYRIAC SUPRALINEAR FULL STOP
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u0702'); // SYRIAC SUBLINEAR FULL STOP
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u07F9'); // NKO EXCLAMATION MARK
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u0964'); // DEVANAGARI DANDA
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u0965'); // DEVANAGARI DOUBLE DANDA
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u104A'); // MYANMAR SIGN LITTLE SECTION
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u104B'); // MYANMAR SIGN SECTION
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u1362'); // ETHIOPIC FULL STOP
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u1367'); // ETHIOPIC QUESTION MARK
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u1368'); // ETHIOPIC PARAGRAPH SEPARATOR
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u166E'); // CANADIAN SYLLABICS FULL STOP
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u1803'); // MONGOLIAN FULL STOP
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u1809'); // MONGOLIAN MANCHU FULL STOP
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u1944'); // LIMBU EXCLAMATION MARK
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u1945'); // LIMBU QUESTION MARK
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u1AA8'); // TAI THAM SIGN KAAN
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u1AA9'); // TAI THAM SIGN KAANKUU
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u1AAA'); // TAI THAM SIGN SATKAAN
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u1AAB'); // TAI THAM SIGN SATKAANKUU
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u1B5A'); // BALINESE PANTI
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u1B5B'); // BALINESE PAMADA
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u1B5E'); // BALINESE CARIK SIKI
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u1B5F'); // BALINESE CARIK PAREREN
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u1C3B'); // LEPCHA PUNCTUATION TA-ROL
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u1C3C'); // LEPCHA PUNCTUATION NYET THYOOM TA-ROL
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u1C7E'); // OL CHIKI PUNCTUATION MUCAAD
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u1C7F'); // OL CHIKI PUNCTUATION DOUBLE MUCAAD
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u203C'); // DOUBLE EXCLAMATION MARK
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u203D'); // INTERROBANG
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u2047'); // DOUBLE QUESTION MARK
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u2048'); // QUESTION EXCLAMATION MARK
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u2049'); // EXCLAMATION QUESTION MARK
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u2E2E'); // REVERSED QUESTION MARK
+		//		CharacterUtils.IsSentenceFinalPunctuation('\u3002'); // IDEOGRAPHIC FULL STOP
+		//		CharacterUtils.IsSentenceFinalPunctuation('\uA4FF'); // LISU PUNCTUATION FULL STOP
+		//		CharacterUtils.IsSentenceFinalPunctuation('\uA60E'); // VAI FULL STOP
+		//		CharacterUtils.IsSentenceFinalPunctuation('\uA60F'); // VAI QUESTION MARK
+		//		CharacterUtils.IsSentenceFinalPunctuation('\uA6F3'); // BAMUM FULL STOP
+		//		CharacterUtils.IsSentenceFinalPunctuation('\uA6F7'); // BAMUM QUESTION MARK
+		//		CharacterUtils.IsSentenceFinalPunctuation('\uA876'); // PHAGS-PA MARK SHAD
+		//		CharacterUtils.IsSentenceFinalPunctuation('\uA877'); // PHAGS-PA MARK DOUBLE SHAD
+		//		CharacterUtils.IsSentenceFinalPunctuation('\uA8CE'); // SAURASHTRA DANDA
+		//		CharacterUtils.IsSentenceFinalPunctuation('\uA8CF'); // SAURASHTRA DOUBLE DANDA
+		//		CharacterUtils.IsSentenceFinalPunctuation('\uA92F'); // KAYAH LI SIGN SHYA
+		//		CharacterUtils.IsSentenceFinalPunctuation('\uA9C8'); // JAVANESE PADA LINGSA
+		//		CharacterUtils.IsSentenceFinalPunctuation('\uA9C9'); // JAVANESE PADA LUNGSI
+		//		CharacterUtils.IsSentenceFinalPunctuation('\uAA5D'); // CHAM PUNCTUATION DANDA
+		//		CharacterUtils.IsSentenceFinalPunctuation('\uAA5E'); // CHAM PUNCTUATION DOUBLE DANDA
+		//		CharacterUtils.IsSentenceFinalPunctuation('\uAA5F'); // CHAM PUNCTUATION TRIPLE DANDA
+		//		CharacterUtils.IsSentenceFinalPunctuation('\uAAF0'); // MEETEI MAYEK CHEIKHAN
+		//		CharacterUtils.IsSentenceFinalPunctuation('\uAAF1'); // MEETEI MAYEK AHANG KHUDAM
+		//		CharacterUtils.IsSentenceFinalPunctuation('\uABEB'); // MEETEI MAYEK CHEIKHEI
+		//		CharacterUtils.IsSentenceFinalPunctuation('\uFE52'); // SMALL FULL STOP
+		//		CharacterUtils.IsSentenceFinalPunctuation('\uFE56'); // SMALL QUESTION MARK
+		//		CharacterUtils.IsSentenceFinalPunctuation('\uFE57'); // SMALL EXCLAMATION MARK
+		//		CharacterUtils.IsSentenceFinalPunctuation('\uFF01'); // FULLWIDTH EXCLAMATION MARK
+		//		CharacterUtils.IsSentenceFinalPunctuation('\uFF0E'); // FULLWIDTH FULL STOP
+		//		CharacterUtils.IsSentenceFinalPunctuation('\uFF1F'); // FULLWIDTH QUESTION MARK
+		//		CharacterUtils.IsSentenceFinalPunctuation('\uFF61'); // HALFWIDTH IDEOGRAPHIC FULL STOP
+		//	}
+		//	sw.Stop();
+		//	Console.WriteLine("Elapsed milliseconds: " + (sw.ElapsedTicks * 1000.0f / Stopwatch.Frequency));
+		//}
+	}
+}
---
+++ SIL.Core/Unicode/CharacterUtils.cs
@@ -0,0 +1,199 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SIL.Unicode
+{
+	public static class CharacterUtils
+	{
+		// This list MUST be kept in synch with the cases in the switch statement below.
+		// To optimize performance (switch is twice as fast as a HashSet lookup), this list
+		// is not used to implement IsSentenceFinalPunctuation.
+		private static readonly char[] s_sentenceFinalCharactersArray = { '.', '?', '!',
+				'\u00A7', // SECTION SIGN
+				'\u055C', // ARMENIAN EXCLAMATION MARK
+				'\u055E', // ARMENIAN QUESTION MARK
+				'\u0589', // ARMENIAN FULL STOP
+				'\u061F', // ARABIC QUESTION MARK
+				'\u06D4', // ARABIC FULL STOP
+				'\u0700', // SYRIAC END OF PARAGRAPH
+				'\u0701', // SYRIAC SUPRALINEAR FULL STOP
+				'\u0702', // SYRIAC SUBLINEAR FULL STOP
+				'\u07F9', // NKO EXCLAMATION MARK
+				'\u0964', // DEVANAGARI DANDA
+				'\u0965', // DEVANAGARI DOUBLE DANDA
+				'\u104A', // MYANMAR SIGN LITTLE SECTION
+				'\u104B', // MYANMAR SIGN SECTION
+				'\u1362', // ETHIOPIC FULL STOP
+				'\u1367', // ETHIOPIC QUESTION MARK
+				'\u1368', // ETHIOPIC PARAGRAPH SEPARATOR
+				'\u166E', // CANADIAN SYLLABICS FULL STOP
+				'\u1803', // MONGOLIAN FULL STOP
+				'\u1809', // MONGOLIAN MANCHU FULL STOP
+				'\u1944', // LIMBU EXCLAMATION MARK
+				'\u1945', // LIMBU QUESTION MARK
+				'\u1AA8', // TAI THAM SIGN KAAN
+				'\u1AA9', // TAI THAM SIGN KAANKUU
+				'\u1AAA', // TAI THAM SIGN SATKAAN
+				'\u1AAB', // TAI THAM SIGN SATKAANKUU
+				'\u1B5A', // BALINESE PANTI
+				'\u1B5B', // BALINESE PAMADA
+				'\u1B5E', // BALINESE CARIK SIKI
+				'\u1B5F', // BALINESE CARIK PAREREN
+				'\u1C3B', // LEPCHA PUNCTUATION TA-ROL
+				'\u1C3C', // LEPCHA PUNCTUATION NYET THYOOM TA-ROL
+				'\u1C7E', // OL CHIKI PUNCTUATION MUCAAD
+				'\u1C7F', // OL CHIKI PUNCTUATION DOUBLE MUCAAD
+				'\u203C', // DOUBLE EXCLAMATION MARK
+				'\u203D', // INTERROBANG
+				'\u2047', // DOUBLE QUESTION MARK
+				'\u2048', // QUESTION EXCLAMATION MARK
+				'\u2049', // EXCLAMATION QUESTION MARK
+				'\u2CF9', // COPTIC OLD NUBIAN FULL STOP
+				'\u2CFA', // COPTIC OLD NUBIAN DIRECT QUESTION MARK
+				'\u2CFB', // COPTIC OLD NUBIAN INDIRECT QUESTION MARK
+				'\u2CFE', // COPTIC FULL STOP
+				'\u2E2E', // REVERSED QUESTION MARK
+				'\u2E3C', // STENOGRAPHIC FULL STOP
+				'\u3002', // IDEOGRAPHIC FULL STOP
+				'\uA4FF', // LISU PUNCTUATION FULL STOP
+				'\uA60E', // VAI FULL STOP
+				'\uA60F', // VAI QUESTION MARK
+				'\uA6F3', // BAMUM FULL STOP
+				'\uA6F7', // BAMUM QUESTION MARK
+				'\uA876', // PHAGS-PA MARK SHAD
+				'\uA877', // PHAGS-PA MARK DOUBLE SHAD
+				'\uA8CE', // SAURASHTRA DANDA
+				'\uA8CF', // SAURASHTRA DOUBLE DANDA
+				'\uA92F', // KAYAH LI SIGN SHYA
+				'\uA9C8', // JAVANESE PADA LINGSA
+				'\uA9C9', // JAVANESE PADA LUNGSI
+				'\uAA5D', // CHAM PUNCTUATION DANDA
+				'\uAA5E', // CHAM PUNCTUATION DOUBLE DANDA
+				'\uAA5F', // CHAM PUNCTUATION TRIPLE DANDA
+				'\uAAF0', // MEETEI MAYEK CHEIKHAN
+				'\uAAF1', // MEETEI MAYEK AHANG KHUDAM
+				'\uABEB', // MEETEI MAYEK CHEIKHEI
+				'\uFE52', // SMALL FULL STOP
+				'\uFE56', // SMALL QUESTION MARK
+				'\uFE57', // SMALL EXCLAMATION MARK
+				'\uFF01', // FULLWIDTH EXCLAMATION MARK
+				'\uFF0E', // FULLWIDTH FULL STOP
+				'\uFF1F', // FULLWIDTH QUESTION MARK
+				'\uFF61', // HALFWIDTH IDEOGRAPHIC FULL STOP
+				// These would require surrogate pairs
+				//'\u11047', // BRAHMI DANDA
+				//'\u11048', // BRAHMI DOUBLE DANDA
+				//'\u110BE', // KAITHI SECTION MARK
+				//'\u110BF', // KAITHI DOUBLE SECTION MARK
+				//'\u110C0', // KAITHI DANDA
+				//'\u110C1', // KAITHI DOUBLE DANDA
+				//'\u11141', // CHAKMA DANDA
+				//'\u11142', // CHAKMA DOUBLE DANDA
+				//'\u11143', // CHAKMA QUESTION MARK
+				//'\u111C5', // SHARADA DANDA
+				//'\u111C6', // SHARADA DOUBLE DANDA
+			};
+
+		public static char[] SentenceFinalPunctuation
+		{
+			get { return s_sentenceFinalCharactersArray.ToArray(); }
+		}
+
+		public static bool IsSentenceFinalPunctuation(char c)
+		{
+			// The cases in this switch statement MUST be kept in synch with the list above.
+			switch (c)
+			{
+				case '.':
+				case '?':
+				case '!':
+				case '\u00A7': // SECTION SIGN
+				case '\u055C': // ARMENIAN EXCLAMATION MARK
+				case '\u055E': // ARMENIAN QUESTION MARK
+				case '\u0589': // ARMENIAN FULL STOP
+				case '\u061F': // ARABIC QUESTION MARK
+				case '\u06D4': // ARABIC FULL STOP
+				case '\u0700': // SYRIAC END OF PARAGRAPH
+				case '\u0701': // SYRIAC SUPRALINEAR FULL STOP
+				case '\u0702': // SYRIAC SUBLINEAR FULL STOP
+				case '\u07F9': // NKO EXCLAMATION MARK
+				case '\u0964': // DEVANAGARI DANDA
+				case '\u0965': // DEVANAGARI DOUBLE DANDA
+				case '\u104A': // MYANMAR SIGN LITTLE SECTION
+				case '\u104B': // MYANMAR SIGN SECTION
+				case '\u1362': // ETHIOPIC FULL STOP
+				case '\u1367': // ETHIOPIC QUESTION MARK
+				case '\u1368': // ETHIOPIC PARAGRAPH SEPARATOR
+				case '\u166E': // CANADIAN SYLLABICS FULL STOP
+				case '\u1803': // MONGOLIAN FULL STOP
+				case '\u1809': // MONGOLIAN MANCHU FULL STOP
+				case '\u1944': // LIMBU EXCLAMATION MARK
+				case '\u1945': // LIMBU QUESTION MARK
+				case '\u1AA8': // TAI THAM SIGN KAAN
+				case '\u1AA9': // TAI THAM SIGN KAANKUU
+				case '\u1AAA': // TAI THAM SIGN SATKAAN
+				case '\u1AAB': // TAI THAM SIGN SATKAANKUU
+				case '\u1B5A': // BALINESE PANTI
+				case '\u1B5B': // BALINESE PAMADA
+				case '\u1B5E': // BALINESE CARIK SIKI
+				case '\u1B5F': // BALINESE CARIK PAREREN
+				case '\u1C3B': // LEPCHA PUNCTUATION TA-ROL
+				case '\u1C3C': // LEPCHA PUNCTUATION NYET THYOOM TA-ROL
+				case '\u1C7E': // OL CHIKI PUNCTUATION MUCAAD
+				case '\u1C7F': // OL CHIKI PUNCTUATION DOUBLE MUCAAD
+				case '\u203C': // DOUBLE EXCLAMATION MARK
+				case '\u203D': // INTERROBANG
+				case '\u2047': // DOUBLE QUESTION MARK
+				case '\u2048': // QUESTION EXCLAMATION MARK
+				case '\u2049': // EXCLAMATION QUESTION MARK
+				case '\u2CF9': // COPTIC OLD NUBIAN FULL STOP
+				case '\u2CFA': // COPTIC OLD NUBIAN DIRECT QUESTION MARK
+				case '\u2CFB': // COPTIC OLD NUBIAN INDIRECT QUESTION MARK
+				case '\u2CFE': // COPTIC FULL STOP
+				case '\u2E2E': // REVERSED QUESTION MARK
+				case '\u2E3C': // STENOGRAPHIC FULL STOP
+				case '\u3002': // IDEOGRAPHIC FULL STOP
+				case '\uA4FF': // LISU PUNCTUATION FULL STOP
+				case '\uA60E': // VAI FULL STOP
+				case '\uA60F': // VAI QUESTION MARK
+				case '\uA6F3': // BAMUM FULL STOP
+				case '\uA6F7': // BAMUM QUESTION MARK
+				case '\uA876': // PHAGS-PA MARK SHAD
+				case '\uA877': // PHAGS-PA MARK DOUBLE SHAD
+				case '\uA8CE': // SAURASHTRA DANDA
+				case '\uA8CF': // SAURASHTRA DOUBLE DANDA
+				case '\uA92F': // KAYAH LI SIGN SHYA
+				case '\uA9C8': // JAVANESE PADA LINGSA
+				case '\uA9C9': // JAVANESE PADA LUNGSI
+				case '\uAA5D': // CHAM PUNCTUATION DANDA
+				case '\uAA5E': // CHAM PUNCTUATION DOUBLE DANDA
+				case '\uAA5F': // CHAM PUNCTUATION TRIPLE DANDA
+				case '\uAAF0': // MEETEI MAYEK CHEIKHAN
+				case '\uAAF1': // MEETEI MAYEK AHANG KHUDAM
+				case '\uABEB': // MEETEI MAYEK CHEIKHEI
+				case '\uFE52': // SMALL FULL STOP
+				case '\uFE56': // SMALL QUESTION MARK
+				case '\uFE57': // SMALL EXCLAMATION MARK
+				case '\uFF01': // FULLWIDTH EXCLAMATION MARK
+				case '\uFF0E': // FULLWIDTH FULL STOP
+				case '\uFF1F': // FULLWIDTH QUESTION MARK
+				case '\uFF61': // HALFWIDTH IDEOGRAPHIC FULL STOP
+				// These would require surrogate pairs
+				//'\u11047', // BRAHMI DANDA
+				//'\u11048', // BRAHMI DOUBLE DANDA
+				//'\u110BE', // KAITHI SECTION MARK
+				//'\u110BF', // KAITHI DOUBLE SECTION MARK
+				//'\u110C0', // KAITHI DANDA
+				//'\u110C1', // KAITHI DOUBLE DANDA
+				//'\u11141', // CHAKMA DANDA
+				//'\u11142', // CHAKMA DOUBLE DANDA
+				//'\u11143', // CHAKMA QUESTION MARK
+				//'\u111C5', // SHARADA DANDA
+				//'\u111C6', // SHARADA DOUBLE DANDA
+					return true;
+				default:
+					return false;
+			}
+		}
+	}
+}
