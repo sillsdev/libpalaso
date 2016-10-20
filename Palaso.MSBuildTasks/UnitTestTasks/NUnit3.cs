09a831872b27d8049142801a0d6210b1a05c541b
Eberhard Beilharz <eb1@sil.org>
2016-10-20 17:02:45 +0200

    Add new NUnit3 build task
    The new NUnit3 task has two new properties: `UseNUnit3Xml` to use the
    native XML format of NUnit 3.0 (default to NUnit 2.x format), and
    `NoColor` to display console output without color (defaults to `false`,
    i.e. will display colors).
    
    This change also changes the parameter separator character that gets
    passed to NUnit 2.x to "--" instead of using '/' or '-' depending on
    the platform. This seems to work on both Windows and Linux at least
    with NUnit 2.8.

---
+++ Palaso.MSBuildTasks/UnitTestTasks/NUnit3.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) 2016 SIL International
+// This software is licensed under the MIT license (http://opensource.org/licenses/MIT)
+using System.Text;
+
+namespace Palaso.BuildTasks.UnitTestTasks
+{
+	/// <summary>
+	/// Run NUnit on a test assembly.
+	/// </summary>
+	public class NUnit3 : NUnit
+	{
+		private bool? _useNUnit3Xml;
+
+		public bool UseNUnit3Xml
+		{
+			get { return _useNUnit3Xml.HasValue && _useNUnit3Xml.Value; }
+			set { _useNUnit3Xml = value; }
+		}
+
+		public bool NoColor { get; set; }
+
+		/// <summary>
+		/// Gets the name (without path) of the NUnit executable. When running on Mono this is
+		/// different from ProgramNameAndPath() which returns the executable we'll start.
+		/// </summary>
+		protected override string RealProgramName
+		{
+			get
+			{
+				return "nunit3-console.exe";
+			}
+		}
+
+		protected override string AddAdditionalProgramArguments()
+		{
+			var bldr = new StringBuilder();
+			//bldr.Append(" --noheader");
+			// We don't support TestInNewThread for now
+			if (!string.IsNullOrEmpty(OutputXmlFile))
+			{
+				bldr.AppendFormat(" \"--result:{0};format={1}\"", OutputXmlFile,
+					UseNUnit3Xml ? "nunit3" : "nunit2");
+			}
+			bldr.Append(" --labels=All");
+			if (NoColor)
+				bldr.Append(" --nocolor");
+			if (Force32Bit)
+				bldr.Append(" --x86");
+			return bldr.ToString();
+		}
+	}
+}
