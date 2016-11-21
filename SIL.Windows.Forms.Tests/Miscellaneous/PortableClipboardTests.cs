a48f2e3916c650cad3ae95535929f3f8dd953661
Stephen McConnel <stephen_mcconnel@sil.org>
2016-11-21 10:24:59 -0600

    Move clipboard class from Bloom to palaso (BL-4006)
    This class logically belongs to the general libary rather than a
    specific program.  It's been in use for some time in Bloom.

---
+++ SIL.Windows.Forms.Tests/Miscellaneous/PortableClipboardTests.cs
@@ -0,0 +1,81 @@
+﻿using SIL.IO;
+using SIL.Windows.Forms.ImageToolbox;
+using SIL.Windows.Forms.Miscellaneous;
+using NUnit.Framework;
+
+namespace SIL.Windows.Forms.Tests.Miscellaneous
+{
+	[TestFixture]
+	[RequiresSTA] // or you get a ThreadStateException
+	class PortableClipboardTests
+	{
+		private const string TestImageDir = "SIL.Windows.Forms.Tests/Miscellaneous/PortableClipboardTestImages";
+
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		private static string GetPathToImage(string requestedImage)
+		{
+			return FileLocator.GetFileDistributedWithApplication(TestImageDir, requestedImage);
+		}
+
+		[Test]
+		[Platform(Exclude = "Linux", Reason = "Linux code not yet available.")]
+		public void ClipboardRoundTripWorks_Png()
+		{
+			var imagePath = GetPathToImage("LineSpacing.png");
+			using (var image = PalasoImage.FromFileRobustly(imagePath))
+			{
+				PortableClipboard.CopyImageToClipboard(image);
+				using (var resultingImage = PortableClipboard.GetImageFromClipboard())
+				{
+					// There is no working PalasoImage.Equals(), so just try a few properties
+					Assert.AreEqual(image.FileName, resultingImage.FileName);
+					Assert.AreEqual(image.Image.Size, resultingImage.Image.Size);
+					Assert.AreEqual(image.Image.Flags, resultingImage.Image.Flags);
+				}
+			}
+		}
+
+		[Test]
+		[Platform(Exclude = "Linux", Reason = "Linux code not yet available.")]
+		public void ClipboardRoundTripWorks_Bmp()
+		{
+			var imagePath = GetPathToImage("PasteHS.bmp");
+			using (var image = PalasoImage.FromFileRobustly(imagePath))
+			{
+				PortableClipboard.CopyImageToClipboard(image);
+				using (var resultingImage = PortableClipboard.GetImageFromClipboard())
+				{
+					// There is no working PalasoImage.Equals(), so just try a few properties
+					Assert.AreEqual(image.FileName, resultingImage.FileName);
+					Assert.AreEqual(image.Image.Size, resultingImage.Image.Size);
+					Assert.AreEqual(image.Image.Flags, resultingImage.Image.Flags);
+				}
+			}
+		}
+
+		[Test]
+		[Platform(Exclude = "Linux", Reason = "Linux code not yet available.")]
+		public void ClipboardRoundTripWorks_GetsExistingMetadata()
+		{
+			var imagePath = GetPathToImage("AOR_EAG00864.png");
+			using (var image = PalasoImage.FromFileRobustly(imagePath))
+			{
+				var preCopyLicense = image.Metadata.License.Token;
+				var preCopyCollectionUri = image.Metadata.CollectionUri;
+				PortableClipboard.CopyImageToClipboard(image);
+				using (var resultingImage = PortableClipboard.GetImageFromClipboard())
+				{
+					// Test that the same metadata came through
+					Assert.IsTrue(resultingImage.Metadata.IsMinimallyComplete);
+					Assert.AreEqual(preCopyLicense, resultingImage.Metadata.License.Token);
+					Assert.AreEqual(preCopyCollectionUri, resultingImage.Metadata.CollectionUri);
+					Assert.AreEqual(image.Image.Flags, resultingImage.Image.Flags);
+				}
+			}
+		}
+	}
+}
