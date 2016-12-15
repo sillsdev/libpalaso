ea099faf8095cefa752ea2586a00206877175930
Daniel <dglassey@gmail.com>
2016-12-15 16:07:46 +0700

    add language data command line project

---
+++ LanguageData/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// This is a test commit
+
+namespace LanguageData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
