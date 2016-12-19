a2bd8b81b373c25d0bee02e84b88ec99e9c1c9a9
Daniel Glassey <wdg@debian.org>
2016-12-19 22:39:40 +0700

    try certificatevalidationcallback - problem only on teamcity, not locally

--- LanguageData/GetAndCheckSources.cs
+++ LanguageData/GetAndCheckSources.cs
@@ -4,6 +4,8 @@ using System.Security.Cryptography;
 using System.Text;
 using System.IO;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
 
 namespace LanguageData
 {
@@ -23,9 +25,30 @@ namespace LanguageData
 		{
 		}
 
+		public bool MyRemoteCertificateValidationCallback(System.Object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) {
+			bool isOk = true;
+			// If there are errors in the certificate chain, look at each error to determine the cause.
+			if (sslPolicyErrors != SslPolicyErrors.None) {
+				for (int i=0; i<chain.ChainStatus.Length; i++) {
+					if (chain.ChainStatus [i].Status != X509ChainStatusFlags.RevocationStatusUnknown) {
+						chain.ChainPolicy.RevocationFlag = X509RevocationFlag.EntireChain;
+						chain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
+						chain.ChainPolicy.UrlRetrievalTimeout = new TimeSpan (0, 1, 0);
+						chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllFlags;
+						bool chainIsValid = chain.Build ((X509Certificate2)certificate);
+						if (!chainIsValid) {
+							isOk = false;
+						}
+					}
+				}
+			}
+			return isOk;
+		}
+
 		public void GetNewSources ()
 		{
 			// Create web client.
+			ServicePointManager.ServerCertificateValidationCallback = MyRemoteCertificateValidationCallback;
 			WebClient client = new WebClient ();
 
 			// Download strings.
