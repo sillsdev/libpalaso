41d97bea36aa487defe3fc11b4bff58d905e56f0
Daniel Glassey <wdg@debian.org>
2017-01-04 11:37:57 +0700

    more thoughts on tests

--- LanguageData/GetAndCheckSources.cs
+++ LanguageData/GetAndCheckSources.cs
@@ -10,6 +10,7 @@ using System.Net.Security;
 // possible tests
 // verify AreFilesDifferent for 2 identical and 2 different strings
 // verify CheckSourcesAreDifferent in 2 cases, same and different
+// verify GenerateTwoToThreeCodes generates the same thing consistently on all platforms
 // what happens to GetNewSources if no net?
 // what happens to GetOldSources if input dir is bad (not exists or files not readable)?
 // what about WriteNewFiles if output dir is bad (not exists or system folder)?
