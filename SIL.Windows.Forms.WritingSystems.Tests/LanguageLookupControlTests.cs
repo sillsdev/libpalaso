4c09991b2ecbfe968a3e6a5abbe5afef45e8649d
Daniel Glassey <wdg@debian.org>
2017-01-23 18:30:36 +0700

    investigation over

--- SIL.Windows.Forms.WritingSystems.Tests/LanguageLookupControlTests.cs
+++ SIL.Windows.Forms.WritingSystems.Tests/LanguageLookupControlTests.cs
@@ -19,17 +19,12 @@ namespace SIL.Windows.Forms.WritingSystems.Tests
 			_control.ReadinessChanged += _control_ReadinessChanged;
 			_testForm = new Form();
 			_testForm.Controls.Add(_control);
-			Console.WriteLine("Setup created control");
 		}
 
 		private void _control_ReadinessChanged(object sender, EventArgs e)
 		{
-			Console.WriteLine("Test: check readiness");
 			if (_control.SelectedLanguage != null)
-			{
-				Console.WriteLine("Test: control readiness has changed");
 				_ready = true;
-			}
 		}
 
 		private void WaitForControl()
@@ -44,22 +39,15 @@ namespace SIL.Windows.Forms.WritingSystems.Tests
 		[Test]
 		public void AkanSearchDoesNotCrash()
 		{
-			Console.WriteLine("AkanSearchDoesNotCrash: starting");
 			_control.SearchText = "a";
 			_testForm.Show();
-			Console.WriteLine("AkanSearchDoesNotCrash: about to wait for control (1)");
 			WaitForControl();
-			Console.WriteLine("AkanSearchDoesNotCrash: finished waiting for control (1)");
 			_control.SearchText = "ak";
-			Console.WriteLine("AkanSearchDoesNotCrash: about to wait for control (2)");
 			WaitForControl();
-			Console.WriteLine("AkanSearchDoesNotCrash: finished waiting for control (2)");
 			Assert.AreEqual("akq", _control.SelectedLanguage.LanguageTag);
 			Assert.AreEqual("Ak", _control.DesiredLanguageName);
 			_control.SearchText = "akq";
-			Console.WriteLine("AkanSearchDoesNotCrash: about to wait for control (3)");
 			WaitForControl();
-			Console.WriteLine("AkanSearchDoesNotCrash: finished waiting for control (3)");
 			Assert.AreEqual("akq", _control.SelectedLanguage.LanguageTag);
 			Assert.AreEqual("Ak", _control.DesiredLanguageName);
 		}
