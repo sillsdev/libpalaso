c0188cce438c12ffd741e2b5832294e9be635cda
Eberhard Beilharz <eb1@sil.org>
2017-03-29 15:27:21 +0200

    Fix two issues with copying directories
    - pass `true` to File.Copy so that existant files get overwritten
    - don't hide exceptions for subdirectories but do as the method
      name promises and throw the exception

--- SIL.Core/IO/DirectoryUtilities.cs
+++ SIL.Core/IO/DirectoryUtilities.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Copyright (c) 2017 SIL International
+// This software is licensed under the MIT License (http://opensource.org/licenses/MIT)
+using System;
 using System.IO;
 using System.Linq;
 using System.Security.AccessControl;
@@ -8,7 +10,7 @@ using SIL.Reporting;
 
 namespace SIL.IO
 {
-	public class DirectoryUtilities
+	public static class DirectoryUtilities
 	{
 
 		/// <summary>
@@ -105,14 +107,14 @@ namespace SIL.IO
 			foreach (var filepath in Directory.GetFiles(sourcePath))
 			{
 				var filename = Path.GetFileName(filepath);
-				File.Copy(filepath, Path.Combine(destinationPath, filename));
+				File.Copy(filepath, Path.Combine(destinationPath, filename), true);
 			}
 
 			// Copy all the sub directorys.
 			foreach (var directorypath in Directory.GetDirectories(sourcePath))
 			{
 				var directoryname = Path.GetFileName(directorypath);
-				CopyDirectoryContents(directorypath, Path.Combine(destinationPath, directoryname));
+				CopyDirectoryWithException(directorypath, Path.Combine(destinationPath, directoryname));
 			}
 		}
 
