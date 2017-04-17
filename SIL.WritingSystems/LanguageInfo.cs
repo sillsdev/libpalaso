d7d68c53c6c2b339234701c28390a2e2fb1f0050
DALLAS\\Thomson <john_thomson@sil.org>
2017-04-17 13:06:59 -0500

    Add info about a language's primary country

--- SIL.WritingSystems/LanguageInfo.cs
+++ SIL.WritingSystems/LanguageInfo.cs
@@ -13,6 +13,13 @@ namespace SIL.WritingSystems
 		public string ThreeLetterTag { get; set; }
 		public bool IsMacroLanguage { get; set; }
 
+		/// <summary>
+		/// In a LanguageInfo retrieved from LanguageLookup, this will be the region (if any)
+		/// indicated for that language in the LanguageCodes resource.
+		/// </summary>
+		/// <remarks> Over 1000 languages return null.</remarks>
+		public RegionSubtag PrimaryRegion { get; internal set; }
+
 		public IList<string> Names
 		{
 			get { return _names; }
@@ -21,6 +28,33 @@ namespace SIL.WritingSystems
 		public ISet<string> Countries
 		{
 			get { return _countries; }
+		}
+
+		/// <summary>
+		/// The primary country where the language is spoken. In a LanguageInfo retrieved from
+		/// LanguageLookup, this should match the main title in the
+		/// Ethnologue website, where it says things like Romanian//A language of Romania
+		/// or English//A language of United Kingdom
+		/// </summary>
+		public string PrimaryCountry
+		{
+			get
+			{
+				if (PrimaryRegion != null)
+					return PrimaryRegion.Name;
+				// Two special cases for the only languages in our database that have two countries
+				// but no specified region. Following what the actual ethnologue.com site does.
+				if (LanguageTag == "itd")
+					return "Indonesia";
+				if (LanguageTag == "xak")
+					return "Venezuala";
+				// All remaining languages have a single country, so just return it.
+				// It might be an empty string.
+				if (Countries.Any())
+					return Countries.First();
+				// For completeness, but this currently never happens.
+				return "";
+			}
 		}
 
 		/// <summary>
