dff439e9d9beeb1c53bcc22035922b787584654e
andrew-polk <andrew_polk@sil.org>
2017-05-30 14:16:01 -0400

    Rename Palaso.BuildTasks to SIL.BuildTasks (and friends)

---
+++ SIL.BuildTasks.Tests/SIL.BuildTasks.Tests.Helper/Tests.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) 2016 SIL International
+// This software is licensed under the MIT License (http://opensource.org/licenses/MIT)
+
+using System;
+using NUnit.Framework;
+
+namespace SIL.BuildTasks.Tests.Helper
+{
+	/// <summary>
+	/// This class defines some tests used for testing the NUnitTask class
+	/// </summary>
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		[Category("Success")]
+		public void Success()
+		{
+			Assert.True(true, "This test always passes");
+		}
+
+		[Test]
+		[Category("Failing")]
+		public void Failing()
+		{
+			Assert.Fail("This test intentionally fails");
+		}
+
+		[Test]
+		[Category("Exception")]
+		public void Exception()
+		{
+			throw new ApplicationException("This test throws an exception");
+		}
+
+		#region ThrowsExceptionInFinalizer
+		/// <summary>
+		/// This class simulates a class that throws an exception when the finalizer runs
+		/// </summary>
+		class ThrowsExceptionInFinalizer
+		{
+			~ThrowsExceptionInFinalizer()
+			{
+				throw new ApplicationException();
+			}
+
+			public bool DoSomething()
+			{
+				// just some arbitrary method so that we can instantiate the class
+				return true;
+			}
+		}
+		#endregion
+
+		[Test]
+		[Category("Finalizer")]
+		public void Finalizer()
+		{
+			Assert.That(new ThrowsExceptionInFinalizer().DoSomething(), Is.True);
+			// will throw an exception sometime later when GC runs
+		}
+	}
+}
