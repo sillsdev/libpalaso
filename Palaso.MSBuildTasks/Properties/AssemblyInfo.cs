374c09ca2b4ad105b4c0d870ff86a292995706ec
andrew-polk <andrew_polk@sil.org>
2017-06-01 11:17:00 -0400

    Remove superflous reference to SIL.Core in Palaso.BuildTasks
    The acknowledgment can be safely removed because it will never be read regardless

--- Palaso.MSBuildTasks/Properties/AssemblyInfo.cs
+++ Palaso.MSBuildTasks/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
-﻿using SIL.Acknowledgements;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -19,7 +18,3 @@ using System.Runtime.InteropServices;
 [assembly: Guid("8a17ca29-630a-4869-8a66-aa05cf5538ba")]
 
 [assembly: InternalsVisibleTo("Palaso.BuildTask.Tests")]
-
-[assembly: Acknowledgement("MarkdownDeep", Url = "https://www.nuget.org/packages/MarkdownDeep.NET/",
-	LicenseUrl = "https://opensource.org/licenses/Apache-2.0", Copyright = "Copyright © 2010-2011 Topten Software",
-	Location = "./MarkdownDeep.dll")]
