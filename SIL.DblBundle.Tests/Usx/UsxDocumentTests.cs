f0d14e0836c4a85c698be4e68e98a250ef8e92f1
andrew-polk <andrew_polk@sil.org>
2017-06-02 10:19:15 -0400

    (Re)Make some constants and methods public which are used by Glyssen

--- SIL.DblBundle.Tests/Usx/UsxDocumentTests.cs
+++ SIL.DblBundle.Tests/Usx/UsxDocumentTests.cs
@@ -6,34 +6,36 @@ namespace SIL.DblBundle.Tests.Usx
 {
 	/// <summary>
 	/// Tests the UsxDocument class.
+	///
+	/// Note: This class also provides constants and helper methods which are used by Glyssen. They must remain public.
 	/// </summary>
 	[TestFixture]
 	public class UsxDocumentTests
 	{
-		private const string kUsxFrameStart = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+		public const string kUsxFrameStart = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
 			"<usx version=\"2.0\">" +
 			"<book code=\"MRK\" style=\"id\">Acholi Bible 1985 Digitised by Africa Typesetting Network for DBL April 2013</book>";
-		private const string kUsxChapter1AndContentPlaceholder = "<chapter number=\"1\" style=\"c\" />{0}";
-		private const string kUsxFrameEnd = "</usx>";
-		private const string kUsxFrame =
+		public const string kUsxChapter1AndContentPlaceholder = "<chapter number=\"1\" style=\"c\" />{0}";
+		public const string kUsxFrameEnd = "</usx>";
+		public const string kUsxFrame =
 			kUsxFrameStart +
 			kUsxChapter1AndContentPlaceholder +
 			kUsxFrameEnd;
 		private const string kParaNodeText = "<para style=\"p\"><verse number=\"1\" style=\"v\" />Verse One Text. <verse number=\"2\" style=\"v\" />Verse Two Text.</para>";
 
-		private static XmlDocument CreateMarkOneDoc(string paraXmlNodes, string usxFrame = kUsxFrame)
+		public static XmlDocument CreateMarkOneDoc(string paraXmlNodes, string usxFrame = kUsxFrame)
 		{
 			return CreateDocFromString(string.Format(usxFrame, paraXmlNodes));
 		}
 
-		private static XmlDocument CreateDocFromString(string xml)
+		public static XmlDocument CreateDocFromString(string xml)
 		{
 			var xmlDoc = new XmlDocument { PreserveWhitespace = true };
 			xmlDoc.LoadXml(xml);
 			return xmlDoc;
 		}
 
-		internal static XmlNodeList GetChaptersAndParasForMarkOneContaining2Verses()
+		public static XmlNodeList GetChaptersAndParasForMarkOneContaining2Verses()
 		{
 			var usxDocument = new UsxDocument(CreateMarkOneDoc(kParaNodeText));
 			return usxDocument.GetChaptersAndParas();
