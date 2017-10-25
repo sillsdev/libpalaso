e8f748e6f1fab67f8494933e4525142f121e3241
andrew-polk <andrew_polk@sil.org>
2017-10-25 16:42:54 -0400

    Enable the reading of the new type element for metadata 2.x

--- SIL.DblBundle.Tests/DBLMetadataTests.cs
+++ SIL.DblBundle.Tests/DBLMetadataTests.cs
@@ -11,82 +11,53 @@ using SIL.TestUtilities;
 
 namespace SIL.DblBundle.Tests
 {
+	[TestFixture]
 	class DblMetadataTests
 	{
-		private DblTextMetadata<DblMetadataLanguage> m_metadata;
-
-		private const string TestXml =
-@"<?xml version=""1.0"" encoding=""UTF-8""?>
-<DBLMetadata id=""3b9fdc679b9319c3"" revision=""1"" mediatype=""text"" typeVersion=""1.3"">
-	<identification>
-		<name>Acholi New Testament 1985</name>
-		<nameLocal>Acoli Baibul 1985</nameLocal>
-		<systemId type=""tms"">b9236acd-66f3-44d0-98fc-3970b3d017cd</systemId>
-		<systemId type=""paratext"">3b9fdc679b9319c3ee45ab86cc1c0c42930c2979</systemId>
-	</identification>
-	<language>
-		<iso>ach</iso>
-	</language>
-	<copyright>
-		<statement contentType=""xhtml"">© 2015, SIL Inc. All rights reserved.</statement>
-	</copyright>
-	<promotion>
-		<promoVersionInfo contentType=""xhtml"">
-			<h1>Acholi New Testament 1985</h1>
-			<p>This translation, published by the Bible Society of Uganda, was first published in 1985.</p>
-			<p>If you are interested in obtaining a printed copy, please contact the Bible Society of Uganda at <a href=""http://www.biblesociety-uganda.org/"">www.biblesociety-uganda.org</a>.</p>
-		</promoVersionInfo>
-		<promoEmail contentType=""xhtml"">
-			<p>Hi YouVersion friend,</p>
-			<p>Sincerely, Your Friends at YouVersion</p>
-		</promoEmail>
-	</promotion>
-	<archiveStatus>
-		<archivistName>Emma Canales -archivist</archivistName>
-		<dateArchived>2014-05-28T15:18:31.080800</dateArchived>
-		<dateUpdated>2014-05-28T15:18:31.080800</dateUpdated>
-		<comments>First submit</comments>
-	</archiveStatus>
-</DBLMetadata>";
+		private DblTextMetadata<DblMetadataLanguage> _metadata;
+		private DblTextMetadata<DblMetadataLanguage> _metadata2;
 
 		[TestFixtureSetUp]
 		public void TestFixtureSetup()
 		{
 			var xs = new XmlSerializer(typeof(DblTextMetadata<DblMetadataLanguage>));
-			using (TextReader reader = new StringReader(TestXml))
-				m_metadata = (DblTextMetadata<DblMetadataLanguage>)xs.Deserialize(reader);
+			using (TextReader reader = new StringReader(Resources.AcholiMetadataVersion1_5_xml))
+				_metadata = (DblTextMetadata<DblMetadataLanguage>)xs.Deserialize(reader);
+
+			using (TextReader reader = new StringReader(Resources.AcholiMetadataVersion2_1_xml))
+				_metadata2 = (DblTextMetadata<DblMetadataLanguage>)xs.Deserialize(reader);
 		}
 
 		[Test]
 		public void GetId()
 		{
-			Assert.AreEqual("3b9fdc679b9319c3", m_metadata.Id);
+			Assert.AreEqual("3b9fdc679b9319c3", _metadata.Id);
 		}
 
 		[Test]
 		public void GetName()
 		{
-			Assert.AreEqual("Acholi New Testament 1985", m_metadata.Identification.Name);
+			Assert.AreEqual("Acholi New Testament 1985", _metadata.Identification.Name);
 		}
 
 		[Test]
 		public void GetParatextSystemId()
 		{
-			Assert.AreEqual("3b9fdc679b9319c3ee45ab86cc1c0c42930c2979", m_metadata.Identification.SystemIds.FirstOrDefault(sid => sid.Type.Equals("paratext")).Id);
+			Assert.AreEqual("3b9fdc679b9319c3ee45ab86cc1c0c42930c2979", _metadata.Identification.SystemIds.FirstOrDefault(sid => sid.Type.Equals("paratext")).Id);
 		}
 
 		[Test]
 		public void GetLanguageIso()
 		{
-			Assert.AreEqual("ach", m_metadata.Language.Iso);
+			Assert.AreEqual("ach", _metadata.Language.Iso);
 		}
 
 		[Test]
 		public void GetCopyrightStatement()
 		{
-			const string expectedValue = @"© 2015, SIL Inc. All rights reserved.";
-			Assert.AreEqual(expectedValue, m_metadata.Copyright.Statement.Xhtml);
-			Assert.AreEqual("xhtml", m_metadata.Copyright.Statement.ContentType);
+			const string expectedValue = "<p>© 1985 The Bible Society of Uganda</p>";
+			Assert.AreEqual(expectedValue, _metadata.Copyright.Statement.Xhtml);
+			Assert.AreEqual("xhtml", _metadata.Copyright.Statement.ContentType);
 		}
 
 		[Test]
@@ -95,21 +66,21 @@ namespace SIL.DblBundle.Tests
 			const string expectedValue = @"<h1>Acholi New Testament 1985</h1><p>This translation, published by the Bible Society " +
 				@"of Uganda, was first published in 1985.</p><p>If you are interested in obtaining a printed copy, please contact " +
 				@"the Bible Society of Uganda at <a href=""http://www.biblesociety-uganda.org/"">www.biblesociety-uganda.org</a>.</p>";
-			Assert.AreEqual(expectedValue, m_metadata.Promotion.PromoVersionInfo.Xhtml);
-			Assert.AreEqual("xhtml", m_metadata.Promotion.PromoVersionInfo.ContentType);
-		}
-
-		[Test]
-		public void GetPromoEmail()
-		{
-			const string expectedValue = @"<p>Hi YouVersion friend,</p><p>Sincerely, Your Friends at YouVersion</p>";
-			Assert.AreEqual(expectedValue, m_metadata.Promotion.PromoEmail.Xhtml);
+			Assert.AreEqual(expectedValue, _metadata.Promotion.PromoVersionInfo.Xhtml);
+			Assert.AreEqual("xhtml", _metadata.Promotion.PromoVersionInfo.ContentType);
 		}
 
 		[Test]
 		public void GetDateArchived()
 		{
-			Assert.AreEqual("2014-05-28T15:18:31.080800", m_metadata.ArchiveStatus.DateArchived);
+			Assert.AreEqual("2014-05-28T15:18:31.080800", _metadata.ArchiveStatus.DateArchived);
+		}
+
+		[Test]
+		public void IsTextReleaseBundle()
+		{
+			Assert.True(_metadata.IsTextReleaseBundle);
+			Assert.True(_metadata2.IsTextReleaseBundle);
 		}
 
 		[Test]
