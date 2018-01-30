ce2373f6563ca1d003ec7e2b6864e33c8366024a
Eberhard Beilharz <eb1@sil.org>
2018-01-30 08:12:47 +0100

    Small refactoring

--- SIL.Core/Email/MacOsXEmailProvider.cs
+++ SIL.Core/Email/MacOsXEmailProvider.cs
@@ -1,6 +1,7 @@
 // Copyright (c) 2018 SIL International
 // This software is licensed under the MIT License (http://opensource.org/licenses/MIT)
 
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
 
@@ -22,21 +23,10 @@ namespace SIL.Email
 			bldr.AppendLine($"set subject to \"{message.Subject}\"");
 			bldr.AppendLine($"set content to \"{message.Body}\"");
 			bldr.AppendLine("-- set visible to true");
-			foreach (var recpt in message.To)
-			{
-				bldr.AppendLine(
-					$"make new to recipient at end of to recipients with properties {{address:\"{recpt}\"}}");
-			}
-			foreach (var recpt in message.Cc)
-			{
-				bldr.AppendLine(
-					$"make new to recipient at end of cc recipients with properties {{address:\"{recpt}\"}}");
-			}
-			foreach (var recpt in message.Bcc)
-			{
-				bldr.AppendLine(
-					$"make new to recipient at end of bcc recipients with properties {{address:\"{recpt}\"}}");
-			}
+
+			bldr.Append(AddRecipients("to", message.To));
+			bldr.Append(AddRecipients("cc", message.Cc));
+			bldr.Append(AddRecipients("bcc", message.Bcc));
 
 			foreach (var attachment in message.AttachmentFilePath)
 			{
@@ -49,6 +39,18 @@ namespace SIL.Email
 			startInfo.Arguments = bldr.ToString();
 
 			return SendEmail(startInfo);
+		}
+
+		private static string AddRecipients(string what, IEnumerable<string> recipients)
+		{
+			var bldr = new StringBuilder();
+			foreach (var recpt in recipients)
+			{
+				bldr.AppendLine(
+					$"make new to recipient at end of {what} recipients with properties {{address:\"{recpt}\"}}");
+			}
+
+			return bldr.ToString();
 		}
 
 		protected virtual bool SendEmail(ProcessStartInfo startInfo)
