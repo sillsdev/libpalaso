fd44878290ed2be6c69255ec89cc2c39820dc612
DALLAS\Thomson <john_thomson@sil.org>
2018-01-31 16:49:25 -0600

    BL-5416 Improve RobustIO.LoadXElement NR path names

--- SIL.Core.Tests/IO/RobustIOTests.cs
+++ SIL.Core.Tests/IO/RobustIOTests.cs
@@ -5,8 +5,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Xml.Linq;
 using NUnit.Framework;
 using SIL.IO;
+using SIL.TestUtilities;
 
 namespace SIL.Tests.IO
 {
@@ -26,6 +28,23 @@ namespace SIL.Tests.IO
 				// Not a great test, since it captures some non-essential features of how XmlDocument writes its content.
 				// But this is at least a reasonable result to get from doing the above.
 				Assert.That(File.ReadAllText(temp.Path, Encoding.UTF8), Is.EqualTo("<?xml version=\"1.0\" encoding=\"utf-8\"?>" + Environment.NewLine + "<root>This is a test</root>"));
+			}
+		}
+
+		// This attempts to be a regression test for a problem (Bloom BL-5416) in using
+		// RobustIO.LoadXElement() with a file name that contained some RTL script.
+		// However, I could not come up with a unit test in which the old code actually failed,
+		// though it definitely and repeatably failed in Bloom using as far as I can tell
+		// these exact strings in the path (though in Bloom's document folder, not Temp).
+		[Test]
+		public void LoadXElement_WithNRFileName_DoesNotCrash()
+		{
+			using (var folder = new TemporaryFolder("Balangao کتابونه"))
+			{
+				var input = new XElement("root");
+				var fileName = Path.Combine(folder.Path, "Balangao کتابونه.xml");
+				input.Save(fileName);
+				Assert.DoesNotThrow(() => RobustIO.LoadXElement(fileName));
 			}
 		}
 	}
