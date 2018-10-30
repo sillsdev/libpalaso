7f268529f0feabebb09b982711b624329d9dee5b
glasseyes <dglassey@gmail.com>
2018-10-30 14:39:15 +0700

    add OfflineSldr to SIL.Archiving.Tests

--- SIL.Archiving.Tests/Properties/AssemblyInfo.cs
+++ SIL.Archiving.Tests/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
+using SIL.TestUtilities;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -15,3 +16,5 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("49f40cd3-46aa-4d25-b787-868040c9cbc8")]
+
+[assembly: OfflineSldr]
