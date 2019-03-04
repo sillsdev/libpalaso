14433d10c6c55d952b3bc66574eba952a4ec8271
sam <sam147doss@gmail.com>
2019-03-04 20:07:21 +0530

    Fix: LT-19564 Improve LCM Project Sharing
     * Added the feature to enable project sharing

--- SIL.Lexicon.Tests/ProjectLexiconSettingsDataMapperTests.cs
+++ SIL.Lexicon.Tests/ProjectLexiconSettingsDataMapperTests.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Xml.Linq;
 using NUnit.Framework;
 
@@ -11,7 +11,7 @@ namespace SIL.Lexicon.Tests
 		public void Read_ValidXml_SetsAllProperties()
 		{
 			const string projectSettingsXml =
-@"<ProjectLexiconSettings>
+@"<ProjectLexiconSettings projectSharing=""true"">
   <WritingSystems addToSldr=""true"">
     <WritingSystem id=""fr-FR"">
       <SpellCheckingId>fr_FR</SpellCheckingId>
@@ -26,6 +26,7 @@ namespace SIL.Lexicon.Tests
 			var settings = new ProjectLexiconSettings();
 			projectSettingsDataMapper.Read(settings);
 			Assert.That(settings.AddWritingSystemsToSldr, Is.True);
+			Assert.That(settings.ProjectSharing, Is.True);
 		}
 
 		[Test]
@@ -45,11 +46,11 @@ namespace SIL.Lexicon.Tests
 			var settingsStore = new MemorySettingsStore();
 			var projectSettingsDataMapper = new ProjectLexiconSettingsDataMapper(settingsStore);
 
-			var settings = new ProjectLexiconSettings {AddWritingSystemsToSldr = true};
+			var settings = new ProjectLexiconSettings {AddWritingSystemsToSldr = true, ProjectSharing = true};
 			projectSettingsDataMapper.Write(settings);
 
 			Assert.That(settingsStore.SettingsElement, Is.EqualTo(XElement.Parse(
-@"<ProjectLexiconSettings>
+@"<ProjectLexiconSettings projectSharing=""true"">
   <WritingSystems addToSldr=""true"" />
 </ProjectLexiconSettings>")).Using((IEqualityComparer<XNode>) new XNodeEqualityComparer()));
 		}
@@ -58,7 +59,7 @@ namespace SIL.Lexicon.Tests
 		public void Write_ValidXml_XmlUpdated()
 		{
 			const string projectSettingsXml =
-@"<ProjectLexiconSettings>
+@"<ProjectLexiconSettings projectSharing=""false"">
   <WritingSystems addToSldr=""false"">
     <WritingSystem id=""fr-FR"">
       <SpellCheckingId>fr_FR</SpellCheckingId>
@@ -70,11 +71,11 @@ namespace SIL.Lexicon.Tests
 
 			var settingsStore = new MemorySettingsStore {SettingsElement = XElement.Parse(projectSettingsXml)};
 			var projectSettingsDataMapper = new ProjectLexiconSettingsDataMapper(settingsStore);
-			var settings = new ProjectLexiconSettings {AddWritingSystemsToSldr = true};
+			var settings = new ProjectLexiconSettings {AddWritingSystemsToSldr = true, ProjectSharing = true };
 			projectSettingsDataMapper.Write(settings);
 
 			Assert.That(settingsStore.SettingsElement, Is.EqualTo(XElement.Parse(
-@"<ProjectLexiconSettings>
+@"<ProjectLexiconSettings projectSharing=""true"">
   <WritingSystems addToSldr=""true"">
     <WritingSystem id=""fr-FR"">
       <SpellCheckingId>fr_FR</SpellCheckingId>
@@ -83,6 +84,37 @@ namespace SIL.Lexicon.Tests
     </WritingSystem>
   </WritingSystems>
 </ProjectLexiconSettings>")).Using((IEqualityComparer<XNode>) new XNodeEqualityComparer()));
+		}
+
+		[Test]
+		public void ProjectSharing_Is_Unchecked()
+		{
+			const string projectSettingsXml =
+				@"<ProjectLexiconSettings projectSharing=""true"">
+  <WritingSystems addToSldr=""false"">
+	<WritingSystem id=""fr-FR"">
+		<SpellCheckingId>fr_FR</SpellCheckingId>
+		<LegacyMapping>converter</LegacyMapping>
+		<Keyboard>Old Keyboard</Keyboard>
+	</WritingSystem>
+</WritingSystems>
+</ProjectLexiconSettings>";
+
+			var settingsStore = new MemorySettingsStore { SettingsElement = XElement.Parse(projectSettingsXml) };
+			var projectSettingsDataMapper = new ProjectLexiconSettingsDataMapper(settingsStore);
+			var settings = new ProjectLexiconSettings { AddWritingSystemsToSldr = true, ProjectSharing = false };
+			projectSettingsDataMapper.Write(settings);
+
+			Assert.That(settingsStore.SettingsElement, Is.EqualTo(XElement.Parse(
+				@"<ProjectLexiconSettings projectSharing=""false"">
+  <WritingSystems addToSldr=""true"">
+	<WritingSystem id=""fr-FR"">
+		<SpellCheckingId>fr_FR</SpellCheckingId>
+		<LegacyMapping>converter</LegacyMapping>
+		<Keyboard>Old Keyboard</Keyboard>
+	</WritingSystem>
+</WritingSystems>
+</ProjectLexiconSettings>")).Using((IEqualityComparer<XNode>)new XNodeEqualityComparer()));
 		}
 	}
 }
