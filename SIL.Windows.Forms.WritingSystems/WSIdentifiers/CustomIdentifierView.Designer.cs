3185e6e2dbab430c7c64f1bf994a19c06d5cc0d5
Jason Naylor <jason_naylor@sil.org>
2019-04-25 15:45:55 -0500

    Add localization to writing system identifier controls
    * Add the L10NSharpExtender to controls and add comments for localizers
    * Also change 'Unspecified' to 'Default' in IpaIdentifier view to
      discourage novice users from changing it
    * Also fix some typos in LicenseInfo code comments

--- SIL.Windows.Forms.WritingSystems/WSIdentifiers/CustomIdentifierView.Designer.cs
+++ SIL.Windows.Forms.WritingSystems/WSIdentifiers/CustomIdentifierView.Designer.cs
@@ -1,4 +1,4 @@
-﻿using SIL.Windows.Forms.Widgets;
+using SIL.Windows.Forms.Widgets;
 
 namespace SIL.Windows.Forms.WritingSystems.WSIdentifiers
 {
@@ -30,24 +30,33 @@ namespace SIL.Windows.Forms.WritingSystems.WSIdentifiers
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
 			this._languageTag = new System.Windows.Forms.TextBox();
 			this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-			this.betterLabel1 = new BetterLabel();
+			this.betterLabel1 = new SIL.Windows.Forms.Widgets.BetterLabel();
+			this._l10NSharpExtender = new L10NSharp.UI.L10NSharpExtender(this.components);
+			((System.ComponentModel.ISupportInitialize)(this._l10NSharpExtender)).BeginInit();
 			this.SuspendLayout();
-			//
+			// 
 			// _languageTag
-			//
-			this._languageTag.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-						| System.Windows.Forms.AnchorStyles.Right)));
+			// 
+			this._languageTag.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this._l10NSharpExtender.SetLocalizableToolTip(this._languageTag, null);
+			this._l10NSharpExtender.SetLocalizationComment(this._languageTag, null);
+			this._l10NSharpExtender.SetLocalizingId(this._languageTag, "CustomIdentifierView._languageTag");
 			this._languageTag.Location = new System.Drawing.Point(84, 18);
 			this._languageTag.Name = "_languageTag";
 			this._languageTag.Size = new System.Drawing.Size(183, 20);
 			this._languageTag.TabIndex = 1;
 			this._languageTag.TextChanged += new System.EventHandler(this._languageTag_TextChanged);
-			//
+			// 
 			// linkLabel1
-			//
+			// 
 			this.linkLabel1.AutoSize = true;
+			this._l10NSharpExtender.SetLocalizableToolTip(this.linkLabel1, null);
+			this._l10NSharpExtender.SetLocalizationComment(this.linkLabel1, "text used in the link to a webpage that describes the language identifiers");
+			this._l10NSharpExtender.SetLocalizingId(this.linkLabel1, "CustomIdentifierView.linkLabel1");
 			this.linkLabel1.Location = new System.Drawing.Point(-1, 44);
 			this.linkLabel1.Name = "linkLabel1";
 			this.linkLabel1.Size = new System.Drawing.Size(213, 13);
@@ -55,28 +64,43 @@ namespace SIL.Windows.Forms.WritingSystems.WSIdentifiers
 			this.linkLabel1.TabStop = true;
 			this.linkLabel1.Text = "Read about language identifiers on the web";
 			this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
-			//
+			// 
 			// betterLabel1
-			//
+			// 
 			this.betterLabel1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.betterLabel1.Enabled = false;
+			this.betterLabel1.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.betterLabel1.IsTextSelectable = false;
+			this._l10NSharpExtender.SetLocalizableToolTip(this.betterLabel1, null);
+			this._l10NSharpExtender.SetLocalizationComment(this.betterLabel1, "label describes the code for a custom identifier. Translate only \'code\'");
+			this._l10NSharpExtender.SetLocalizingId(this.betterLabel1, "CustomIdentifierView.betterLabel1");
 			this.betterLabel1.Location = new System.Drawing.Point(4, 18);
 			this.betterLabel1.Multiline = true;
 			this.betterLabel1.Name = "betterLabel1";
 			this.betterLabel1.ReadOnly = true;
-			this.betterLabel1.Size = new System.Drawing.Size(86, 20);
+			this.betterLabel1.Size = new System.Drawing.Size(86, 13);
 			this.betterLabel1.TabIndex = 0;
 			this.betterLabel1.TabStop = false;
 			this.betterLabel1.Text = "RFC5646 code:";
-			//
+			// 
+			// _l10NSharpExtender
+			// 
+			this._l10NSharpExtender.LocalizationManagerId = null;
+			this._l10NSharpExtender.PrefixForNewItems = null;
+			// 
 			// CustomIdentifierView
-			//
+			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.Controls.Add(this.linkLabel1);
 			this.Controls.Add(this._languageTag);
 			this.Controls.Add(this.betterLabel1);
+			this._l10NSharpExtender.SetLocalizableToolTip(this, null);
+			this._l10NSharpExtender.SetLocalizationComment(this, null);
+			this._l10NSharpExtender.SetLocalizingId(this, "CustomIdentifierView.CustomIdentifierView");
 			this.Name = "CustomIdentifierView";
 			this.Size = new System.Drawing.Size(270, 76);
+			((System.ComponentModel.ISupportInitialize)(this._l10NSharpExtender)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -87,5 +111,6 @@ namespace SIL.Windows.Forms.WritingSystems.WSIdentifiers
 		private BetterLabel betterLabel1;
 		private System.Windows.Forms.TextBox _languageTag;
 		private System.Windows.Forms.LinkLabel linkLabel1;
+		private L10NSharp.UI.L10NSharpExtender _l10NSharpExtender;
 	}
 }
--- SIL.Windows.Forms.WritingSystems/WSIdentifiers/IpaIdentifierView.Designer.cs
+++ SIL.Windows.Forms.WritingSystems/WSIdentifiers/IpaIdentifierView.Designer.cs
@@ -1,4 +1,4 @@
-﻿using SIL.Windows.Forms.Widgets;
+using SIL.Windows.Forms.Widgets;
 
 namespace SIL.Windows.Forms.WritingSystems.WSIdentifiers
 {
@@ -30,14 +30,21 @@ namespace SIL.Windows.Forms.WritingSystems.WSIdentifiers
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
 			this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-			this.comboBox1 = new System.Windows.Forms.ComboBox();
-			this.betterLabel1 = new BetterLabel();
+			this._purposeComboBox = new System.Windows.Forms.ComboBox();
+			this.betterLabel1 = new SIL.Windows.Forms.Widgets.BetterLabel();
+			this._localizationHelper = new L10NSharp.UI.L10NSharpExtender(this.components);
+			((System.ComponentModel.ISupportInitialize)(this._localizationHelper)).BeginInit();
 			this.SuspendLayout();
-			//
+			// 
 			// linkLabel1
-			//
+			// 
 			this.linkLabel1.AutoSize = true;
+			this._localizationHelper.SetLocalizableToolTip(this.linkLabel1, null);
+			this._localizationHelper.SetLocalizationComment(this.linkLabel1, "In writing system setup when IPA special option is selected: text used in link to" +
+        " a webpage describing IPA transcription");
+			this._localizationHelper.SetLocalizingId(this.linkLabel1, "IpaIdentifierView.linkLabel1");
 			this.linkLabel1.Location = new System.Drawing.Point(3, 44);
 			this.linkLabel1.Name = "linkLabel1";
 			this.linkLabel1.Size = new System.Drawing.Size(199, 13);
@@ -45,46 +52,61 @@ namespace SIL.Windows.Forms.WritingSystems.WSIdentifiers
 			this.linkLabel1.TabStop = true;
 			this.linkLabel1.Text = "Read about IPA transcription on the web";
 			this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
-			//
-			// comboBox1
-			//
-			this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-						| System.Windows.Forms.AnchorStyles.Right)));
-			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-			this.comboBox1.FormattingEnabled = true;
-			this.comboBox1.Items.AddRange(new object[] {
-			"Unspecified",
-			"Etic (raw phonetic transcription)",
-			"Emic (uses phonology of the language)"});
-			this.comboBox1.Location = new System.Drawing.Point(84, 15);
-			this.comboBox1.Name = "comboBox1";
-			this.comboBox1.Size = new System.Drawing.Size(175, 21);
-			this.comboBox1.TabIndex = 3;
-			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
-			//
+			// 
+			// _purposeComboBox
+			// 
+			this._purposeComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this._purposeComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this._purposeComboBox.FormattingEnabled = true;
+			this._localizationHelper.SetLocalizableToolTip(this._purposeComboBox, null);
+			this._localizationHelper.SetLocalizationComment(this._purposeComboBox, null);
+			this._localizationHelper.SetLocalizingId(this._purposeComboBox, "IpaIdentifierView._purposeComboBox");
+			this._purposeComboBox.Location = new System.Drawing.Point(84, 15);
+			this._purposeComboBox.Name = "_purposeComboBox";
+			this._purposeComboBox.Size = new System.Drawing.Size(202, 21);
+			this._purposeComboBox.TabIndex = 3;
+			this._purposeComboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
 			// betterLabel1
-			//
-			this.betterLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-						| System.Windows.Forms.AnchorStyles.Right)));
+			// 
+			this.betterLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
 			this.betterLabel1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.betterLabel1.Enabled = false;
+			this.betterLabel1.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.betterLabel1.IsTextSelectable = false;
+			this._localizationHelper.SetLocalizableToolTip(this.betterLabel1, null);
+			this._localizationHelper.SetLocalizationComment(this.betterLabel1, "In writing system setup when IPA special option is selected: Label for the purpos" +
+        "e combo box");
+			this._localizationHelper.SetLocalizingId(this.betterLabel1, "IpaIdentifierView.betterLabel1");
 			this.betterLabel1.Location = new System.Drawing.Point(4, 18);
 			this.betterLabel1.Multiline = true;
 			this.betterLabel1.Name = "betterLabel1";
 			this.betterLabel1.ReadOnly = true;
-			this.betterLabel1.Size = new System.Drawing.Size(74, 20);
+			this.betterLabel1.Size = new System.Drawing.Size(101, 13);
 			this.betterLabel1.TabIndex = 0;
 			this.betterLabel1.TabStop = false;
 			this.betterLabel1.Text = "Purpose:";
-			//
+			// 
+			// _localizationHelper
+			// 
+			this._localizationHelper.LocalizationManagerId = null;
+			this._localizationHelper.PrefixForNewItems = null;
+			// 
 			// IpaIdentifierView
-			//
+			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this._purposeComboBox);
 			this.Controls.Add(this.linkLabel1);
 			this.Controls.Add(this.betterLabel1);
+			this._localizationHelper.SetLocalizableToolTip(this, null);
+			this._localizationHelper.SetLocalizationComment(this, null);
+			this._localizationHelper.SetLocalizingId(this, "IpaIdentifierView.IpaIdentifierView");
 			this.Name = "IpaIdentifierView";
-			this.Size = new System.Drawing.Size(259, 76);
+			this.Size = new System.Drawing.Size(286, 78);
+			((System.ComponentModel.ISupportInitialize)(this._localizationHelper)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -94,6 +116,7 @@ namespace SIL.Windows.Forms.WritingSystems.WSIdentifiers
 
 		private BetterLabel betterLabel1;
 		private System.Windows.Forms.LinkLabel linkLabel1;
-		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.ComboBox _purposeComboBox;
+		private L10NSharp.UI.L10NSharpExtender _localizationHelper;
 	}
 }
