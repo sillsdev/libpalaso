deebc2e71a5d4a1b3e7b144657ddcde1ba31f715
Eberhard Beilharz <eb1@sil.org>
2019-11-27 12:09:12 +0100

    [SIL.Core] Improve ConsoleExceptionHandler.DisplayError
    Output at least some information in DisplayError.

--- SIL.Core/Reporting/ConsoleExceptionHandler.cs
+++ SIL.Core/Reporting/ConsoleExceptionHandler.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace SIL.Reporting
 {
@@ -17,87 +19,44 @@ namespace SIL.Reporting
 			AppDomain.CurrentDomain.UnhandledException += HandleUnhandledException;
 		}
 
-		protected override bool ShowUI
-		{
-			get { return false; }
-		}
+		protected override bool ShowUI => false;
 
 		protected override bool DisplayError(Exception exception)
 		{
-
+			try
 			{
-				//try
-				//{
-				//    if (exception.InnerException != null)
-				//    {
-				//        Debug.WriteLine(string.Format("Exception: {0} {1}", exception.Message, exception.InnerException.Message));
-				//        Logger.WriteEvent("Exception: {0} {1}", exception.Message, exception.InnerException.Message);
-				//    }
-				//    else
-				//    {
-				//        Debug.WriteLine(String.Format("Exception: {0}", exception.Message));
-				//        Logger.WriteEvent("Exception: {0}", exception.Message);
-				//    }
+				if (exception.InnerException != null)
+				{
+					Debug.WriteLine(
+						$"Exception: {exception.Message} {exception.InnerException.Message}");
+					Logger.WriteError(exception.Message, exception.InnerException);
+				}
+				else
+				{
+					Debug.WriteLine($"Exception: {exception.Message}");
+					Logger.WriteError(exception.Message, exception);
+				}
 
-				//    if (exception is ExternalException
-				//        && (uint)(((ExternalException)exception).ErrorCode) == 0x8007000E) // E_OUTOFMEMORY
-				//    {
-				//        if (showUI)
-				//            ExceptionReportingDialog.ReportException(exception);//, parent);
-				//        else
-				//        {
-				//            Trace.Fail("Out of memory");
-				//            //                        Trace.Assert(false, FwApp.GetResourceString("kstidMiscError"),
-				//            //                            FwApp.GetResourceString("kstidOutOfMemory"));
-				//        }
-				//    }
-				//    else
-				//    {
-				//        Debug.Assert(exception.Message != string.Empty || exception is COMException,
-				//            "Oops - we got an empty exception description. Change the code to handle that!");
-
-				//        if (showUI)
-				//        {
-				//            // bool fIsLethal = !(exception is Reporting.ConfigurationException);
-				//            //ErrorReporter.ReportException(exception, parent, fIsLethal);
-				//            ExceptionReportingDialog.ReportException(exception);
-				//            return false;
-				//        }
-				//        else
-				//        {
-				//            //todo: the reporting component should do all this, too
-				//            /*                       Exception innerE = ExceptionHelper.GetInnerMostException(exception);
-				//                                   string strMessage = "Error: "; // FwApp.GetResourceString("kstidProgError") + FwApp.GetResourceString("kstidFatalError");
-				//                                   string strVersion;
-				//                                   Assembly assembly = Assembly.GetEntryAssembly();
-				//                                   object[] attributes = null;
-				//                                   if (assembly != null)
-				//                                       attributes = assembly.GetCustomAttributes(typeof(AssemblyFileVersionAttribute), false);
-				//                                   if (attributes != null && attributes.Length > 0)
-				//                                       strVersion = ((AssemblyFileVersionAttribute)attributes[0]).Version;
-				//                                   else
-				//                                       strVersion = Application.ProductVersion;
-				//                                   string strReport = string.Format(
-				//                                       "Got Exception", //"FwApp.GetResourceString("kstidGotException"),
-				//                                       "errors@wesay.org", //"FwApp.GetResourceString("kstidSupportEmail"),
-
-				//                                       exception.Source, strVersion,
-				//                                       ExceptionHelper.GetAllExceptionMessages(exception), innerE.Source,
-				//                                       innerE.TargetSite.Name, ExceptionHelper.GetAllStackTraces(exception));
-				//                                   Trace.Assert(false, strMessage, strReport);
-				//            */
-
-				//            Debug.Fail(exception.Message);
-				//        }
-				//    }
-
-				//}
-				//catch (Exception)
-				//{
-				//    Debug.Fail("This error could not be reported normally: ", exception.Message);
-				//}
-				return true;
+				if (exception is ExternalException externalException
+					&& (uint) (externalException.ErrorCode) == 0x8007000E) // E_OUTOFMEMORY
+				{
+					Debug.WriteLine("Out of memory");
+					Logger.WriteEvent("Out of memory");
+				}
+				else
+				{
+					Debug.Assert(
+						exception.Message != string.Empty || exception is COMException,
+						"Oops - we got an empty exception description. Change the code to handle that!");
+				}
 			}
+			catch (Exception)
+			{
+				Logger.WriteEvent($"This error could not be reported normally: ${exception.Message}");
+				Debug.Fail("This error could not be reported normally: ", exception.Message);
+			}
+
+			return true;
 		}
 	}
 }
