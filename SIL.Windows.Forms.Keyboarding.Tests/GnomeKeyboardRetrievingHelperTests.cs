75b3befb3471ddf6a9586c7ef0ffd3cdd96355e8
Eberhard Beilharz <ermshiperete@users.noreply.github.com>
2020-10-13 21:04:22 +0200

    [feature/nuget - keyboarding] Don't crash if we encounter duplicate layouts (#977)
    Sometimes for whatever reason we get duplicate keyboard layouts.
    This shouldn't lead to a crash. This fixes LT-20410.
    
    (more or less cherry-pick from master branch)

---
+++ SIL.Windows.Forms.Keyboarding.Tests/GnomeKeyboardRetrievingHelperTests.cs
@@ -0,0 +1,52 @@
+// Copyright (c) 2020 SIL International
+// This software is licensed under the MIT License (http://opensource.org/licenses/MIT)
+
+using System.Collections.Generic;
+using NUnit.Framework;
+using SIL.Windows.Forms.Keyboarding.Linux;
+
+namespace SIL.Windows.Forms.Keyboarding.Tests
+{
+	[TestFixture]
+	[Platform(Include="Linux", Reason="Linux specific tests")]
+	public class GnomeKeyboardRetrievingHelperTests
+	{
+		[Test]
+		public void InitKeyboards_XkbAndIbus()
+		{
+			IDictionary<string, uint> registeredKeyboards = null;
+			var installedKeyboards = new[] {
+				"xkb;;us",
+				"ibus;;km:/home/user/.local/share/keyman/khmer_angkor/khmer_angkor.kmx"
+			};
+			var sut = new GnomeKeyboardRetrievingHelper(() => installedKeyboards);
+			Assert.That(() => sut.InitKeyboards(s => true,
+					(keyboards, firstKeyboard) => registeredKeyboards = keyboards),
+				Throws.Nothing);
+			Assert.That(registeredKeyboards, Is.EquivalentTo(new Dictionary<string, int> {
+				{ "us", 0 },
+				{ "km:/home/user/.local/share/keyman/khmer_angkor/khmer_angkor.kmx", 1 }
+			}));
+		}
+
+		[Test]
+		public void InitKeyboards_DuplicateKeyboard()
+		{
+			IDictionary<string, uint> registeredKeyboards = null;
+			var installedKeyboards = new[] {
+				"ibus;;km:/home/user/.local/share/keyman/khmer_angkor/khmer_angkor.kmx",
+				"ibus;;km:/home/user/.local/share/keyman/khmer_angkor/khmer_angkor.kmx",
+				"xkb;;us"
+			};
+			var sut = new GnomeKeyboardRetrievingHelper(() => installedKeyboards);
+			Assert.That(() => sut.InitKeyboards(s => true,
+					(keyboards, firstKeyboard) => registeredKeyboards = keyboards),
+				Throws.Nothing); // LT-20410
+			Assert.That(registeredKeyboards, Is.EquivalentTo(new Dictionary<string, int> {
+				{ "km:/home/user/.local/share/keyman/khmer_angkor/khmer_angkor.kmx", 0 },
+				{ "us", 2 }
+			}));
+		}
+
+	}
+}
