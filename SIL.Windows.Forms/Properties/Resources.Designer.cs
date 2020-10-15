9afb5bb8adbc5d7a75b418836e371c43d29969d3
papeh <hasso_pape@sil.org>
2020-10-15 10:33:28 -0500

    [API] Add PasswordBox widget (#979)
    +semver: minor

--- SIL.Windows.Forms/Properties/Resources.Designer.cs
+++ SIL.Windows.Forms/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace SIL.Windows.Forms.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -138,6 +138,16 @@ namespace SIL.Windows.Forms.Properties {
         public static string OlacRoles {
             get {
                 return ResourceManager.GetString("OlacRoles", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap passwordRevealEye16x16 {
+            get {
+                object obj = ResourceManager.GetObject("passwordRevealEye16x16", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
         
