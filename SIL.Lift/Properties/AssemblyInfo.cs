56a69609b305935b3aa744afb9afac504f91bdaa
Joseph Myers <wjosephmyers@gmail.com>
2022-04-28 09:49:52 +0700

    Removed acknowledgement of Commons.Xml.Relaxng from SIL.Lift.AssemblyInfo

--- SIL.Lift/Properties/AssemblyInfo.cs
+++ SIL.Lift/Properties/AssemblyInfo.cs
@@ -1,12 +1,7 @@
 ﻿using System.Runtime.CompilerServices;
-using SIL.Acknowledgements;
 
 [assembly: InternalsVisibleTo("SIL.Lift.Tests, PublicKey=0024000004800000940000000" +
 	"6020000002400005253413100040000010001008339b2ae1bf006934f6176dec6ea2a8a7d67383613dcb03d7197" +
 	"5e7b05ad546562c84529a4811e94c889e55f2532d1a90baaf20be9bff39ac6f5365bd605d70b90489840b7ba6d1" +
 	"c231b0e550c4abe4f60553856ef142a40a91e53d56e79f69dc79c4e95817de498aac924ee011f03b4e1c1d772d5" +
 	"1c4946c1185e3bfb621bc6")]
-
-[assembly: Acknowledgement("Commons.Xml.Relaxng", Name = "Commons.Xml.Relaxng", Location = "./Commons.Xml.Relaxng.dll",
-	LicenseUrl = "https://opensource.org/licenses/MIT",
-	Url = "http://www.mono-project.com/docs/tools+libraries/libraries/xml/#relax-ng")]
