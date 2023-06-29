961377af6a332b0f461362d5c60a4f07297001b2
tombogle <tom_bogle@sil.org>
2023-06-29 15:10:17 -0400

    Improvements to Windows Defender checks in response to code review.

--- SIL.Windows.Forms/Miscellaneous/WindowsUtilities.cs
+++ SIL.Windows.Forms/Miscellaneous/WindowsUtilities.cs
@@ -18,7 +18,7 @@ namespace SIL.Windows.Forms.Miscellaneous
 	{
 		/// <summary>
 		/// Returns 'true' unless we find we can't write to all the specified folders, in which
-		/// case it (by default) reports the condition to the user.
+		/// case it (normally) reports the condition to the user.
 		/// In Oct of 2017, a Windows update to Defender on some machines set Protections on
 		/// certain basic folders, like MyDocuments! This resulted in throwing an exception any
 		/// time Bloom tried to write out CollectionSettings files! More recently (especially on
@@ -30,25 +30,27 @@ namespace SIL.Windows.Forms.Miscellaneous
 		/// will not be obvious which folder was not writable.)</param>
 		/// <param name="foldersToCheck">The folders to test for write access.</param>
 		[PublicAPI]
-		public static bool CanWriteToDirectories(string applicationName, params string[] foldersToCheck)
+		public static bool CanWriteToDirectories(string applicationName,
+			params string[] foldersToCheck)
 		{
-			return CanWriteToDirectories((e, s) => ReportDefenderProblem(applicationName, e, s),
-				foldersToCheck);
+			return CanWriteToDirectories(GetReportingMethodFor(applicationName), foldersToCheck);
 		}
 		/// <summary>
 		/// Returns 'true' unless we find we can't write to all the specified folders, in which
-		/// case it (by default) reports the condition to the user.
+		/// case it performs the specified report action.
 		/// In Oct of 2017, a Windows update to Defender on some machines set Protections on
 		/// certain basic folders, like MyDocuments! This resulted in throwing an exception any
 		/// time Bloom tried to write out CollectionSettings files! More recently (especially on
 		/// Windows 11), some other apps have been having similar failures due to enhanced anti-
 		/// ransomware checking.
 		/// </summary>
-		/// <param name="report">Action to perform if a folder is found which is not writable
+		/// <param name="report">Action to perform if a folder is found which is not writable (or
+		/// null to jst return false)
 		/// </param>
 		/// <param name="foldersToCheck">The folders to test for write access.</param>
 		[PublicAPI]
-		public static bool CanWriteToDirectories(Action<Exception, string> report, params string[] foldersToCheck)
+		public static bool CanWriteToDirectories(Action<Exception, string> report,
+			params string[] foldersToCheck)
 		{
 			foreach (var folder in foldersToCheck)
 			{
@@ -60,31 +62,33 @@ namespace SIL.Windows.Forms.Miscellaneous
 		}
 
 		/// <summary>
-		/// Returns 'true' unless we find we can't write to the specified folder, in which case it
-		/// (by default) reports the condition to the user.
-		/// In Oct of 2017, a Windows update to Defender on some machines set Protections on certain
-		/// basic folders, like MyDocuments! This resulted in throwing an exception any time Bloom tried
-		/// to write out CollectionSettings files! More recently (especially on Windows 11), some other
-		/// apps have been having similar failures due to enhanced anti-ransomware checking.
+		/// Returns 'true' unless we find we can't write to the specified folder, in which case
+		/// it (normally) reports the condition to the user.
+		/// In Oct of 2017, a Windows update to Defender on some machines set Protections on
+		/// certain basic folders, like MyDocuments! This resulted in throwing an exception any
+		/// time Bloom tried to write out CollectionSettings files! More recently (especially on
+		/// Windows 11), some other apps have been having similar failures due to enhanced anti-
+		/// ransomware checking.
 		/// </summary>
 		/// <param name="applicationName">Name of the application to use when formatting the error.
-		/// (Leave as null to just return false to the caller without reporting the problem.)</param>
+		/// (Leave as null to just return false to the caller without reporting the problem.)
+		/// </param>
 		/// <param name="folderToCheck">An optional folder to test for write access.</param>
 		[PublicAPI]
 		public static bool CanWriteToDirectory(string applicationName = null,
 			string folderToCheck = null)
 		{
-			return CanWriteToDirectory((e, s) => ReportDefenderProblem(applicationName, e, s),
-				folderToCheck);
+			return CanWriteToDirectory(GetReportingMethodFor(applicationName), folderToCheck);
 		}
 
 		/// <summary>
-		/// Returns 'true' unless we find we can't write to the specified folder, in which case it
-		/// (by default) reports the condition to the user.
-		/// In Oct of 2017, a Windows update to Defender on some machines set Protections on certain
-		/// basic folders, like MyDocuments! This resulted in throwing an exception any time Bloom tried
-		/// to write out CollectionSettings files! More recently (especially on Windows 11), some other
-		/// apps have been having similar failures due to enhanced anti-ransomware checking.
+		/// Returns 'true' unless we find we can't write to the specified folder, in which
+		/// case it performs the specified report action.
+		/// In Oct of 2017, a Windows update to Defender on some machines set Protections on
+		/// certain basic folders, like MyDocuments! This resulted in throwing an exception any
+		/// time Bloom tried to write out CollectionSettings files! More recently (especially on
+		/// Windows 11), some other apps have been having similar failures due to enhanced anti-
+		/// ransomware checking.
 		/// </summary>
 		/// <param name="report">Action to perform if a folder is found which is not writable
 		/// </param>
@@ -94,55 +98,81 @@ namespace SIL.Windows.Forms.Miscellaneous
 			string folderToCheck = null)
 		{
 			if (!Platform.IsWindows)
-                return true;
+				return true;
 
-            if (string.IsNullOrEmpty(folderToCheck))
-				folderToCheck = Combine(GetFolderPath(MyDocuments), TestFileName);
-            var testPath = Combine(folderToCheck, TestFileName);
-            try
-            {
-                RobustFile.WriteAllText(testPath, "test contents");
-            }
-            catch (Exception exc)
-            {
-                // Creating a previously non-existent file under these conditions just gives a WinIOError, "could not find file".
-                report?.Invoke(exc, GetDirectoryName(testPath));
+			if (string.IsNullOrEmpty(folderToCheck))
+				folderToCheck = GetFolderPath(MyDocuments);
+			else
+			{
+				if (!Directory.Exists(folderToCheck))
+					throw new ArgumentException(
+						"Folder provided should be an existing directory.",
+						nameof(folderToCheck));
+			}
+
+			string testPath = GetTestFileName(folderToCheck);
+
+			try
+			{
+				RobustFile.WriteAllText(testPath, "test contents");
+			}
+			catch (Exception exc)
+			{
+				// Creating a previously non-existent file under these conditions just gives a WinIOError, "could not find file".
+				report?.Invoke(exc, GetDirectoryName(testPath));
 				return false;
-            }
-            finally
-            {
-                Cleanup(testPath);
-            }
-            return true;
-        }
+			}
+			finally
+			{
+				Cleanup(testPath);
+			}
+			return true;
+		}
 
-		private static string TestFileName => GetFileName(TempFile.CreateAndGetPathButDontMakeTheFile().Path);
+		private static string GetTestFileName(string folderToCheck)
+		{
+			while (true)
+			{
+				var testPath = Combine(folderToCheck,
+					GetFileName(TempFile.CreateAndGetPathButDontMakeTheFile().Path));
+				// Really unlikely that the temp file name would exist, but let's check to be sure.
+				if (!File.Exists(testPath) && !Directory.Exists(testPath))
+					return testPath;
+			}
+		}
+
+		private static Action<Exception, string> GetReportingMethodFor(string applicationName)
+		{
+			if (applicationName == null)
+				return null;
+			return (e, s) => ReportDefenderProblem(applicationName, e, s);
+		}
 
 		[PublicAPI]
 		public static void ReportDefenderProblem(string applicationName, Exception exc, string failingFolder)
-        {
-            var heading = LocalizationManager.GetString("Errors.DefenderFolderProtectionHeading",
-	            "{0} cannot write to the folder {1}.");
-            var mainMsg = LocalizationManager.GetString("Errors.DefenderFolderProtection",
-                "This might be caused by Windows Defender \"Controlled Folder Access\" or some " +
-                "other virus or malware protection. You can search online or contact a local " +
-                "support person for help with configuring Windows Defender (and other antivirus " +
-                "software) to allow this program to work.");
-            var msg = string.Format(heading, applicationName, failingFolder) + NewLine + NewLine + mainMsg;
-            ErrorReport.NotifyUserOfProblem(exc, msg);
-        }
+		{
+			var heading = LocalizationManager.GetString("Errors.DefenderFolderProtectionHeading",
+				"{0} cannot write to the folder {1}.");
+			var mainMsg = LocalizationManager.GetString("Errors.DefenderFolderProtection",
+				"This might be caused by Windows Defender \"Controlled Folder Access\" or some " +
+				"other virus or malware protection. You can search online or contact a local " +
+				"support person for help with configuring Windows Defender (and other antivirus " +
+				"software) to allow this program to work.");
+			var msg = string.Format(heading, applicationName, failingFolder) + NewLine + NewLine + mainMsg;
+			ErrorReport.NotifyUserOfProblem(exc, msg);
+		}
 
-        private static void Cleanup(string testPath)
-        {
-            // try to clean up behind ourselves
-            try
-            {
-	            File.Delete(testPath);
-            }
-            catch (Exception)
-            {
-                // but don't try too hard
-            }
-        }
-    }
+		private static void Cleanup(string testPath)
+		{
+			// try to clean up behind ourselves
+			try
+			{
+				File.Delete(testPath);
+			}
+			catch (Exception)
+			{
+				// but don't try too hard
+			}
+		}
+	}
 }
