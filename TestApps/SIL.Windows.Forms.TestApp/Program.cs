be0aa622c4463c820f31f16d3674d5e8f7e14944
papeh <hasso_pape@sil.org>
2023-10-05 17:04:57 -0500

    Auto-publish Localizations on Tag (#1286)
    * Publish l10ns to NuGet when a version tag is pushed
    
    * Remove unmaintained XLF files from DistFiles
    * Download XLF files from NuGet as a build step
    * Ignore all *.xlf files
    * Update SIL.Windows.Forms.TestApp to work without Palaso.en.xlf
    
    * Remove Mono 5 compensation: we have been using Mono 6 for a while.
    (Mono 5 didn't GeneratePathProperty for PackageReferences)
    
    * Fix AppVeyor Build
    On AppVeyor (but not on TeamCity or my Win 11 dev box), the L10NSharp string extraction log added 331 lines to the console output, causing Get10LinesAsync to fail.
    
    * Enable CommandLineRunnerTests Get10LinesAsync on Linux
    Now that the SUT has been reorganised, or perhaps due to Mono changes, it works.
    
    * Move CommandLineRunnerTestApp to its own app
    Including it in another test app was confusing, complicated, and fragile.

--- TestApps/SIL.Windows.Forms.TestApp/Program.cs
+++ TestApps/SIL.Windows.Forms.TestApp/Program.cs
@@ -1,25 +1,20 @@
 using System;
 using System.IO;
-using System.Threading;
 using System.Windows.Forms;
 using L10NSharp;
 using SIL.IO;
 using SIL.Reporting;
 using SIL.Windows.Forms.Reporting;
 using SIL.WritingSystems;
-using static System.StringComparison;
 
 namespace SIL.Windows.Forms.TestApp
 {
-	static class Program
+	public static class Program
 	{
 		internal static ILocalizationManager PrimaryL10NManager;
 
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
 		[STAThread]
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
@@ -29,43 +24,22 @@ namespace SIL.Windows.Forms.TestApp
 			Sldr.Initialize();
 			Icu.Wrapper.Init();
 
-			var testCommandLineRunner = false;
-			var distFilesEnglishStrings = "Palaso.en.xlf";
-			var localizationType = TranslationMemory.XLiff;
 			var preferredUILocale = "fr";
 			if (args.Length > 0)
 			{
-				if (args[0].Equals("es", OrdinalIgnoreCase))
-				{
-					preferredUILocale = "es";
-				}
-				else
-				{
-					testCommandLineRunner = true;
-				}
+				preferredUILocale = args[0].ToLowerInvariant();
 			}
 
 			var localizationFolder = Path.GetDirectoryName(
-				FileLocationUtilities.GetFileDistributedWithApplication(distFilesEnglishStrings));
-			PrimaryL10NManager = LocalizationManager.Create(localizationType, preferredUILocale, "Palaso", "Palaso",
+				FileLocationUtilities.GetFileDistributedWithApplication($"Palaso.{preferredUILocale}.xlf"));
+			PrimaryL10NManager = LocalizationManager.Create(TranslationMemory.XLiff, preferredUILocale, "Palaso", "Palaso",
 				"1.0.0", localizationFolder, "SIL/Palaso", null, "testapp@sil.org");
-
-			if (testCommandLineRunner)
-			{
-				for (int i = 0; i < 10; i++)
-				{
-					Console.WriteLine(i);
-					Thread.Sleep(1000);
-				}
-				return;
-			}
 
 			Application.Run(new TestAppForm());
 
 			Sldr.Cleanup();
 		}
 
-		/// ------------------------------------------------------------------------------------
 		private static void SetUpErrorHandling()
 		{
 			ErrorReport.EmailAddress = "bogus_test_app_email_addr@sil.org";
