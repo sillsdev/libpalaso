c664b1b9c721a01ba61c9173fe2d5dc3e77190dc
tombogle <tom_bogle@sil.org>
2025-03-14 16:49:32 -0400

    Removed unused Image1 resource from SIL.Media

--- SIL.Media.Tests/Properties/Resources.Designer.cs
+++ SIL.Media.Tests/Properties/Resources.Designer.cs
@@ -88,16 +88,6 @@ namespace SIL.Media.Tests.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Image1 {
-            get {
-                object obj = ResourceManager.GetObject("Image1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
         internal static byte[] ShortMp3 {
