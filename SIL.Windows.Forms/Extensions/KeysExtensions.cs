4242a32f8829b816e8e77ef3324527eeed9ee8ef
tombogle <tom_bogle@sil.org>
2025-05-23 10:01:33 -0400

    Improved summary comments for IsNavigationKey

--- SIL.Windows.Forms/Extensions/KeysExtensions.cs
+++ SIL.Windows.Forms/Extensions/KeysExtensions.cs
@@ -5,12 +5,16 @@ namespace SIL.Windows.Forms.Extensions
 	public static class KeysExtensions
 	{
 		/// <summary>
-		/// Determines if the key is a navigation key.
+		/// Determines whether the specified key is considered a navigation key.
 		/// </summary>
-		/// <param name="key">The key to examine</param>
-		/// <param name="ctrlPressed">If <c>true</c>, the letter `A` will be treated as a
-		/// navigation key. In contexts where Ctrl-A is not a shortcut for Select All or
-		/// that behavior is not relevant, pass false or omit this optional parameter.</param>
+		/// <param name="key">The key (or key-combination) to examine.</param>
+		/// <param name="ctrlPressed">If <c>true</c>, the letter <c>Keys.A</c> will be treated as a
+		/// navigation key (even if the <paramref name="key"/> value does not explicitly include
+		/// <c>Keys.Control</c>). In contexts where Ctrl-A is not a shortcut for Select All or that
+		/// behavior is not relevant, pass false or omit this optional parameter.</param>
+		/// <remarks>If <paramref name="key"/> represents the combination Ctrl-A (i.e.,
+		/// <c>Keys.Control | Keys.A</c>), but <paramref name="ctrlPressed"/> is <c>false</c>,
+		/// this method will return <c>false</c>.</remarks>
 		public static bool IsNavigationKey(this Keys key, bool ctrlPressed = false)
 		{
 			switch (key & Keys.KeyCode)
