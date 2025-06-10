dcb61255f9951bd4ea2197b4d666894ee40f5ad6
John Thomson <john_thomson@sil.org>
2025-06-10 17:13:25 -0500

    +semver:minor Added PortableClipboard.CanGetImage()
    Also restores and enhances the ClipboardTestApp. Most of it is restored after e3f8d8da748bed3a436f3c095b6d86d9dd52f526. I added the button and label for "paste any image" and "contains any image" which use the newer functions, and tweaked to show the whole image.

---
+++ TestApps/ClipboardTestApp/Form1.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SIL.PlatformUtilities;
+using SIL.Windows.Forms.ImageToolbox;
+using SIL.Windows.Forms.Miscellaneous;
+
+namespace ClipboardTestApp
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void OnCopyText(object sender, EventArgs e)
+		{
+			PortableClipboard.SetText(textBox1.Text);
+			UpdateLabels();
+		}
+
+		private void OnPasteText(object sender, EventArgs e)
+		{
+			textBox1.Text = PortableClipboard.GetText();
+		}
+
+		private void OnCopyImage(object sender, EventArgs e)
+		{
+			try
+			{
+				using var openFileDialog =
+					new OpenFileDialogWithViews(OpenFileDialogWithViews.DialogViewTypes.Details);
+				if (openFileDialog.ShowDialog(this) != DialogResult.OK)
+					return;
+
+				using var image = PalasoImage.FromFile(openFileDialog.FileName);
+				PortableClipboard.CopyImageToClipboard(image);
+				UpdateLabels();
+			}
+			catch (NotImplementedException)
+			{
+				if (Platform.IsWindows)
+					throw;
+				// ignore on Linux - currently not yet implemented
+			}
+		}
+
+		private void OnPasteImage(object sender, EventArgs e)
+		{
+			pictureBox1.Image = PortableClipboard.GetImage();
+		}
+
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+			UpdateLabels();
+		}
+
+		protected override void OnActivated(EventArgs e)
+		{
+			base.OnActivated(e);
+			UpdateLabels();
+		}
+
+		private void UpdateLabels()
+		{
+			labelContainsText.Text = PortableClipboard.ContainsText() ? "true" : "false";
+			labelContainsImage.Text = PortableClipboard.ContainsImage() ? "true" : "false";
+			labelContainsAnyImage.Text = PortableClipboard.CanGetImage() ? "true" : "false";
+		}
+
+		private void OnClose(object sender, EventArgs e)
+		{
+			Close();
+		}
+		private void OnPasteAnyImage(object sender, EventArgs e)
+		{
+			var imageFromClipboard = PortableClipboard.GetImageFromClipboard();
+			if (imageFromClipboard == null)
+			{
+				MessageBox.Show("No image found in clipboard.");
+				return;
+			}
+			pictureBox1.Image = imageFromClipboard.Image;
+		}
+
+		private void btnClose_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
+	}
+}
