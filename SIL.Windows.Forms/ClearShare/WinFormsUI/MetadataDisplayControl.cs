e95cacfbb1c94d0dede10bfa5076780814ba7d08
Ariel Rorabaugh <ariel_rorabaugh@sil.org>
2025-10-28 09:35:11 -0400

    Fix ILicenseWithImage cast

--- SIL.Windows.Forms/ClearShare/WinFormsUI/MetadataDisplayControl.cs
+++ SIL.Windows.Forms/ClearShare/WinFormsUI/MetadataDisplayControl.cs
@@ -65,7 +65,11 @@ namespace SIL.Windows.Forms.ClearShare.WinFormsUI
 				}
 				else
 				{
-					var licenseImage = ((ILicenseWithImage)metaData.License).GetImage();
+					System.Drawing.Image licenseImage = null;
+					// Licenses constructed from SIL.Core.Clearshare will not have images,
+					// and will not be of type ILicenseWithImage.
+					if (metaData.License is ILicenseWithImage)
+						licenseImage = ((ILicenseWithImage)metaData.License).GetImage();
 					PictureBox pictureBox = null;
 					if (licenseImage != null)
 					{
