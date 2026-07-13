7747a64408cbb7faf6dc38a559f8fa83d0972338
Danny Rorabaugh <imnasnainaec@gmail.com>
2026-07-13 13:47:49 -0400

    Add wide-image downscale test and factor out cropping-image accessor

--- SIL.Windows.Forms.Tests/ImageToolbox/ImageCropperTests.cs
+++ SIL.Windows.Forms.Tests/ImageToolbox/ImageCropperTests.cs
@@ -16,8 +16,6 @@ namespace SIL.Windows.Forms.Tests.ImageToolbox
 		[Test]
 		public void SetImage_TallImage_DownscalesCroppingImage()
 		{
-			// A tall image (height > 1000, width <= 1000) should be downscaled before cropping,
-			// same as a wide image (width > 1000, height <= 1000).
 			using (var tempFile = TempFile.WithExtension(".png"))
 			{
 				using (var bmp = new Bitmap(100, 1200))
@@ -30,14 +28,42 @@ namespace SIL.Windows.Forms.Tests.ImageToolbox
 					cropper.SetImage(palasoImage);
 
 					// Not owned by this test -- the cropper still holds and will dispose this itself.
-					var croppingImage = (Image)typeof(ImageCropper)
-						.GetField("_croppingImage", BindingFlags.NonPublic | BindingFlags.Instance)
-						.GetValue(cropper);
+					var croppingImage = GetCroppingImage(cropper);
 
 					Assert.Less(croppingImage.Height, 1200,
 						"Tall image should have been downscaled before cropping");
 				}
 			}
+		}
+
+		[Test]
+		public void SetImage_WideImage_DownscalesCroppingImage()
+		{
+			using (var tempFile = TempFile.WithExtension(".png"))
+			{
+				using (var bmp = new Bitmap(1200, 100))
+					bmp.Save(tempFile.Path, ImageFormat.Png);
+
+				using (var palasoImage = PalasoImage.FromFile(tempFile.Path))
+				using (var cropper = new ImageCropper())
+				{
+					cropper.Size = new Size(400, 300);
+					cropper.SetImage(palasoImage);
+
+					// Not owned by this test -- the cropper still holds and will dispose this itself.
+					var croppingImage = GetCroppingImage(cropper);
+
+					Assert.Less(croppingImage.Width, 1200,
+						"Wide image should have been downscaled before cropping");
+				}
+			}
+		}
+
+		private static Image GetCroppingImage(ImageCropper cropper)
+		{
+			return (Image)typeof(ImageCropper)
+				.GetField("_croppingImage", BindingFlags.NonPublic | BindingFlags.Instance)
+				.GetValue(cropper);
 		}
 	}
 }
